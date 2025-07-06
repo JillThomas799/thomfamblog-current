567d2c2d0493ff04a4da72a85c4cb33a608bf22c
Jill <125391549+JillThomas799@users.noreply.github.com>
2025-07-06 19:09:15 +0100

    Addition clerk authentication software and creation of sign in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,25 @@
+import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
 
 
 export default function Home() {
   return (
     <div>
-      <h1>Hello home page</h1>
+      <header className="flex justify-end items-center p-4 gap-4 h-16">
+            <SignedOut>
+              <SignInButton mode="modal">
+              <button className="bg-red-500"> Sign in </button>
+
+              </SignInButton>
+              <SignUpButton>
+                <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+                  Sign Up
+                </button>
+              </SignUpButton>
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+          </header>
     </div>
   );
 }
