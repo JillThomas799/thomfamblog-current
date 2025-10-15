21c514b064844aba4479b917e457181c2926240c
Jill <125391549+JillThomas799@users.noreply.github.com>
2025-10-15 11:42:56 +0100

    Retest of seed.ts. This is now working!

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,16 +1,16 @@
-// import { PrismaClient } from "@prisma/client";  //allows the seed function below to access prisma and import the PrismaClient class.
-// import { Month } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";  //allows the seed function below to access prisma and import the PrismaClient class.
+import { Month } from "@prisma/client";
 
 const prisma =  new PrismaClient()          //Creates an instance of this class. With the prisma variable, you have access to altering and executing commands to your table.  It has full details of how our schema is because we ran the 'generate' function. 
 
-// async function seed() {
-//     await prisma.month.createMany( {
-//         data: [ {
-//             id: "December2025", year: 2025, month: "December", component: "<December2025 />"},
+async function seed() {
+    await prisma.month.createMany( {
+        data: [ {
+            id: "December2025", year: 2025, month: "December", component: "<December2025 />"},
            
-//          ]
-//     })
-// }
+         ]
+    })
+}
 
 
-// seed().then (() => prisma.$disconnect());           //This calls the seed() function but importantly, also disconnects the connection between our project with our prisma client.  prisma.$disconnect() prevents any leaks from happening due to us not closing the connection.
+seed().then (() => prisma.$disconnect());           //This calls the seed() function but importantly, also disconnects the connection between our project with our prisma client.  prisma.$disconnect() prevents any leaks from happening due to us not closing the connection.
