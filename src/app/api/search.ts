4a571aa104a024e63b33c67c5031bd71a802aaa9
Jill <125391549+JillThomas799@users.noreply.github.com>
2025-10-15 16:39:25 +0100

    Removal of pagination files as not required

--- src/app/api/search.ts
+++ src/app/api/search.ts
@@ -1,40 +1,3 @@
-'use client';
-
-import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
-import { useSearchParams, usePathname, useRouter } from 'next/navigation';
- 
-export default function Search({ placeholder }: { placeholder: string }) {
-  const searchParams = useSearchParams();
-    const pathname = usePathname();
-  const { replace } = useRouter();
-  function handleSearch(term: string) {
-      const params = new URLSearchParams(searchParams);
-  if (term) {
-      params.set('query', term);
-    } else {
-      params.delete('query');
-    }
-     replace(`${pathname}?${params.toString()}`);
-  }
- 
-  return (
-    <div className="relative flex flex-1 flex-shrink-0">
-      <label htmlFor="search" className="sr-only">
-        Search
-      </label>
-      <input
-        className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-md outline-2 placeholder:grey-500"
-        placeholder={'Search required monthly blog in format monthyear eg. August2025'}
-        onChange={(e) => {
-          handleSearch(e.target.value);
-        }}
-         defaultValue={searchParams.get('query')?.toString()}
-      />
-      <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[24px] w-[18px] -translate-y-1/2 text-white peer-focus:text-gray-900" />
-    </div>
-  );
-}
-
 //This file is the api end point for our searchQuery. It is the code in this file which enables us to connect to our database to look for the terms of our searchQuery.
 
 import { Month } from "@prisma/client";
@@ -47,8 +10,10 @@ import { PrismaClient, Prisma } from "@prisma/client";
 const prisma =  new PrismaClient()          //Creates an instance of this class. With the prisma variable, you have access to altering and executing commands to your table.  It has full details of how our schema is because we ran the 'generate' function. 
 
 
- async function handler(
-  req: NextApiRequest,   res: NextApiResponse) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   if (req.method === "GET") {
     try {
       const { q: query } = req.query;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -31,7 +31,6 @@ export default async function Home() {
     
     <div className="col-span-1 md:col-span-2 p-2 gap-3 bg-[#90AEAD]">
     <div>
-      {/* <MonthsTableSkeleton /> */}
     <October2025 />
     <September2025 />
     <August2025 />
