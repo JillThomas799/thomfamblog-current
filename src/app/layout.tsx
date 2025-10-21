62e5672ddeb7131ae239535396d198bf72b587b7
Jill <125391549+JillThomas799@users.noreply.github.com>
2025-10-21 11:06:04 +0100

    Addition of vercel speed insights function

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import { ThemeProvider } from "next-themes";
 import Navbar from "@/components/ui/Navbar";
 import Sidebar from "@/components/ui/Sidebar";
 import { Toaster } from "react-hot-toast";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 
 const geistSans = localFont({
@@ -49,6 +50,7 @@ export default function RootLayout({
                 <div className="max-w-7xl mx-auto px-4 ">
                   <div className="flex flex-col lg:grid-cols-12 gap-6">
                     <div className="hidden lg:block lg:col-span-3">
+                      <SpeedInsights />
                       {/* <Sidebar />  */}
                       {/* Removed sidebar which holds Sidebar.tsx settings. can be restored if needed or desired. */}
                     </div>
