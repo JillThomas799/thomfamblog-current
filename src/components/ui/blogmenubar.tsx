94fef041dbfa659e6b73cd474f1893819643d0f2
Jill <125391549+JillThomas799@users.noreply.github.com>
2025-10-24 16:42:48 +0100

    Work on tailwind css to finalise blog design for merger

--- src/components/ui/blogmenubar.tsx
+++ src/components/ui/blogmenubar.tsx
@@ -17,7 +17,7 @@ import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from "@/co
 const BlogMenuBar=()    => {
     const [isCollapsed , setIsCollapsed] = useState(true);
   return (
-  <div className={`h-10 bg-gray-800 text-gray-100 flex flex-col ${isCollapsed ? 'h-10':'h-full w-full'} transition-all duration-300`}>
+  <div className={`h-10 bg-gray-800 text-gray-100 mb-4 flex flex-col ${isCollapsed ? 'h-10':'h-full w-full'} transition-all duration-300`}>
         <Button 
         variant="outline"
         onClick={()=> setIsCollapsed(!isCollapsed)} className="w-auto h-auto text-gray font-bold items-center focus:outline-none">BLOG MENU<MenuIcon /> 
@@ -25,17 +25,16 @@ const BlogMenuBar=()    => {
 
 
 {! isCollapsed && (
-    <div className="flex">
-  
-    <div className="flex flex-col items-center h-20 bg-gray-800 border-b border-gray-700  p-4 hover:text-blue-300">
+    <div className="flex flex-col md:flex-row md:flex-wrap md:place-content-evenly">  
+    <div className="text-center h-15 bg-gray-600 border-b border-gray-700 font-semibold p-4 hover:text-blue-300">
       <Link href="/dashboard/months/august2025" >August 2025</Link></div>
-       <div className="flex flex-col items-center h-20 bg-gray-800 border-b border-gray-700  p-4 hover:text-blue-300">
+       <div className="text-center h-15 bg-gray-600 border-b border-gray-700 font-semibold  p-4 hover:text-blue-300">
       <Link href="/dashboard/months/september2025" >September 2025</Link>
     </div>
-    <div className="flex flex-col items-center h-20 bg-gray-800 border-b border-gray-700  p-4 hover:text-blue-300">
+    <div className="text-center h-15 bg-gray-600 border-b border-gray-700 font-semibold p-4 hover:text-blue-300">
       <Link href="/dashboard/months/october2025" >October 2025</Link>
     </div>
-    <div className="flex flex-col items-center h-20 bg-gray-800 border-b border-gray-700  p-4 hover:text-blue-300">
+    <div className="text-center h-15 bg-gray-600 border-b border-gray-700 font-semibold p-4 hover:text-blue-300">
       <Link href="/dashboard/months/november2025" >November 2025</Link>
     </div>
     </div>
