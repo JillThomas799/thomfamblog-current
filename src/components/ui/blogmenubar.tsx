af0974789a5ca8d41852cb0010dfae47b7488564
Jill <125391549+JillThomas799@users.noreply.github.com>
2025-10-25 19:45:57 +0100

    Work on adding loading spinner

--- src/components/ui/blogmenubar.tsx
+++ src/components/ui/blogmenubar.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from "react";
 import Link from "next/link";
+import { Spinner } from "@/components/ui/spinner"
 import {
   BellIcon,
   HomeIcon,
@@ -18,9 +19,12 @@ const BlogMenuBar=()    => {
     const [isCollapsed , setIsCollapsed] = useState(true);
   return (
   <div className={` h-10 bg-gray-800 text-gray-100 mb-4 flex flex-col ${isCollapsed ? 'h-10':'h-full w-full'} transition-all duration-300`}>
-        <Button 
-        variant="outline"
-        onClick={()=> setIsCollapsed(!isCollapsed)} className="w-auto h-auto bg-slate-300 md:bg-gray-800 text-gray-800 md:text-gray-200 font-bold text-md items-center gap-x-10 focus:outline-none">BLOG MENU<MenuIcon /> 
+    <Button 
+      variant="outline"
+      onClick={() => setIsCollapsed(!isCollapsed)}
+      className="w-auto h-auto bg-slate-300 md:bg-gray-800 text-gray-800 md:text-gray-200 font-bold text-md items-center gap-x-10 focus:outline-none"
+    >
+      BLOG MENU <MenuIcon />
     </Button>
 
 
@@ -34,9 +38,7 @@ const BlogMenuBar=()    => {
     <div className="text-center h-15 bg-gray-800 border-b border-gray-700 font-semibold p-4 hover:text-blue-300">
       <Link href="/dashboard/months/october2025" >October 2025</Link>
     </div>
-    <div className="text-center h-15 bg-gray-800 border-b border-gray-700 font-semibold p-4 hover:text-blue-300">
-      <Link href="/dashboard/months/november2025" >November 2025</Link>
-    </div>
+    
     </div>
 )}
 </div>
