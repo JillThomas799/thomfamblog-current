74b9dded30c003b661ce0da58cfaf016d069dfb7
Jill <125391549+JillThomas799@users.noreply.github.com>
2025-11-02 11:54:36 +0000

    Work on November blog layout

--- src/components/ui/November2025.tsx
+++ src/components/ui/November2025.tsx
@@ -2,23 +2,31 @@
 
 import Image from 'next/image'
 import Link from 'next/link'
-import NovemberBonfire from "./novemberbonfire";  
+import NovemberBonfire from "./novemberbonfire"; 
+import "./November2025.css"; 
 
 
 export default function November2025() {
 
   return (   
- <div className=" bg-[#383f17] border-solid border-2 border-[#22311d] mb-28"> {/*November block background*/}
+ <div className=" bg-black border-solid border-2 border-black mb-28"> {/*November block background*/}
     <div>
-       <div className="text-[#FBE9d0] text-center font-semibold text-2xl p-3 bg-[#2F4D49] border-solid border-2 border-[#22311d] m-2 animate-pulse">November 2025
+       <div className="text-white text-center font-semibold text-2xl p-3 bg-[#41231c] border-solid border-2 border-[#b55b4b] m-2 animate-pulse">November 2025
         </div>
+        <div className="FireworkContainer">
+         <div className="pyro">
+            {" "}
+            <div className="before"></div>
+            <div className="after"></div>
+          </div>{" "}
+          {/*Closing div pyro*/}
         <div className="bg-black h-[65vh] ml-2 mr-2 mt-0 mb-[10%]"><NovemberBonfire /></div>
-
+        </div>
         <div className="mb-20"> 
-      <div className="flex flex-col max-w-full leading-1.5 sm:p-2 md:p-2 lg:p-2 m-4 border-gray-800 border-solid border-2 bg-[#b55b4b] ">
-         <div className="text-md md:text-lg max-w-full font-bold text-[#D99E49] text-center dark:text-[#D99E49] justify-center items-center">
-        {/* <div className="font-bold py-2 px-12 text-[#FBE9D0] text-center dark:text-[#FBE9D0]">  October 2025</div> End of news date block  */}
-        <div className="font-bold text-left border-solid text-black font-normal border-black border-2 mr-1 ml-1 mb-4 mt-4 px-4 py-4 bg-[#61603c]">
+      <div className="flex flex-col max-w-full leading-1.5 pr-3 pl-3 sm:p-2 md:p-2 lg:p-2 m-4 border-gray-800 border-solid border-2 bg-[#b55b4b] ">
+         <div className="text-md md:text-lg max-w-full font-bold text-white text-center dark:text-white justify-center items-center">
+
+        <div className="text-left text-white border-solid  font-semibold border-black border-2 mr-1 ml-1 mb-4 mt-4 px-4 py-4 bg-[#773f3f]">
           Welcome to our November blog. The days are getting colder (a little), Harry has settled really well into his new job and is very much enjoying it.   </div>{" "}
       </div>{/*End of individual dated entry photo album block*/} 
            </div> {/*End of news content block*/}
@@ -26,11 +34,11 @@ export default function November2025() {
 
                 <div className="mb-20"> 
       <div className="flex flex-col max-w-full leading-1.5 sm:p-2 md:p-2 lg:p-2 m-4 border-gray-800 border-solid border-2 bg-[#b55b4b]">
-         <div className="text-md md:text-lg max-w-full font-bold text-[#D99E49] text-center dark:text-[#D99E49] justify-center items-center">
-        <div className="font-bold py-2 px-12 text-[#22311d] text-center dark:text-[#22311d]"> 1st November 2025</div> {/*End of news date block*/} 
-        <div className="font-bold border-solid border-black border-2 mr-1 ml-1 mb-4 px-4 py-4 bg-[#61603c]">Sizergh Castle in the electric car</div> {/*End of news header block*/}
+         <div className="text-md md:text-lg max-w-full font-bold text-white text-center dark:text-white justify-center items-center">
+        <div className="font-bold py-2 px-12 text-white text-center dark:text-white"> 1st November 2025</div> {/*End of news date block*/} 
+        <div className="font-bold text-white border-solid border-black border-2 mr-1 ml-1 mb-2 mt-2 px-4 py-4 bg-[#773f3f]">Sizergh Castle in the electric car</div> {/*End of news header block*/}
         
-        <div className="pr-1 pl-1 md:pr-1 md:pl-1 font-bold text-left text-[#22311d] dark:text-[#22311d]">
+        <div className="pr-3 pl-3 md:pr-1 md:pl-1 font-semibold text-left text-[#22311d] dark:text-[#22311d]">
          Today we decided to take the new electric car on a test longer drive of 132 miles. So we headed off as a family to Sizergh Castle, Kendal.
          <br />
          <br />  
