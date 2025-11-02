58eeb5189698b4585762d26661461a9ed3358ad2
Jill <125391549+JillThomas799@users.noreply.github.com>
2025-11-02 16:40:38 +0000

    Finalization of initial November 2025 blog layout

--- src/components/ui/November2025.tsx
+++ src/components/ui/November2025.tsx
@@ -4,8 +4,7 @@ import Image from 'next/image'
 import Link from 'next/link'
 import NovemberBonfire from "./novemberbonfire"; 
 import "./November2025.css"; 
-import MG4Electric from "/public/imagesresized/MG4Electric.jpg"
-
+import MG4Electric from '/public/imagesresized/MG4Electric.jpg'
 
 export default function November2025() {
 
@@ -20,21 +19,23 @@ export default function November2025() {
             <div className="before"></div>
             <div className="after"></div>
           </div>{" "}
-          {/*Closing div pyro*/}
-        <div className="bg-black h-[65vh] ml-2 mr-2 mt-0 mb-[10%]"><NovemberBonfire /></div>
+         {/*Closing div pyro*/}
+        <div className="bg-black to-[100%] h-[75vh] sm:h-[55vh] md:h-[65vh] mx-auto mt-0">
+          <NovemberBonfire />
+        </div>
         </div>
         <div className="mb-20"> 
       <div className="flex flex-col max-w-full leading-1.5 pr-3 pl-3 sm:p-2 md:p-2 lg:p-2 m-4 border-gray-800 border-solid border-2 bg-[#b55b4b] ">
          <div className="text-md md:text-lg max-w-full font-bold text-white text-center dark:text-white justify-center items-center">
 
-        <div className="text-center text-white border-solid  font-semibold border-black border-2 mr-1 ml-1 mb-4 mt-4 px-4 py-4 bg-[#773f3f]">
-          Welcome to our November 2025 blog. The days are getting colder (although not yet cold enough to need to light the log fire). This month includes Bonfire night and so this month's display is topical, being that of a bonfire and fireworks animation. It is also time for Pets' Classics on ClassicFM radio, Lennie's favourite programme.
+        <div className="text-center text-white font-semibold border-black border-solid border-2 mr-1 ml-1 mb-4 mt-4 px-4 py-4 bg-[#773f3f]">
+          Welcome to our November 2025 blog. The days are getting colder (although not yet cold enough to need to light the log fire). This month includes bonfire night and so this month's display is topical, being that of a bonfire and fireworks animation. It is also time for Pets' Classics on ClassicFM radio, Lennie's favourite programme.
           <br />
           <br />
           Harry has settled very well into his new job and is really enjoying it, so congratulations to Harry. Elena is keeping well and working hard. She is looking forward to welcoming her partner up to Cumbria later this month, as are we all.
           <br />
           <br />
-          Otherwise, following the climate change exhibition at Hexham cathedral, we have taken the plunge and bought ourselves our first electric car. So that has been quite exciting. We have included some photos with this month's blog
+          Otherwise, following the climate change exhibition at Hexham cathedral, we have taken the plunge and bought ourselves our first electric car. So that has been quite exciting. We have included some photos with this month's blog.
           <br />
           <br />
           Bernard and Jill are off to Glasgow this month for a short break, so we will be adding some photos and information regarding this trip as part of this month's blog. Otherwise, we hope everyone else is also well and enjoying November.
@@ -47,7 +48,7 @@ export default function November2025() {
       <div className="flex flex-col max-w-full leading-1.5 sm:p-2 md:p-2 lg:p-2 m-4 border-gray-800 border-solid border-2 bg-[#b55b4b]">
          <div className="text-md md:text-lg max-w-full font-semibold text-white text-center dark:text-white justify-center items-center">
         <div className="font-semibold py-2 px-12 text-white text-center dark:text-white"> 1st November 2025</div> {/*End of news date block*/} 
-        <div className="font-semibold text-white border-solid border-black border-2 mr-1 ml-1 mb-2 mt-2 px-4 py-4 bg-[#773f3f]">Sizergh Castle in the electric car</div> {/*End of news header block*/}
+        <div className="font-semibold text-white border-solid border-black border-2 mr-1 ml-1 mb-2 mt-2 px-4 py-4 bg-[#773f3f]">Sizergh Castle, Kendal in the electric car</div> {/*End of news header block*/}
         
         <div className="text-center pr-3 pl-3 md:pr-1 md:pl-1 font-semibold text-white dark:text-white border-solid border-black border-2 mr-1 ml-1 mb-2 mt-2 px-4 py-4 bg-[#773f3f]">
          Today we decided to take the new electric car on a longer drive of 132 miles to test it out, so we headed off as a family to Sizergh Castle, Kendal for the day. 
@@ -56,34 +57,14 @@ export default function November2025() {
          The car is an MG4 Turbo extended range. It has a good range from its 77kwh battery so we easily managed to reach Sizergh and back on one charge with still 45% battery remaining. It drives really well and is easy to manoeuvre making it feel both light and agile. It is an automatic, which seems strange at first, after driving a manual car but you soon get used to it.
          <br />
          <br />
-         There were some minor teething problems such as the speed limit detection system, which had been working fine on the first day, not initially seeming to be working, . We soon discovered however, that this was due to rain water partially obscuring the cameras. A quick wipe with a cloth and all was fine! We are looking forward during the forthcoming months to hopefully reducing our carbon footprint a little by using this vehicle. 
+         There were some minor teething problems such as the speed limit detection system, which had been working fine on the first day, not seeming to be working, . We soon discovered however, that this was due to rain water partially obscuring the cameras. A quick wipe with a cloth and all was fine! We are looking forward during the forthcoming months to hopefully reducing our carbon footprint a little by using this vehicle. 
          <br />
          <br />  
          <Image
                   src={ MG4Electric }
-                  className="block ml-auto mr-auto h-auto w-[70%] md:w-[70%] lg:w-[60%] border-solid border-[#22311d] border-2 mt-1 mb-11 rounded-[70%]"
+                  className="block ml-auto mr-auto h-auto w-[90%] md:w-[70%] lg:w-[60%] border-solid border-black-800 border-4 mt-1 mb-11 rounded-[70%]"
                   alt="Photo of MG4 electric"
                 />
-
-           {/* <div className="flex flex-col border-solid border-2 border-[#22311d] bg-[#61603c] pb-2 pt-2 mr-[12%] ml-[12%] mt-5 mb-4  justify-center text-center">
-            <div className="mt-1 mb-1 sm:m-2 text-md font-normal block text-[#22311d] bg-[#61603c]">
-              {" "}
-                <div className="m-1 p-1 font-bold  border-gray-800 border-solid border-2 bg-[#b55b4b]">Click on the image below to open the album <br /> of our visit to Hexham Abbey, the Gaia Exhibition and to visit the Climate Change Exhibition for yourself</div>
-                <br />
-              <br />
-              <Link
-                href="https://photos.app.goo.gl/9Y9ydZbHXfyR27Ty8"
-                target="_blank"
-              >
-                <Image
-                  src={ GaiaTitled }
-                  className="block ml-auto mr-auto h-auto w-[70%] md:w-[70%] lg:w-[60%] border-solid border-[#22311d] border-2 mt-1 mb-11 rounded-[70%]"
-                  alt="Photo of Durham Cathedral"
-                />
-              </Link>
-            </div>{" "} */}
-      {/* </div>                 End of individual dated entry photo album block  */}
-           {/* </div>            End of news content block */}
            </div>           {/*  End of individual dated entry design and color template block */}
            </div> {/*End of individual dated entry block*/}</div>     
           
