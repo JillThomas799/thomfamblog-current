cb546821f9fecb7f97ab9443e7783a5039f2e657
Jill <125391549+JillThomas799@users.noreply.github.com>
2025-11-02 09:10:47 +0000

    Addition of new November blog entries

--- src/components/ui/November2025.tsx
+++ src/components/ui/November2025.tsx
@@ -18,8 +18,8 @@ export default function November2025() {
       <div className="flex flex-col max-w-full leading-1.5 sm:p-2 md:p-2 lg:p-2 m-4 border-gray-800 border-solid border-2 bg-[#b55b4b] ">
          <div className="text-md md:text-lg max-w-full font-bold text-[#D99E49] text-center dark:text-[#D99E49] justify-center items-center">
         {/* <div className="font-bold py-2 px-12 text-[#FBE9D0] text-center dark:text-[#FBE9D0]">  October 2025</div> End of news date block  */}
-        <div className="font-bold text-left border-solid border-black border-2 mr-1 ml-1 mb-4 mt-4 px-4 py-4 bg-[#61603c]">
-            </div>{" "}
+        <div className="font-bold text-left border-solid text-black font-normal border-black border-2 mr-1 ml-1 mb-4 mt-4 px-4 py-4 bg-[#61603c]">
+          Welcome to our November blog. The days are getting colder (a little), Harry has settled really well into his new job and is very much enjoying it.   </div>{" "}
       </div>{/*End of individual dated entry photo album block*/} 
            </div> {/*End of news content block*/}
            </div> {/*End of individual dated entry design and color template block*/}  
