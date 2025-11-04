67a6c44b86f103897bf1fd2ea4e5ea0b3d5d570b
Jill <125391549+JillThomas799@users.noreply.github.com>
2025-11-04 17:17:13 +0000

    Work on bernard's bio profile

---
+++ src/components/ui/bernardbioprofile.tsx
@@ -0,0 +1,70 @@
+import "./Bio_bernard.css";
+import Image from 'next/image'
+import Link from 'next/link'
+import bernardbiophoto from '/public/imagesresized/bernardbiophoto.jpg';
+
+
+export function BernardBio() {
+  return (
+    <div>
+      <div className="midnightblue">
+        <section className="Profession">
+          <h2>Profession</h2>
+          <h3>Retired General Practitioner</h3>
+        </section>
+
+        <div className="profile">
+          <section className="Likes">
+            <h2>Likes</h2>
+            <ul>
+            <li>Playing on computer games, especially blood-bourne</li>
+            <li>Cats</li>
+            <li>Being retired</li>
+            <li>Cooking</li>
+            </ul>
+          </section>
+          <section className="photo">
+            <figure>
+              <Image id="Bernard_profile_photo" src={bernardbiophoto} alt="" />
+              <figcaption>
+                <strong />
+              </figcaption>
+            </figure>
+            <article>
+              {" "}
+              Bernard was born in Billinge and grew up in Ashton-in-Makerfield,
+              the second eldest of three boys. He attended Ashton Grammar School
+              and at 16 years of age then went onto study for his A levels at
+              Wigan Technical College. After applying for and accepting a place
+              to study dentistry, he changed his mind and secured a place at
+              Manchester University to study medicine.
+            </article>
+          </section>
+          <section className="Dislikes">
+            <h2>Dislikes</h2>
+            <ul>
+            <li>Shopping</li>
+            <li>Cruises</li>
+            <li>Going out to work</li>
+            <li>Foreign holidays</li>
+            </ul>
+          </section>
+        </div>
+        <section className="surprisingFacts">
+          <h2>Surprising Facts</h2>
+          <ul>
+          <li>Bernard has an 'O' level in Astronomy.</li>
+          <li>
+            In 2013 was,together with Jill, voted by patients as the 2nd best GP
+            practice in the UK for trust and confidence in their doctor
+          </li>
+          <li>
+            Favourite saying is that of Groucho Marx who said 'I refuse to join
+            any club which would have me as a member'
+          </li>
+          </ul>
+        </section>
+      </div>
+    </div>
+  );
+}
