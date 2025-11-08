ac189bb300a7306d2d072963457c625631837d98
Jill <125391549+JillThomas799@users.noreply.github.com>
2025-11-08 09:11:12 +0000

    Edits to desktop navbar

--- src/components/ui/DesktopNavbar.tsx
+++ src/components/ui/DesktopNavbar.tsx
@@ -12,7 +12,9 @@ async function DesktopNavbar() {
   return (
     <div className="hidden md:flex items-center space-x-4">
         <ModeToggle />
-        <Button variant="ghost" className="flex items-center gap-2 font-bold b-solid bg-slate-500 dark:bg-black p-4" asChild>
+               {user ? (
+            <>
+             <Button variant="ghost" className="flex items-center gap-2 font-bold b-solid bg-slate-500 dark:bg-black p-4" asChild>
             <Link href="/">
             <HomeIcon className="w-4 h-4" />
             <span className="hidden lg:inline text-lg text-slate-300 dark:text-slate-200 ">Home</span>
@@ -25,9 +27,6 @@ async function DesktopNavbar() {
             <span className="hidden lg:inline text-lg text-slate-300 dark:text-slate-200 ">About Us</span>
             </Link>
         </Button>
-
-        {user ? (
-            <>
             <Button variant="ghost" className="flex items-center gap-2 font-semibold" asChild>
                 <Link href="/notifications">
                 <BellIcon className="w-4 h-4" />
