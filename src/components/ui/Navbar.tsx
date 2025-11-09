093abd1c8aa49198caf71d65c19056764bd8b2b7
Jill <125391549+JillThomas799@users.noreply.github.com>
2025-11-09 13:24:23 +0000

    Changes to background color scheme on mobile screens

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -12,11 +12,12 @@ async function Navbar() {
 
   
   return (
-    <nav className="sticky top-0 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 z-50 ">
+    <nav className="sticky top-0 w-full bg-white md:bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 z-50 ">
+      {/*The background property refers to the default background color which is set in globals.css. Refer to globals.css to see settings */}
         <div className="w-screen mx-auto px-4 py-20 border-b-4 border-solid border-neutral-100">
             <div className="flex items-center justify-between h-16">
                 <div className="flex-items-center">
-                    <div className=" text-5xl font-bold text-primary font-mono tracking-wider text-slate-500 dark:text-slate-300">Thomas Family Blog</div>
+                    <div className=" text-5xl font-bold text-primary font-mono tracking-wider text-slate-800 md:text-slate-500 dark:text-slate-300">Thomas Family Blog</div>
                 </div>
           <DesktopNavbar />
           <MobileNavbar />
