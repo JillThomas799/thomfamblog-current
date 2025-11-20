8958380492aba1484e3b080bbf13c03d02a28d7a
Jill <125391549+JillThomas799@users.noreply.github.com>
2025-11-20 21:18:54 +0000

    Addition 16th December 2025 blog Dumbarton Rock and Castle

--- src/components/ui/November2025.tsx
+++ src/components/ui/November2025.tsx
@@ -8,6 +8,7 @@ import JillGlasgowTramTitled from '/public/imagesresized/JillGlasgowTramTitled.j
 import TheatreRoyalTitled from '/public/imagesresized/TheatreRoyalTitled.jpg'
 import KelvingroveOrganTitled from '/public/imagesresized/KelvingroveOrganTitled.jpg'
 import GlasgowCathedralTitled from '/public/imagesresized/GlasgowCathedralTitled.jpg'
+import DumbartonCastleTitled from '/public/imagesresized/DumbartonCastleTitled.jpg'
 
 export default function November2025() {
 
@@ -292,13 +293,46 @@ export default function November2025() {
            </div>
 
 
-
-
-
-
-
+<div className="mb-20"> 
+      <div className="flex flex-col max-w-full leading-1.5 sm:p-2 md:p-2 lg:p-2 m-4  border-gray-800 border-solid border-2 bg-[#b55b4b]">
+         <div className="text-md md:text-lg max-w-full font-semibold text-white text-center dark:text-white justify-center items-center">
+        <div className="font-semibold py-2 px-12 text-white text-center dark:text-white"> 16th November 2025, Sunday.</div> {/*End of news date block*/} 
+        <div className="font-semibold text-white border-solid border-black border-2 mr-1 ml-1 mb-2 mt-2 px-4 py-4 bg-[#773f3f]">Glasgow City Break Day 5: Dumbarton Rock and Castle and return home.</div> {/*End of news header block*/}
+        
+        <div className="text-left pr-3 pl-3 md:pr-3 md:pl-3 font-normal text-white dark:text-white border-solid border-black border-2 mr-1 ml-1 mb-2 mt-2 px-4 py-4 bg-[#773f3f]">It was our final day in Scotland today and time to go home. It had been a really busy few days and we had packed lots of things in, but before we headed off home we first wanted to take a slight detour further west of the city to see a very important historic location Dumbarton Rock and Castle or 'Alt Clud', the capital of the ancient Kingdom of Strathclyde between the 5th and 11th centuries.
+          <br />
+          <br />
+          We were very lucky because it was an absolutely beautiful day, sunny but cold and clear meaning the views from the top of the Rock were amazing. It was easy to see why it had been such an important and popular stronghold for past Kingdoms and their rulers. 
+           <br />
+          <br />
+          We were also very fortunate as we were one of the first visitors there and as we were climbing the Rock met a volunteer who goes there every day and he got chatting to us. We learnt a tremendous amount as he lives locally and is very knowledgeable about the local history. He was in the navy for many years and was a submariner for a period so he had some interesting stories. He told us much about the Clyde and the shipyards, the types of boat out on that day and what their roles were, together with a very detailed description of the history of Dumbarton Rock and its buildings.
+        
+         <div className="flex flex-col border-solid border-2 border-[#22311d] bg-[#b55b4b] pb-2 pt-2 mr-[12%] ml-[12%] mt-5 mb-4  justify-center text-center">
+            <div className="mt-1 mb-1 sm:m-2 text-md font-normal block text-white bg-[#b55b4b]">
+              {" "}
+         <div className="m-1 p-1 font-normal  border-gray-800 border-solid border-2 bg-[#773f3f] mb-6">Click on the image below to open the album <br /> of our day at Dumbarton Rock and Castle.</div>
+          <Link
+                href="https://photos.app.goo.gl/xEiPNdRy5Gen7VfP6"
+                target="_blank"
+              >
+         <Image
+                  src={ DumbartonCastleTitled }
+                  className="block ml-auto mr-auto h-auto w-[90%] md:w-[70%] lg:w-[60%] border-solid border-black-400 border-4 mt-1 mb-11 rounded-[70%]"
+                  alt="Photo of Governor's House Dumbarton."
+                />
+                </Link>
            
-     
+             </div>{" "}
+      </div>{/*End of individual dated entry photo album block*/} 
+      </div>           {/*  End of individual dated entry design and color template block */}
+           </div> {/*End of individual dated entry block*/}
+           </div>
+           </div>
+
+
+
+
+
       </div> {/*End of November block*/}
       </div> {/*End of November block background*/}
      </div>
