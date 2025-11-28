84c45e9a797c917c3d7e90b89b512b399ba2a122
Jill <125391549+JillThomas799@users.noreply.github.com>
2025-11-28 15:01:21 +0000

    Development December2025blog

--- src/components/ui/December2025.tsx
+++ src/components/ui/December2025.tsx
@@ -5,11 +5,10 @@ import SnowmanShakerDisplay from './snowmanshakerdisplay';
 
 export default function December2025() {
   return (   
- <div className=" bg-black border-solid border-2 border-black mb-28"> {/*December block background*/}
+ <div className=" bg-black border-solid border-4 border- border-[#165b33]  mb-28"> {/*December block background*/}
     <div>
-       <div className="text-white text-center font-semibold text-2xl p-3 bg-[#bb252b] border-solid border-2 border-[#165b33] m-2 animate-pulse">December 2025
-        </div> 
-        <div className="block relative mt-[70%] mb-[135%] md:mt-[50%] md:scale-60 md:mb-[100%] lg:scale-80 text-center 
+       <div className="text-white text-center font-semibold text-2xl p-3 bg-[#bb252b] border-solid border-2 border-[#165b33] m-2 animate-pulse">December 2025 </div> 
+        <div className="block relative mt-[70%] mb-[135%] md:mt-[50%] md:scale-[65%]  md:mb-[100%] lg:scale-80 text-center 
         text-[1.75em] font-bold width-full height-full ">
             <SnowmanShakerDisplay /></div>   
               
@@ -26,7 +25,7 @@ export default function December2025() {
                 <div className="mb-20"> 
       <div className="flex flex-col max-w-full leading-1.5 sm:p-2 md:p-2 lg:p-2 m-4 border-gray-800 border-solid border-2 bg-[#165b33]">
          <div className="text-md md:text-lg max-w-full font-semibold text-white text-center dark:text-white justify-center items-center">
-        <div className="font-semibold py-2 px-12 text-white text-center dark:text-white"> 1st November 2025</div> {/*End of news date block*/} 
+        <div className="font-semibold py-2 px-12 text-white text-center dark:text-white"> 1st December 2025</div> {/*End of news date block*/} 
         <div className="font-semibold text-white border-solid border-black border-2 mr-1 ml-1 mb-2 mt-2 px-4 py-4 bg-[#bb252b]">Sizergh Castle</div> {/*End of news header block*/}
         
         <div className="text-center pr-3 pl-3 md:pr-1 md:pl-1 font-normal text-white dark:text-white border-solid border-black border-2 mr-1 ml-1 mb-2 mt-2 px-4 py-4 bg-[#bb252b]">
@@ -37,7 +36,7 @@ export default function December2025() {
            <div className="mb-20"> 
       <div className="flex flex-col max-w-full leading-1.5 sm:p-2 md:p-2 lg:p-2 m-4 border-gray-800 border-solid border-2 bg-[#165b33]">
          <div className="text-md md:text-lg max-w-full font-semibold text-white text-center dark:text-white justify-center items-center">
-        <div className="font-semibold py-2 px-12 text-white text-center dark:text-white"> 7th November 2025</div> {/*End of news date block*/} 
+        <div className="font-semibold py-2 px-12 text-white text-center dark:text-white"> 7th December 2025</div> {/*End of news date block*/} 
         <div className="font-semibold text-white border-solid border-black border-2 mr-1 ml-1 mb-2 mt-2 px-4 py-4 bg-[#bb252b]">Visit</div> {/*End of news header block*/}
         
         <div className="text-left pr-3 pl-3 md:pr-3 md:pl-3 font-normal text-white dark:text-white border-solid border-black border-2 mr-1 ml-1 mb-2 mt-2 px-4 py-4 bg-[#bb252b]">
@@ -50,7 +49,7 @@ export default function December2025() {
                      <div className="mb-20"> 
       <div className="flex flex-col max-w-full leading-1.5 sm:p-2 md:p-2 lg:p-2 m-4 border-gray-800 border-solid border-2 bg-[#165b33]">
          <div className="text-md md:text-lg max-w-full font-semibold text-white text-center dark:text-white justify-center items-center">
-        <div className="font-semibold py-2 px-12 text-white text-center dark:text-white"> 12th November 2025, Wednesday.</div> {/*End of news date block*/} 
+        <div className="font-semibold py-2 px-12 text-white text-center dark:text-white"> 12th December 2025, Wednesday.</div> {/*End of news date block*/} 
         <div className="font-semibold text-white border-solid border-black border-2 mr-1 ml-1 mb-2 mt-2 px-4 py-4 bg-[#bb252b]">Glasgow City Break Day 1:</div> {/*End of news header block*/}
         
         <div className="text-left pr-3 pl-3 md:pr-3 md:pl-3 font-normal text-white dark:text-white border-solid border-black border-2 mr-1 ml-1 mb-2 mt-2 px-4 py-4 bg-[#bb252b]">
@@ -64,7 +63,7 @@ export default function December2025() {
           <div className="mb-20"> 
       <div className="flex flex-col max-w-full leading-1.5 sm:p-2 md:p-2 lg:p-2 m-4 border-gray-800 border-solid border-2 bg-[#165b33]">
          <div className="text-md md:text-lg max-w-full font-semibold text-white text-center dark:text-white justify-center items-center">
-        <div className="font-semibold py-2 px-12 text-white text-center dark:text-white"> 13th November 2025, Thursday.</div> {/*End of news date block*/} 
+        <div className="font-semibold py-2 px-12 text-white text-center dark:text-white"> 13th December 2025, Thursday.</div> {/*End of news date block*/} 
         <div className="font-semibold text-white border-solid border-black border-2 mr-1 ml-1 mb-2 mt-2 px-4 py-4 bg-[#bb252b]">Glasgow City Break Day 2: </div> {/*End of news header block*/}
         
         <div className="text-left pr-3 pl-3 md:pr-3 md:pl-3 font-normal text-white dark:text-white border-solid border-black border-2 mr-1 ml-1 mb-2 mt-2 px-4 py-4 bg-[#bb252b]">
@@ -76,7 +75,7 @@ export default function December2025() {
      <div className="mb-20"> 
       <div className="flex flex-col max-w-full leading-1.5 sm:p-2 md:p-2 lg:p-2 m-4 border-gray-800 border-solid border-2 bg-[#165b33]">
          <div className="text-md md:text-lg max-w-full font-semibold text-white text-center dark:text-white justify-center items-center">
-        <div className="font-semibold py-2 px-12 text-white text-center dark:text-white"> 14th November 2025, Friday.</div> {/*End of news date block*/} 
+        <div className="font-semibold py-2 px-12 text-white text-center dark:text-white"> 14th December 2025, Friday.</div> {/*End of news date block*/} 
         <div className="font-semibold text-white border-solid border-black border-2 mr-1 ml-1 mb-2 mt-2 px-4 py-4 bg-[#bb252b]">Glasgow City Break Day 3: </div> {/*End of news header block*/}
         
         <div className="text-left pr-3 pl-3 md:pr-3 md:pl-3 font-normal text-white dark:text-white border-solid border-black border-2 mr-1 ml-1 mb-2 mt-2 px-4 py-4 bg-[#bb252b]">
