0082fd088ecd5e00af88adc90efb37c4f1fe0790
Jill <125391549+JillThomas799@users.noreply.github.com>
2025-12-19 15:30:04 +0000

    Addition 12th and 13th December 2025 blogs

--- src/components/ui/December2025.tsx
+++ src/components/ui/December2025.tsx
@@ -151,7 +151,84 @@ export default function December2025() {
       </div>{/*End of individual dated entry photo album block*/} 
       </div>           {/*  End of individual dated entry design and color template block */}
            </div> {/*End of individual dated entry block*/}</div>    
-    
+
+
+  <div className="mb-20"> 
+      <div className="flex flex-col max-w-full leading-1.5 sm:p-2 md:p-2 lg:p-2 m-4 border-gray-800 border-solid border-2 bg-[#165b33]">
+         <div className="text-md md:text-lg max-w-full font-semibold text-white text-center dark:text-white justify-center items-center">
+        <div className="font-semibold py-2 px-12 text-white text-center dark:text-white"> 12th December 2025, Wednesday</div> {/*End of news date block*/} 
+        <div className="font-semibold text-white border-solid border-black border-2 mr-1 ml-1 mb-2 mt-2 px-4 py-4 bg-[#bb252b]">London, Cambridge and the South East Short Break and Christmas Gift Dispatches: Day 1 - Arrival in the South East to see relatives.</div> {/*End of news header block*/}
+        
+         <div className="flex flex-col border-solid border-2 border-[#22311d] bg-[#bb252b] pb-2 pt-2 mr-[2%] ml-[2%] mt-5 mb-4  justify-center text-center">
+            <div className="mt-1 mb-1 sm:m-2 text-md font-normal block text-white bg-[#bb252b]">  It was that time of year again and so we packed our bags and set off for the South East and South of England for a pre-Christmas visit to our relatives in those distant and far-off lands (both geographically and culturally)...  Harry was back home working and had been put in charge of looking after Lennie.....
+</div>
+</div>
+</div>
+</div>
+</div>
+
+
+  <div className="mb-20"> 
+      <div className="flex flex-col max-w-full leading-1.5 sm:p-2 md:p-2 lg:p-2 m-4 border-gray-800 border-solid border-2 bg-[#165b33]">
+         <div className="text-md md:text-lg max-w-full font-semibold text-white text-center dark:text-white justify-center items-center">
+        <div className="font-semibold py-2 px-12 text-white text-center dark:text-white"> 13th December 2025, Sunday</div> {/*End of news date block*/} 
+        <div className="font-semibold text-white border-solid border-black border-2 mr-1 ml-1 mb-2 mt-2 px-4 py-4 bg-[#bb252b]">London, Cambridge and the South East Short Break and Christmas Gift Dispatches: Day 2 - Cambridge for the day, exploring the history of computing and a ride on the Park and Ride bus service... </div> {/*End of news header block*/}
+        
+         <div className="flex flex-col border-solid border-2 border-[#22311d] bg-[#bb252b] pb-2 pt-2 pr-3 pl-3 mr-[2%] ml-[2%] mt-5 mb-4  justify-left text-left">Today, it was off to Cambridge for the day. There are many museums in Cambridge but unfortunately many are not open on a Sunday. The Fitzwilliam Museum was an obvious contender but decided, in view of a British Museum visit planned for Monday, to instead head to the slightly more niche <Link href="https://www.computinghistory.org.uk/" target="_blank" className="underline decoration-black text-yellow-400 font-semibold">Centre for Computing History</Link>.
+            <div className="mt-1 mb-1 sm:m-2 text-md font-normal block text-white bg-[#bb252b]">  
+</div>
+</div>
+</div>
+</div>
+</div>
+
+
+
+  <div className="mb-20"> 
+      <div className="flex flex-col max-w-full leading-1.5 sm:p-2 md:p-2 lg:p-2 m-4 border-gray-800 border-solid border-2 bg-[#165b33]">
+         <div className="text-md md:text-lg max-w-full font-semibold text-white text-center dark:text-white justify-center items-center">
+        <div className="font-semibold py-2 px-12 text-white text-center dark:text-white"> 14th December 2025, Monday</div> {/*End of news date block*/} 
+        <div className="font-semibold text-white border-solid border-black border-2 mr-1 ml-1 mb-2 mt-2 px-4 py-4 bg-[#bb252b]">London, Cambridge and the South East Short Break and Christmas Gift Dispatches: Day 3 - Day in London with Bernard's brother to catch up on news and see the Iranian and Islamic galleries at the British Museum.</div> {/*End of news header block*/}
+        
+         <div className="flex flex-col border-solid border-2 border-[#22311d] bg-[#bb252b] pb-2 pt-2 mr-[2%] ml-[2%] mt-5 mb-4  justify-center text-center">
+            <div className="mt-1 mb-1 sm:m-2 text-md font-normal block text-white bg-[#bb252b]">  
+</div>
+</div>
+</div>
+</div>
+</div>
+
+
+  <div className="mb-20"> 
+      <div className="flex flex-col max-w-full leading-1.5 sm:p-2 md:p-2 lg:p-2 m-4 border-gray-800 border-solid border-2 bg-[#165b33]">
+         <div className="text-md md:text-lg max-w-full font-semibold text-white text-center dark:text-white justify-center items-center">
+        <div className="font-semibold py-2 px-12 text-white text-center dark:text-white"> 15th December 2025, Tuesday</div> {/*End of news date block*/} 
+        <div className="font-semibold text-white border-solid border-black border-2 mr-1 ml-1 mb-2 mt-2 px-4 py-4 bg-[#bb252b]">London, Cambridge and the South East Short Break and Christmas Gift Dispatches: Day 4 - Christmas lunch at RHS Hyde Hall.</div> {/*End of news header block*/}
+        
+         <div className="flex flex-col border-solid border-2 border-[#22311d] bg-[#bb252b] pb-2 pt-2 mr-[2%] ml-[2%] mt-5 mb-4  justify-center text-center">
+            <div className="mt-1 mb-1 sm:m-2 text-md font-normal block text-white bg-[#bb252b]">  
+</div>
+</div>
+</div>
+</div>
+</div>
+
+
+  <div className="mb-20"> 
+      <div className="flex flex-col max-w-full leading-1.5 sm:p-2 md:p-2 lg:p-2 m-4 border-gray-800 border-solid border-2 bg-[#165b33]">
+         <div className="text-md md:text-lg max-w-full font-semibold text-white text-center dark:text-white justify-center items-center">
+        <div className="font-semibold py-2 px-12 text-white text-center dark:text-white"> 16th December 2025, Wednesday</div> {/*End of news date block*/} 
+        <div className="font-semibold text-white border-solid border-black border-2 mr-1 ml-1 mb-2 mt-2 px-4 py-4 bg-[#bb252b]">London, Cambridge and the South East Short Break and Christmas Gift Dispatches: Day 5 - A detour to see Bernard's dad and then the long journey home to the wet, windy but very beloved (and quiet) North.</div> {/*End of news header block*/}
+        
+         <div className="flex flex-col border-solid border-2 border-[#22311d] bg-[#bb252b] pb-2 pt-2 mr-[2%] ml-[2%] mt-5 mb-4  justify-center text-center">
+            <div className="mt-1 mb-1 sm:m-2 text-md font-normal block text-white bg-[#bb252b]">  
+</div>
+</div>
+</div>
+</div>
+</div>
+
+
 
      </div>
   );
