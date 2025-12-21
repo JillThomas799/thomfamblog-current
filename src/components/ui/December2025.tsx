90d1958215e845bff4da2af856b44372b0655691
Jill <125391549+JillThomas799@users.noreply.github.com>
2025-12-21 19:07:54 +0000

    Edits to December blog entries

--- src/components/ui/December2025.tsx
+++ src/components/ui/December2025.tsx
@@ -157,7 +157,7 @@ export default function December2025() {
   <div className="mb-20"> 
       <div className="flex flex-col max-w-full leading-1.5 sm:p-2 md:p-2 lg:p-2 m-4 border-gray-800 border-solid border-2 bg-[#165b33]">
          <div className="text-md md:text-lg max-w-full font-semibold text-white text-center dark:text-white justify-center items-center">
-        <div className="font-semibold py-2 px-12 text-white text-center dark:text-white"> 12th December 2025, Wednesday</div> {/*End of news date block*/} 
+        <div className="font-semibold py-2 px-12 text-white text-center dark:text-white"> 13th December 2025, Saturday</div> {/*End of news date block*/} 
         <div className="font-semibold text-white border-solid border-black border-2 mr-1 ml-1 mb-2 mt-2 px-4 py-4 bg-[#bb252b]">London, Cambridge and the South East Short Break and Christmas Gift Dispatches: Day 1 - Arrival in the South East to see relatives.</div> {/*End of news header block*/}
         
          <div className="flex flex-col border-solid border-2 border-[#22311d] bg-[#bb252b] pb-2 pt-2 pr-4 pl-4 mr-[2%] ml-[2%] mt-5 mb-4  justify-center text-center">
@@ -172,7 +172,7 @@ export default function December2025() {
   <div className="mb-20"> 
       <div className="flex flex-col max-w-full leading-1.5 sm:p-2 md:p-2 lg:p-2 m-4 border-gray-800 border-solid border-2 bg-[#165b33]">
          <div className="text-md md:text-lg max-w-full font-semibold text-white text-center dark:text-white justify-center items-center">
-        <div className="font-semibold py-2 px-12 text-white text-center dark:text-white"> 13th December 2025, Sunday</div> {/*End of news date block*/} 
+        <div className="font-semibold py-2 px-12 text-white text-center dark:text-white"> 14th December 2025, Sunday</div> {/*End of news date block*/} 
         <div className="font-semibold text-white border-solid border-black border-2 mr-1 ml-1 mb-2 mt-2 px-4 py-4 bg-[#bb252b]">London, Cambridge and the South East Short Break and Christmas Gift Dispatches: Day 2 - Cambridge for the day, exploring the history of computing and a ride on the Park and Ride bus service... </div> {/*End of news header block*/}
         
          <div className="flex flex-col border-solid border-2 border-[#22311d] bg-[#bb252b] pb-2 pt-2 pr-3 pl-3 mr-[2%] ml-[2%] mt-5 mb-4  justify-center text-center"><p>Today, we were off to Cambridge for the day. There are many museums in Cambridge but unfortunately not all are open on a Sunday. The Fitzwilliam Museum was an obvious contender but we decided, in view of a forthcoming British Museum visit planned for Monday, to instead head to the slightly more niche <Link href="https://www.computinghistory.org.uk/" target="_blank" className="underline decoration-black text-yellow-400 font-semibold">Centre for Computing History.</Link> The link contains lots of information about the work of the museum, its activities, projects and future events. It is sited tucked away on a commercial park, so if visiting, we would advise taking careful note of the 'How to find us' instructions. It is a few miles out of the centre.</p>
