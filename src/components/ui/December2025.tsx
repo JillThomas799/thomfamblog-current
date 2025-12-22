8395d6f47a07e69ca36c89e0160dcad611bce0bf
Jill <125391549+JillThomas799@users.noreply.github.com>
2025-12-22 09:11:17 +0000

    Edits to 15th December 2025 blog

--- src/components/ui/December2025.tsx
+++ src/components/ui/December2025.tsx
@@ -212,7 +212,7 @@ export default function December2025() {
   <div className="mb-20"> 
       <div className="flex flex-col max-w-full leading-1.5 sm:p-2 md:p-2 lg:p-2 m-4 border-gray-800 border-solid border-2 bg-[#165b33]">
          <div className="text-md md:text-lg max-w-full font-semibold text-white text-center dark:text-white justify-center items-center">
-        <div className="font-semibold py-2 px-12 text-white text-center dark:text-white"> 14th December 2025, Monday</div> {/*End of news date block*/} 
+        <div className="font-semibold py-2 px-12 text-white text-center dark:text-white"> 15th December 2025, Monday</div> {/*End of news date block*/} 
         <div className="font-semibold text-white border-solid border-black border-2 mr-1 ml-1 mb-2 mt-2 px-4 py-4 bg-[#bb252b]">London, Cambridge and the South East Short Break and Christmas Gift Dispatches: Day 3 - Day in London with Bernard's brother to catch up on news and to see the Rahim Irvani Gallery of Ancient Iran - Room 52 and the Albukhary Gallery of the Islamic World - Rooms 42 and 43, at the British Museum.</div> {/*End of news header block*/}
         
          <div className="flex flex-col border-solid border-2 border-[#22311d] bg-[#bb252b] pb-2 pt-2 mr-[2%] ml-[2%] mt-5 mb-4  justify-center text-center">
