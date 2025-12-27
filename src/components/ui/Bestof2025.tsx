98d9312127f62731085aaa208a7686633c1f9b6c
Jill <125391549+JillThomas799@users.noreply.github.com>
2025-12-27 19:25:11 +0000

    Work on Best of 2025 blog

--- src/components/ui/Bestof2025.tsx
+++ src/components/ui/Bestof2025.tsx
@@ -1,47 +1,139 @@
-
+import Image from 'next/image'
+import Link from 'next/link'
+import SuttonHoo from '/public/imagesresized/SuttonHoo.jpg';
 
 export default function Bestof2025() {
   return ( 
     <div className="bg-black h-[100%]">
-        <div className="text-2xl  text-center text-white font-bold pt-10 pb-10 animate-pulse">THE BEST OF 2025</div>
+        <div className="text-2xl  text-center text-[#FDDC5C] font-bold pt-10 pb-8 mb-8 border-[#bb252b] border-4 rounded-md bg-[#165b33] animate-pulse">THE BEST OF 2025</div>
         <div className="text-white text-center font-semibold text-2xl p-3 bg-[#bb252b] border-solid border-2 border-[#165b33] m-2">
-            Holidays:
+            <div className="border-black border-4 rounded-md text-white mr-[30%] ml-[30%] pb-2 pt-2 bg-[#165b33]">Holidays - Highlights</div>
+            <div className="text-wrap">
+              <br />
+              Holidays visiting Family in South East: 
+             <div className="grid grid-cols-3 gap-4 bg-black mt-8 pt-8 pr-8">Sutton Hoo  
+                <Image
+                  src={ SuttonHoo }
+                  className=" w-[50%] md:w-[100%] lg:w-[100%] border-solid border-black-400 border-2 mt-1 mb-11 rounded-[30%] transform transition hover:scale-150"
+                  alt="Photo of recreation of boat skeleton Sutton Hoo" />
+                  <Image
+                  src={ SuttonHoo }
+                  className="h-auto w-[100%] md:w-[100%] lg:w-[100%] border-solid border-black-400 border-2 mt-1 mb-11 rounded-[30%] transform transition hover:scale-150"
+                  alt="Photo of recreation of boat skeleton Sutton Hoo" />
+                  </div> 
+              <br />
+                <div className="grid grid-cols-3 gap-4 bg-black mt-8 pt-8 pl-4 pr-8">Glynebourne Opera to see Verdi's Falstaff
+                <Image
+                  src={ SuttonHoo }
+                  className=" w-[50%] md:w-[100%] lg:w-[100%] border-solid border-black-400 border-2 mt-1 mb-11 rounded-[30%] transform transition hover:scale-150"
+                  alt="Photo of recreation of boat skeleton Sutton Hoo" />
+                  <Image
+                  src={ SuttonHoo }
+                  className="h-auto w-[100%] md:w-[100%] lg:w-[100%] border-solid border-black-400 border-2 mt-1 mb-11 rounded-[30%] transform transition hover:scale-150"
+                  alt="Photo of recreation of boat skeleton Sutton Hoo" />
+                  </div> 
+              <br />
+              <br />
+              Northumberland Coast
+             <div className="grid grid-cols-3 gap-4 bg-black mt-8 pt-8 pl-4 pr-8">Bamburgh Castle, Northumbria Coast and Seahouses 
+                <Image
+                  src={ SuttonHoo }
+                  className=" w-[50%] md:w-[100%] lg:w-[100%] border-solid border-black-400 border-2 mt-1 mb-11 rounded-[30%] transform transition hover:scale-150"
+                  alt="Photo of recreation of boat skeleton Sutton Hoo" />
+                  <Image
+                  src={ SuttonHoo }
+                  className="h-auto w-[100%] md:w-[100%] lg:w-[100%] border-solid border-black-400 border-2 mt-1 mb-11 rounded-[30%] transform transition hover:scale-150"
+                  alt="Photo of recreation of boat skeleton Sutton Hoo" />
+                  </div> 
+              <br />
+              <br />
+            Portsmouth: 
+             <br />
+              <div className="grid grid-cols-3 gap-4 bg-black mt-8 pt-8 pr-8">Portsmouth Historic Dockyard 
+                <Image
+                  src={ SuttonHoo }
+                  className=" w-[50%] md:w-[100%] lg:w-[100%] border-solid border-black-400 border-2 mt-1 mb-11 rounded-[30%] transform transition hover:scale-150"
+                  alt="Photo of recreation of boat skeleton Sutton Hoo" />
+                  <Image
+                  src={ SuttonHoo }
+                  className="h-auto w-[100%] md:w-[100%] lg:w-[100%] border-solid border-black-400 border-2 mt-1 mb-11 rounded-[30%] transform transition hover:scale-150"
+                  alt="Photo of recreation of boat skeleton Sutton Hoo" />
+                  </div> 
+             <br />
+               <div className="grid grid-cols-3 gap-4 bg-black mt-8 pt-8 pr-8">Winchester Cathedral 
+                <Image
+                  src={ SuttonHoo }
+                  className=" w-[50%] md:w-[100%] lg:w-[100%] border-solid border-black-400 border-2 mt-1 mb-11 rounded-[30%] transform transition hover:scale-150"
+                  alt="Photo of recreation of boat skeleton Sutton Hoo" />
+                  <Image
+                  src={ SuttonHoo }
+                  className="h-auto w-[100%] md:w-[100%] lg:w-[100%] border-solid border-black-400 border-2 mt-1 mb-11 rounded-[30%] transform transition hover:scale-150"
+                  alt="Photo of recreation of boat skeleton Sutton Hoo" />
+                  </div> 
+             <br />
+               <div className="grid grid-cols-3 gap-4 bg-black mt-8 pt-8 pr-8">Stonehenge 
+                <Image
+                  src={ SuttonHoo }
+                  className=" w-[50%] md:w-[100%] lg:w-[100%] border-solid border-black-400 border-2 mt-1 mb-11 rounded-[30%] transform transition hover:scale-150"
+                  alt="Photo of recreation of boat skeleton Sutton Hoo" />
+                  <Image
+                  src={ SuttonHoo }
+                  className="h-auto w-[100%] md:w-[100%] lg:w-[100%] border-solid border-black-400 border-2 mt-1 mb-11 rounded-[30%] transform transition hover:scale-150"
+                  alt="Photo of recreation of boat skeleton Sutton Hoo" />
+                  </div> 
+              <br />
+              <br />
+            Glasgow: 
             <br />
-            Northumberland Coast.
-              <br />
-            Portsmouth 
-              <br />
-            Glasgow
+            'Hamilton' live, 
+            <br />
+            'Rest is Politics' live at SEC arena, 
+            <br />
+            Riverside Transport museum.
+            <br />
+            <br />
+            </div>
         </div>
 <br />
 <br />
-        <div className="text-white text-center font-semibold text-2xl p-3 bg-[#bb252b] border-solid border-2 border-[#165b33] m-2">
-            History:
-            <br />
+        <div className="text-white text-center font-semibold text-2xl pt-3 pb-4 bg-[#bb252b] border-solid border-2 border-[#165b33] m-2">
+            <div className="border-black border-4 rounded-md text-white mr-[30%] ml-[30%] pb-4 pt-2 bg-[#165b33]">History</div>
+            <div>
             Winchester Abbey and Westminster Abbey.
               <br />
             Stonehenge.
               <br />
             Lindisfarne/Holy Island
+            </div>
         </div>
 
         <br />
 <br />
-        <div className="text-white text-center font-semibold text-2xl p-3 bg-[#bb252b] border-solid border-2 border-[#165b33] m-2">
-            Achievements:
-            <br />
-            Bernard: Learning Spanish and French. Learning how to fly an airbus.
+        <div className="text-white text-center font-semibold text-2xl pt-3 pb-4 bg-[#bb252b] border-solid border-2 border-[#165b33] m-2">
+            <div className="border-black border-4 rounded-md text-white mr-[30%] ml-[30%] pb-4 pt-2 bg-[#165b33]">Achievements</div>
+            <div>
+            Bernard: 
+             <br />
+            Learning Spanish and French. 
+             <br />
+             Learning how to fly an airbus.
               <br />
-            Jill: Learning how to read early modern Scottish palaeography, software and new blog.
+               <br />
+            Jill: Learning how to read early modern Scottish palaeography, 
+             <br />
+             software and new blog.
             <br />
-            Harry: Obtaining employment as a Digital and Technology Solutions professional with one of the local councils. 
+             <br />
+            Harry: Graduating with his PhD,  <br />
+            Obtaining employment as a Digital and Technology Solutions professional with one of the local councils. 
+            </div>
         </div>
 
         <br />
 <br />
-        <div className="text-white text-left font-semibold text-2xl p-3 bg-[#bb252b] border-solid border-2 border-[#165b33] m-2">
-            Events:
-            <br />
+        <div className="text-white text-center font-semibold text-2xl pt-3 pb-4 bg-[#bb252b] border-solid border-2 border-[#165b33] m-2">
+            <div className="border-black border-4 rounded-md text-white mr-[30%] ml-[30%] pb-4 pt-2 bg-[#165b33]">Events</div>
+            <div>
             Harry's PhD Graduation in Cardiff.
               <br />
             Elena's partner comes to live in Cumbria.
@@ -53,7 +145,7 @@ export default function Bestof2025() {
            Rest is Politics Live show at the SEC arena Glasgow.
              <br />
              Bernard's magical mystery tour Father's day trip to Flight Experience, Newcastle
-
+             </div>
         </div>
     </div>
   );
