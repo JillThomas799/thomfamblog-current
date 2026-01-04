e5588839c0b452292ba68d498c71cdaefc2cf08e
Jill <125391549+JillThomas799@users.noreply.github.com>
2026-01-04 18:54:14 +0000

    Work on January 2026 blog

--- src/components/ui/January2026.tsx
+++ src/components/ui/January2026.tsx
@@ -5,21 +5,10 @@ import Link from 'next/link'
 
 export default function January2026() {
   return (   
- <div className=" bg-black border-solid border-4 border- border-[#165b33]  mb-28">
-    <div>
+ <div className=" bg-black border-solid border-4 border- border-[#165b33]  mb-28">     
        <div className="text-white text-center font-semibold text-2xl p-3 bg-[#bb252b] border-solid border-2 border-[#165b33] m-2">January 2026 </div> 
-        <div className="block relative mt-[70%] mb-[135%] md:mt-[50%] md:scale-[65%]  md:mb-[100%] lg:scale-80 text-center 
-        text-[1.75em] font-bold width-full height-full ">
-            </div>   
-              
-        <div className="mb-20"> 
-      <div className="flex flex-col max-w-full leading-1.5 pr-3 pl-3 sm:p-2 md:p-2 lg:p-2 m-4 border-gray-800 border-solid border-2 bg-[#165b33] ">
-         <div className="text-md md:text-lg max-w-full font-normal text-white text-center dark:text-white justify-center items-center">
-        < div className="text-center text-white font-normal border-black border-solid border-2 mr-1 ml-1 mb-4 mt-4 px-4 py-4 bg-[#bb252b]">  </div>
-      </div>
-      </div>
-           </div> 
-           </div>  
-           </div> 
-           );
-           }
+  
+</div>
+
+  );
+}
