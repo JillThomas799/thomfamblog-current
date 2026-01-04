21de7eb4772229cc8665c99679f82b35410dcfcc
Jill <125391549+JillThomas799@users.noreply.github.com>
2026-01-04 17:25:29 +0000

    Work on January 2026 blog

---
+++ src/components/ui/January2026.tsx
@@ -0,0 +1,10 @@
+
+
+
+export default function January2026(){ return
+    (
+    <div>
+        <div className="bg-black h-[25%] w-full border-4 border-white"></div>
+    </div>
+);
+}
--- src/components/ui/blogmenubar.tsx
+++ src/components/ui/blogmenubar.tsx
@@ -49,6 +49,9 @@ const BlogMenuBar=()    => {
     <div className="text-center h-15 bg-gray-800 border-b border-gray-700 font-semibold p-4 hover:text-blue-300">
       <Link href="/dashboard/months/bestof2025" >Best of 2025</Link>
     </div>
+    <div className="text-center h-15 bg-gray-800 border-b border-gray-700 font-semibold p-4 hover:text-blue-300">
+      <Link href="/dashboard/months/January2026" >January2026</Link>
+    </div>
     
     </div>
 )}
