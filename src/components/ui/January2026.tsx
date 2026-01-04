9e5afc488217d29e0a8beec3681258a257e0dd51
Jill <125391549+JillThomas799@users.noreply.github.com>
2026-01-04 19:07:07 +0000

    Work on January 2026 blog

--- src/components/ui/January2026.tsx
+++ src/components/ui/January2026.tsx
@@ -5,8 +5,8 @@ import Link from 'next/link'
 
 export default function January2026() {
   return (   
- <div className=" bg-black border-solid border-4 border- border-[#165b33]  mb-28">     
-       <div className="text-white text-center font-semibold text-2xl p-3 bg-[#bb252b] border-solid border-2 border-[#165b33] m-2">January 2026 </div> 
+ <div className=" bg-black pb-[70vh] border-solid border-4 border- border-gray-600  mb-28">     
+       <div className="text-white text-center font-semibold text-2xl p-3 bg-gray-500 m-2">January 2026 </div> 
   
 </div>
 
