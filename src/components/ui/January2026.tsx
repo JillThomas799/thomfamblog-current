ab57d9f2305f0b4379b61f073da1028a42fcaf2e
Jill <125391549+JillThomas799@users.noreply.github.com>
2026-01-05 18:48:50 +0000

    Work on January2026 blog

--- src/components/ui/January2026.tsx
+++ src/components/ui/January2026.tsx
@@ -1,12 +1,19 @@
 import Image from 'next/image'
 import Link from 'next/link'
+import WinterRobin from "/public/imagesresized/WinterRobin.jpg";
 
 
 
 export default function January2026() {
   return (   
- <div className=" bg-black pb-[70vh] border-solid border-4 border- border-gray-600  mb-28">     
-       <div className="text-white text-center font-semibold text-2xl p-3 bg-gray-500 m-2">January 2026 </div> 
+ <div className=" bg-gray-500 h-[100vh] w-full pb-[70vh] border-solid border-4 border- border-gray-600  mb-28">     
+       <div className="text-white text-center font-semibold text-2xl p-3 bg-gray-500 m-2">January 2026
+         <Image 
+        src={ WinterRobin } 
+         className="block ml-auto mr-auto h-[auto] w-[100%] md:w-[90%] border-solid border-black-400 border-4 mt-6 mb-11"
+                  alt="Photo of Winter Robin"
+                  />
+        </div> 
   
 </div>
 
