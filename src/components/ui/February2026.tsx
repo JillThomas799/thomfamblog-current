e564faca6510b93dc35d0533ffbf8d15562b5672
Jill <125391549+JillThomas799@users.noreply.github.com>
2026-02-16 19:19:05 +0000

    14th February 2026 Gladiators and Uncovering Roman Carlisle

--- src/components/ui/February2026.tsx
+++ src/components/ui/February2026.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link'
 import "./February2026.css"; 
 import Deer from "./deer";
 import GawainArrivalTitled from "/public/imagesresized/GawainArrivalTitled.jpg";
+import TullieGladiatorsTitled from "/public/imagesresized/TullieGladiatorsTitled.jpg";
 
 
 export default function February2026() {
@@ -67,6 +68,62 @@ export default function February2026() {
             </div>
           </div>
 
+
+
+
+           <div className="mb-20">
+          <div className="flex flex-col max-w-full leading-1.5 p-1 md:p-2 lg:p-4 m-4 border-gray-800 border-solid border-2 bg-[#bdbeba]">
+            <div className="text-md md:text-lg max-w-full font-normal mt-4 mb-4 p-2 text-white text-center md:text-left justify-center items-center bg-[#786d68]">
+
+              <div className="font-semibold pt-2 pb-2 mr-[30%] ml-[30%]  text-[#f3e2d6] text-center  border-gray-800 border-solid border-2 bg-[#5d5450]"> 14th February 2026</div> {/*End of news date block*/}
+              <div className="font-bold m-4 pt-2 pb-4 py-2 md:px-4 text-[#f3e2d6] text-center  border-gray-800 border-solid border-2 bg-[#5d5450]"> Tullie Museum, Carlisle: Gladiator and Roman Carlisle Exhibition</div>
+              <div className="text-[#f3e2d6] text-center md:text-left mb-4 md:*:pr-6 md:*:pl-6 ">
+                From the 7th February until 31st May 2026, Tullie Museum, Carlisle is hosting the British Museum Partnership Exhibition 'Gladiators of Britain' together with Colchester and Ipswich Museums and 'Uncovering Roman Carlisle: Where Worlds Meet'. The 'Gladiators of Britain' exhibition explores the lives and deaths of those who fought in Britain's arenas through extraordinary objects from across the country and beyond. The 'Uncovering Roman Carlisle: Where World's Met' exhibition brings the latest chapter in the going exploration of Roman Carlisle.
+                So where better for us to be on a cold February day....?
+                <br />
+                <br />
+                In May 2017, the project <Link  href="https://www.uncoveringromancarlisle.co.uk/"                   target="_blank" className="text-decoration-underline text-white font-semibold underline">'Uncovering Roman Carlisle'</Link> came into being when Carlisle Cricket club, situated close to the River Eden and prone to the troublesome flooding which has periodically occurred in this and other susceptible parts of the city, commissioned a team of professional archeologists from Wardell Armstrong (now SLR) to undertake an evaluation for a new Cricket Pavilion, and discovered a Roman Bathhouse, now the largest known building on Hadrian’s Wall, together with many other significant finds. 
+                <br />
+                <br />
+                Returning to the site every year since 2021, Uncovering Roman Carlisle has brought hundreds of volunteers, The Diggers, to further explore the site, study and conserve the finds, and to exhibit the discoveries to the world.
+                <br />
+                <br />
+                Uncovering Roman Carlisle has been an ongoing project since 2017 with much potentially still to find. Here are some interesting links about the project. 
+                <br />
+                <br />
+                <ul className='marker:text-green list-outside list-disc ml-6'>
+                   <li><Link  href="https://www.youtube.com/watch?v=tgbGGycv0_8" target="_blank" className="text-decoration-underline text-white font-semibold underline">Film 2023 about the Bath House Excavations, Uncovering Roman Carlisle </Link></li>
+                   <li><Link  href="https://www.cumberland.gov.uk/news/2024/rare-roman-discovery-found-carlisle" target="_blank" className="text-decoration-underline text-white font-semibold underline">Cumberland Council Article 3rd May 2024: 'Rare Roman discovery found in Carlisle'</Link></li>
+                 <li><Link  href="https://www.cumberland.gov.uk/news/2024/new-archaeological-find-uncovered-carlisle-dig" target="_blank" className="text-decoration-underline text-white font-semibold underline">Cumberland Council Article 19th September 2024: 'New archaeological find uncovered at Carlisle dig'</Link></li>
+                 <li><Link  href="https://www.youtube.com/watch?v=Zq8es5osM1c" target="_blank" className="text-decoration-underline text-white font-semibold underline">Film February 2025 about the Bath House Excavations, Uncovering Roman Carlisle </Link></li>
+                <li><Link  href="https://www.bbc.co.uk/news/articles/c93y0ll0n9eo" target="_blank" className="text-decoration-underline text-white font-semibold underline list-disc">BBC article 13th June 2025: Roman column dig discovery 'tip of the iceberg'</Link> </li>
+                </ul>
+                 <br />
+                 If anyone is interested in volunteering with the project, the details of how to go about expressing interest are included on the <Link href="https://www.uncoveringromancarlisle.co.uk/" target="_blank" className="text-decoration-underline text-white font-semibold underline">'Uncovering Roman Carlisle'</Link> site.
+                  <br />
+                 <br />
+                 
+                 <div className="m-1 pr-2 pl-2 pt-4 pb-4 font-semibold text-center border-gray-800 border-solid border-2 bg-[#5d5450] mb-6">Click on the image below to open the album <br /> of our visit to the Gladiators and Roman Carlisle Exhibition at the Tullie Museum, Carlisle.
+                <br />
+                <br />
+                      Please make sure to click on the <em>'i'</em> icon, in the top right-hand corner of each photo, as we have included a lot of additional information about the photo in that space.
+                   </div>
+                  <Link
+                    href="https://photos.app.goo.gl/4mXrnPR7wkTA8M55A"
+                    target="_blank"
+                  >
+                    <Image
+                      src={TullieGladiatorsTitled}
+                      className="block ml-auto mr-auto h-auto w-[90%] md:w-[70%] lg:w-[50%] border-solid border-black-400 border-4 mt-4 mb-6"
+                      alt="Photo of Gawain day 2."
+                    />
+                  </Link>
+                 
+              </div>
+              
+  </div>
+            </div>
+          </div>
 
         </div>{" "}
 
