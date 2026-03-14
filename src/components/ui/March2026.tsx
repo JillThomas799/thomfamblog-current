8223474a916cb6ffb0476001ea24baf3fdae16fd
Jill <125391549+JillThomas799@users.noreply.github.com>
2026-03-14 16:02:57 +0000

    6th March to 10th March 2026 blog

--- src/components/ui/March2026.tsx
+++ src/components/ui/March2026.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import SamuraiArmourTitled from "/public/imagesresized/SamuraiArmourTitled.jpg";
+import WaterParkTitled from "/public/imagesresized/WaterParkTitled.jpg";
 
 export default function March2026() {
   return (
@@ -27,17 +28,68 @@ export default function March2026() {
          </div> 
           </div> 
 
-           < div className="mb-20"> 
+          < div className="mb-20"> 
       < div className="flex flex-col w-auto leading-1.5 p-2 md:p-4 lg:p-4  mb-4 mt-4 border-[#efca60] border-solid border-2 md:border-2 bg-[#f4f498]">
          < div className="text-md md:text-base max-w-full font-semibold text-[#2b8936] text-center dark:text-[#2b8936] justify-center items-center">
-         <div className="font-semibold pt-2 pb-2 mr-[30%] ml-[30%]  text-[#bbe89e] text-center  border-[#bbe89e] border-2 bg-[#2b8936]"> Saturday 7th March 2026</div> {/*End of news date block*/}
-              <div className="font-bold m-4 pt-2 pb-4 py-2 md:px-4 text-[#bbe89e] text-center  border-[#bbe89e] border-2  bg-[#2b8936]"> Day in London with Nicholas, including the British Museum Samurai Exhibition and the British Library Highlights</div>
-        <div className="font-semibold text-center border-solid border-[#efca60] border-2 mr-1 ml-1 mt-2 mb-2 px-4 py-4 bg-[#bbe89e]"> 
+         <div className="font-semibold pt-2 pb-2 mr-[30%] ml-[30%]  text-[#bbe89e] text-center  border-[#bbe89e] border-2 bg-[#2b8936]"> 1st March 2026</div> {/*End of news date block*/}
+              <div className="font-bold m-4 pt-2 pb-4 py-2 md:px-4 text-[#bbe89e] text-center  border-[#bbe89e] border-2  bg-[#2b8936]"> Friday 6th March - Tuesday 10th March 2026 </div>
+               <div className="font-bold m-4 pt-2 pb-4 py-2 md:px-4 text-[#bbe89e] text-center  border-[#bbe89e] border-2  bg-[#2b8936]"> Visit to foreign lands, a.k.a the South and South East of England, to spend time with friends and relatives with a chance to visit a few museums and restaurants en route....</div>
+        </div> 
+         </div> 
+          </div> 
 
-
+                  < div className="mb-20"> 
+      < div className="flex flex-col w-auto leading-1.5 p-2 md:p-4 lg:p-4  mb-4 mt-4 border-[#efca60] border-solid border-2 md:border-2 bg-[#f4f498]">
+         < div className="text-md md:text-base max-w-full font-semibold text-[#2b8936] text-center  md:text-left dark:text-[#2b8936] justify-center items-center">
+         <div className="font-semibold pt-2 pb-2 mr-[30%] ml-[30%]  text-[#bbe89e] text-center  border-[#bbe89e] border-2 bg-[#2b8936]">Day 1: Friday 6th March 2026</div> {/*End of news date block*/}
+              <div className="font-bold m-4 pt-2 pb-4 py-2 md:px-4 text-[#bbe89e] text-center  border-[#bbe89e] border-2  bg-[#2b8936]"> Home to Bernard's dad's: A long journey....</div>
+        <div className="font-semibold text-center md:text-left border-solid border-[#efca60] border-2 mr-1 ml-1 mt-2 mb-2 px-4 py-4 bg-[#bbe89e]"> A l..o..n..g journey today of just over 300 miles, so to try and break it up a little, we took a minor detour to visit the remembrance garden where Jill's mum and dad were laid to rest and to see Jill's aunt.
+           <br />
+          <br />
+           We headed through snow over Shap but by the time we reached Lancashire it was cold, dull but settled weather. It was great to see Jill's aunt, and to catch up on news. Whilst in the area, we took the opportunity to visit what used to be the murky 'ressies' or reservoirs, to give them their proper title. It was good to see that the local council have turned them into a very pleasant water and nature park which have proved popular with the locals. Whilst the trees and plants were still relatively dormant from the winter, the ducks and geese were having a good time.
           <br />
                 <br />
-                <div className="m-1 pr-2 pl-2 pt-4 pb-4 font-semibold text-center text-[#bbe89e] border-[#efca60] border-solid border-2 bg-[#2b8936] mb-6">Click on the image below to open the album <br /> of our day in London including the Samurai exhibition at the British Museum, Treasures of the British Library and Cypriot food.
+                <div className="m-1 pr-2 pl-2 pt-4 pb-4 font-semibold text-center md:text-left text-[#bbe89e] border-[#efca60] border-solid border-2 bg-[#2b8936] mb-6">Click on the image below to open the album of our first day's journey down South.
+                <br />
+                <br />
+                      Please make sure to click on the <em>'i'</em> icon, in the top right-hand corner of each photo, as we have included a lot of additional information about the photo in that space.
+                   </div>
+                  <Link
+                    href="https://photos.app.goo.gl/VmLoUd7HthusNFAZ9"
+                    target="_blank"
+                  >
+                    <Image
+                      src={WaterParkTitled}
+                      className="block ml-auto mr-auto h-auto w-[90%] md:w-[70%] lg:w-[50%] border-solid border-[#efca60] border-4 mt-4 mb-6"
+                      alt="Photo of Water Park."
+                    />
+                  </Link>
+                
+               
+            </div>{" "}
+        </div> 
+         </div> 
+          </div> 
+
+           < div className="mb-20"> 
+      < div className="flex flex-col w-auto leading-1.5 p-2 md:p-4 lg:p-4  mb-4 mt-4 border-[#efca60] border-solid border-2 md:border-2 bg-[#f4f498]">
+         < div className="text-md md:text-base max-w-full font-semibold text-[#2b8936] text-center  md:text-left dark:text-[#2b8936] justify-center items-center">
+         <div className="font-semibold pt-2 pb-2 mr-[30%] ml-[30%]  text-[#bbe89e] text-center  border-[#bbe89e] border-2 bg-[#2b8936]">Day 2: Saturday 7th March 2026</div> {/*End of news date block*/}
+              <div className="font-bold m-4 pt-2 pb-4 py-2 md:px-4 text-[#bbe89e] text-center  border-[#bbe89e] border-2  bg-[#2b8936]"> Day in London with Nicholas, including the British Museum Samurai Exhibition and the British Library Highlights</div>
+        <div className="font-semibold text-center md:text-left border-solid border-[#efca60] border-2 mr-1 ml-1 mt-2 mb-2 px-4 py-4 bg-[#bbe89e]"> No more driving today (hoorah!....). We walked to the local station and caught the train into London King's Cross in order to meet Bernard's brother Nicholas at the British Museum and see the Samurai Exhibition.  It was a very good and interesting exhibition which had been well put together. Nicholas is actually going to Nagasaki in Japan for 3 weeks this summer so he will be able to experience some of this history first hand...
+          <br />
+                <br />
+                A colleague of Nicholas who works at Great Ormond Street Children's Hospital had recommended the <Link href="https://apollobrunswick.com/"  target="_blank" className="text-decoration-underline text-red-600 font-semibold underline">Apollo Cypriot Restaurant, Brunswick Square</Link> for lunch, which was a very pleasant and relaxing experience after going round the exhibition.
+                 <br />
+                <br />
+                In the afternoon, we headed around the corner to the British Library to visit the Treasures of the British Library in the Sir John Ritblat gallery on the ground floor. This contains, amongst others, the original of the Lindisfarne Gospels, the 11th February 1225 Magna Carta, certain of William Tyndale's first printed publications of the New Testament in English (which was at that time banned), examples of William Caxton, the first English printer's works and key religious works from a variety of religions. 
+               
+          <br />
+                <br />
+                Then it was a return to Bernard's dad's where it was good to be able to see stay with him for 2 nights and to catch up with news. 
+                <br />
+                <br />
+                <div className="m-1 pr-2 pl-2 pt-4 pb-4 font-semibold text-center md:text-left text-[#bbe89e] border-[#efca60] border-solid border-2 bg-[#2b8936] mb-6">Click on the image below to open the album of our day in London including the Samurai exhibition at the British Museum, Treasures of the British Library and Cypriot food.
                 <br />
                 <br />
                       Please make sure to click on the <em>'i'</em> icon, in the top right-hand corner of each photo, as we have included a lot of additional information about the photo in that space.
@@ -49,7 +101,7 @@ export default function March2026() {
                     <Image
                       src={SamuraiArmourTitled}
                       className="block ml-auto mr-auto h-auto w-[90%] md:w-[70%] lg:w-[50%] border-solid border-[#efca60] border-4 mt-4 mb-6"
-                      alt="Photo of Gawain day 2."
+                      alt="Photo of samurai armour"
                     />
                   </Link>
                 
