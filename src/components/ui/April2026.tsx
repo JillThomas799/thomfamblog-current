122aca9dfdf0713192a020a423f248e0f45a610e
Jill <125391549+JillThomas799@users.noreply.github.com>
2026-03-18 17:51:56 +0000

    Toysheep design

--- src/components/ui/April2026.tsx
+++ src/components/ui/April2026.tsx
@@ -7,8 +7,8 @@ import Toysheep from "./toysheep";
 export default function April2026() {
   return (
     <div className=" bg-black  border-solid border-4 border-gray-600  md:p-4 mb-8">
-      <div className="height-[100%] text-[#D99E49] text-center font-semibold text-xl p-3 bg-gray-700 border-[#bdbeba] border-4 m-2">
-        <div className="bg-black mr-[10%] ml-[10%] md:mr-[20%] md:ml-[20%] p-1 border-[#bdbeba] border-2 text-[#D99E49 font-bold">April 2026</div>
+      <div className="height-[100%] text-[#D99E49] text-center font-semibold text-xl p-3 bg-green-600 border-[#bdbeba] border-4 m-2">
+        <div className="bg-black mr-[10%] ml-[10%] md:mr-[20%] md:ml-[20%] p-1 border-[#bdbeba] border-2 text-[white] font-bold">April 2026</div>
         <br />
         <br />
         < Toysheep/ >
--- src/components/ui/toysheep.tsx
+++ src/components/ui/toysheep.tsx
@@ -26,16 +26,85 @@ export default function Toysheep() {
               </filter>
             </defs>
 
+            <g id="toysheepbody">
+                <circle
+                cx="50"
+                cy="150"
+                r="90.0"
+                fill="#FCF5E5"
+                opacity="1"
+               /></g>
            
-            <g id="toysheepface">
-              <circle
-                cx="0"
+            <g id="toysheephead"  className="md:translate-x-[-150px] md:translate-y-[50px]" >
+              <g id="toysheepface">
+                <circle
+                cx="50"
                 cy="100"
                 r="45.0"
-                fill="#FF0000"
+                fill="#483C32"
                 opacity="1"
-                />
+               /></g>              
+
+               <g id="toysheepeyeleft">
+                <circle
+                id="outereyeleft"
+                cx="70"
+                cy="90"
+                r="10.0"
+                fill="#FCF5E5"
+                opacity="1"
+               />
+               <circle
+                id="innereyeleft"
+                cx="70"
+                cy="90"
+                r="5.7"
+                fill="#000000"
+                opacity="1"
+               />               
+               </g>
+               <g id="toysheepeyeright">
+                <circle
+                id="outereyeright"
+                cx="35"
+                cy="90"
+                r="10.0"
+                fill="#FCF5E5"
+                opacity="1"
+               />
+               <circle
+                id="innereyeright"
+                cx="35"
+                cy="90"
+                r="5.7"
+                fill="#000000"
+                opacity="1"
+               />
+               </g>
+               <g id="toysheepearright">
+                <rect
+                x="-30"
+                y="60"
+                rx="6"
+                ry="6"
+                width="65"
+                height="15"
+                fill="#000000"
+              ></rect></g>
+               <g id="toysheepearleft">
+                <rect
+                x="70"
+                y="60"
+                rx="6"
+                ry="6"
+                width="65"
+                height="15"
+                fill="#000000"
+              ></rect>
+               </g>
             </g>
+
+             
 
           
           </g>
