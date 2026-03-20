4deeeb3350b57f104ac21c4687a46aca59eebe29
Jill <125391549+JillThomas799@users.noreply.github.com>
2026-03-20 20:17:24 +0000

    Toysheep design

--- src/components/ui/toysheep.tsx
+++ src/components/ui/toysheep.tsx
@@ -21,27 +21,189 @@ export default function Toysheep() {
             filter="url(#blur8)"            
           >
             <defs>
+              <radialGradient
+              id="gradtoysheepjumper"
+              cx="50%"
+              cy="50%"
+              spreadMethod="repeat"
+            >
+               <stop
+                offset="30%"
+                stopColor ="#f4eee0"
+               stopOpacity = "1"
+              >
+                {" "}
+              </stop>
+              
+              <stop
+                offset="40%"
+                stopColor ="#483c32"
+               stopOpacity = "1"
+              >
+                {" "}
+              </stop>
+              <stop
+                offset="40%"
+                stopColor ="#483c32"
+               stopOpacity = "1"
+              >
+                {" "}
+              </stop>
+              <stop
+                offset="50%"
+                stopColor ="#f4eee0"
+               stopOpacity = "1"
+              >
+                {" "}
+              </stop>
+               <stop
+                offset="50%"
+                stopColor ="#f4eee0"
+               stopOpacity = "1"
+              ></stop>
+              <stop
+                offset="60%"
+                stopColor = '#483c32'
+                stopOpacity = "1"
+              ></stop>
+              <stop
+                offset="60%"
+                stopColor = '#483c32'
+                stopOpacity = "1"
+              ></stop>
+               <stop
+                offset="80%"
+                stopColor ="#f4eee0"
+               stopOpacity = "1"
+              ></stop>
+               <stop
+                offset="80%"
+                stopColor ="#f4eee0"
+               stopOpacity = "1"
+              ></stop>
+              <stop
+                offset="100%"
+                stopColor ="#483c32"
+               stopOpacity = "1"
+              ></stop>
+              
+            </radialGradient>
+
               <filter id="blur8" x="0" y="0">
                 <feGaussianBlur in="SourceGraphic" stdDeviation="0.5" />
               </filter>
             </defs>
 
+            <g id="toysheeplegleftfront">
+              <g id="toysheeplegleftfrontupper">
+                <rect 
+                x="-220"
+                y="230"
+                rx="6"
+                ry="6"
+                width="20"
+                height="85"
+                fill="#444444" />
+              </g>
+               <g id="toysheeplegleftfrontfoot">
+                <ellipse
+                cx="-210"
+                cy="315"
+                rx="20"
+                ry="15"
+                fill="black"
+                opacity="0.9"
+                />
+               </g>
+            </g>
+
+             <g id="toysheeplegrightfront">
+              <g id="toysheeplegrightfrontupper"> 
+                <rect 
+                x="-280"
+                y="240"
+                rx="6"
+                ry="6"
+                width="20"
+                height="75"
+                fill="#444444" />
+              </g>
+               <g id="toysheeplegrightfrontfoot">
+                <ellipse
+                cx="-270"
+                cy="315"
+                rx="20"
+                ry="15"
+                fill="black"
+                opacity="0.9"
+                />
+               </g>
+            </g>
+
+              <g id="toysheeplegrightrear">
+              <g id="toysheeplegrightrearupper">
+                <rect 
+                x="-310"
+                y="220"
+                rx="6"
+                ry="6"
+                width="20"
+                height="60"
+                fill="#444444" />
+              </g>
+               <g id="toysheeplegrightrearfoot">
+                  <ellipse
+                cx="-305"
+                cy="290"
+                rx="20"
+                ry="15"
+                fill="black"
+                opacity="0.9"
+                />
+               </g>
+            </g>
+            
+            <g id="toysheeplegleftrear">
+              <g id="toysheeplegleftrearupper"> <rect 
+                x="-250"
+                y="220"
+                rx="6"
+                ry="6"
+                width="20"
+                height="60"
+                fill="#444444" />
+                </g>
+               <g id="toysheeplegleftrearfoot">
+                     <ellipse
+                cx="-240"
+                cy="290"
+                rx="20"
+                ry="15"
+                fill="black"
+                opacity="0.9"
+                />
+               </g>
+            </g>
+
+             
+
             <g id="toysheepbody">
                 <circle
                 cx="50"
                 cy="150"
-                r="90.0"
-                fill="#FCF5E5"
+                r="75.0"
+                // fill="#FCF5E5"
+                fill="url(#gradtoysheepjumper)"
                 opacity="1"
                 className="md:translate-x-[-300px] md:translate-y-[30px]"
                /></g>
            
-            <g id="toysheephead"  className="md:translate-x-[-300px] md:translate-y-[30px]" >
-              <g id="toysheepface">
+            <g id="toysheephead"  className=" md:translate-x-[-310px] md:translate-y-[30px]" >
+              <g id="toysheepface" >
                 <circle
                 cx="50"
-                cy="150"
-                r="45.0"
+                cy="145"
+                r="38.0"
                 fill="#483C32"
                 opacity="1"
                /></g>              
@@ -105,7 +267,10 @@ export default function Toysheep() {
                </g>
             </g>
 
-             
+           
+            
+            
+           
 
           
           </g>
