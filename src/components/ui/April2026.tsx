db65cd86f147cabe157426e4ce91340655742806
Jill <125391549+JillThomas799@users.noreply.github.com>
2026-03-21 19:00:11 +0000

    April blog Welcome

--- src/components/ui/April2026.tsx
+++ src/components/ui/April2026.tsx
@@ -14,6 +14,18 @@ export default function April2026() {
         < Toysheep/ >
       </div>
 
+       < div className="mb-20"> 
+      < div className="flex flex-col w-auto leading-1.5 p-2 md:p-4 lg:p-4  mb-4 mt-4 border-[#efca60] border-solid border-2 md:border-2 bg-[#f4f498]">
+         < div className="text-md md:text-base max-w-full font-semibold text-[#2b8936] text-center dark:text-[#2b8936] justify-center items-center">
+         <div className="font-semibold pt-2 pb-2 mr-[30%] ml-[30%]  text-[#bbe89e] text-center  border-[#bbe89e] border-2 bg-[#2b8936]"> 1st April 2026</div> {/*End of news date block*/}
+              <div className="font-bold m-4 pt-2 pb-4 py-2 md:px-4 text-[#bbe89e] text-center  border-[#bbe89e] border-2  bg-[#2b8936]"> Welcome to the April 2026 blog</div>
+        <div className="font-semibold text-center border-solid border-[#efca60] border-2 mr-1 ml-1 mt-2 mb-2 px-4 py-4 bg-[#bbe89e]"> Welcome to our April blog. 
+            </div>{" "}
+        </div> 
+         </div> 
+          </div> 
+
+
       </div>
   )
 }
