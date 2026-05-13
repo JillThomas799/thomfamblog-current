edba86156c8fb9cf5adff9d7feb1172410f3b676
Jill <125391549+JillThomas799@users.noreply.github.com>
2026-05-13 16:38:36 +0100

    Completion of 2nd May 2026 blog

--- src/components/ui/May2026.tsx
+++ src/components/ui/May2026.tsx
@@ -10,7 +10,7 @@ export default function May2026() {
   return (
     <div className="">
       <div className="bg-RavenglassEskdaleRailway bg-cover border-solid border-4 border-gray-600 md:p-4 mb-8 h-[50vh] text-[#D99E49] text-center font-normal text-l p-3">
-        <div className="bg-gray-800 mr-[7%] ml-[7%] md:mr-[40%] md:ml-[40%] p-1 border-[#bdbeba] border-2 text-[white] font-semibold">May 2026</div>
+        <div className="bg-[#93c572] mr-[7%] ml-[7%] md:mr-[40%] md:ml-[40%] p-1 border-gray-800 border-2 text-gray-800 font-semibold">May 2026</div>
         <br />
         <br />
 
@@ -19,9 +19,9 @@ export default function May2026() {
          < div className="mb-20"> 
                 < div className="flex flex-col w-auto leading-1.5 pt-4 mt-4 md:p-4 lg:p-4  border-[#bbe89e] border-solid border-2 md:border-2 bg-[#F38e7e]">
                    < div className="text-md md:text-base max-w-full font-normal md:font-semibold text-[#white] text-center  md:text-center dark:text-[#2b8936] justify-center items-center">
-                   <div className="font-semibold md:font-semibold p-2 m-2 text-white text-center  border-[#bbe89e] border-2 bg-[#93c572]">2nd May 2026, Saturday.</div> {/*End of news date block*/}
-                        <div className="font-semibold md:font-semibold p-2 m-2 text-white text-center text-md border-[#bbe89e] border-2  bg-[#93c572]">'A Dog's Life' Exhibition at the Rheged Centre, Penrith and 'The Sheep Detectives' film at Rheged Cinema.</div>
-                  <div className="font-normal md:font-semibold text-center text-white border-solid border-[#bbe89e] border-2 mr-1 ml-1 mt-2 px-4 py-2 mb-10 bg-[#93c572]">
+                   <div className="font-semibold md:font-semibold p-2 m-2 text-gray-800 text-center  border-[#bbe89e] border-2 bg-[#93c572]">2nd May 2026, Saturday.</div> {/*End of news date block*/}
+                        <div className="font-semibold md:font-semibold p-2 m-2 text-gray-800 text-center text-md border-[#bbe89e] border-2  bg-[#93c572]">'A Dog's Life' Exhibition at the Rheged Centre, Penrith and 'The Sheep Detectives' film at Rheged Cinema.</div>
+                  <div className="font-normal md:font-semibold text-center text-gray-800 border-solid border-[#bbe89e] border-2 mr-1 ml-1 mt-2 px-4 py-2 mb-10 bg-[#93c572]">
                     Elena and Willow had really enjoyed going to see <Link href="https://rheged.com/event/a-dogs-life/"  target="_blank" className="text-decoration-underline text-gray-800 font-normal md:font-semibold underline">A Dog's Life</Link> recently at the Rheged Centre near Penrith and had recommended it to us. So this morning, we headed off there with Harry to see the exhibition and also a preview of the film <Link href="https://www.rottentomatoes.com/m/the_sheep_detectives"  target="_blank" className="text-decoration-underline text-gray-800 font-normal md:font-semibold underline">The Sheep Detectives</Link> , starring Hugh Jackman, Brett Goldstein and Patrick Stewart.
                           <br />
                           <br />
@@ -34,7 +34,7 @@ export default function May2026() {
                     We then went on to see 'The Sheep Detectives'. The film tells how every night a shepherd (played by Hugh Jackman) reads aloud a murder mystery, pretending his sheep can understand. When he is found dead, the sheep realize at once that it was a murder and think they know everything about how to go about solving it. Its a great film suitable for all ages and we really enjoyed it.
                     <br />
                           <br />
-                    <div className="p-2 font-semibold md:font-semibold text-center text-sm md:text-md text-white border-[#bbe89e] border-solid border-2 bg-[#F38e7e] ">Click on the image below to open the album of our visit to 'A Dog's Life Exhibition' at the Rheged centre, Cumbria.
+                    <div className="p-4 font-semibold md:font-semibold text-center text-md text-gray-800 border-[#bbe89e] border-solid border-2 bg-[#F38e7e] ">Click on the image below to open the album of our visit to 'A Dog's Life Exhibition' at the Rheged centre, Cumbria.
                           <br />
                           <br />
                                 Please make sure to click on the <em>'i'</em> icon, in the top right-hand corner of each photo, as we have included a lot of additional information about the photo in that space.
