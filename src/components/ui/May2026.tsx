40ac9a8a4e756c0f51a9b074877329e8155bd23d
Jill <125391549+JillThomas799@users.noreply.github.com>
2026-05-14 16:44:48 +0100

    Addition of 10th May 2026 blog

--- src/components/ui/May2026.tsx
+++ src/components/ui/May2026.tsx
@@ -4,6 +4,7 @@ import DogsLifeTitled from "/public/imagesresized/DogsLifeTitled.jpg";
 import Greystoke2026Titled from "/public/imagesresized/Greystoke2026Titled.jpg";
 import BurghByMarshTitled from "/public/imagesresized/BurghByMarshTitled.jpg";
 import TrainDalegarth2026Titled from "/public/imagesresized/TrainDalegarth2026Titled.jpg";
+import QuarrybeckWoodBluebellsTitled from "/public/imagesresized/QuarrybeckWoodBluebellsTitled.jpg";
 
 
 
@@ -132,10 +133,55 @@ export default function May2026() {
                         </div>
 
 
+                        
+                < div className="mb-20"> 
+                < div className="flex flex-col w-auto leading-1.5 pt-4 mt-4 md:p-4 lg:p-4  border-[#bbe89e] border-solid border-2 md:border-2 bg-[#F38e7e]">
+                   < div className="text-md md:text-base max-w-full font-normal md:font-semibold text-[#white] text-center  md:text-center dark:text-[#2b8936] justify-center items-center">
+                   <div className="font-semibold md:font-semibold p-2 m-2 text-gray-800 text-center  border-[#bbe89e] border-2 bg-[#93c572]">10th May 2026, Sunday.</div> {/*End of news date block*/}
+                        <div className="font-semibold md:font-semibold p-2 m-2 text-gray-800 text-center text-md border-[#bbe89e] border-2  bg-[#93c572]">Brampton, the Ridge and Ridge wood, Quarrybeck wood with bluebells and Lanercost walk, returning via Naworth Castle and Miltonrigg woods, 9 miles.</div>
+                  <div className="font-normal md:font-semibold text-center text-gray-800 border-solid border-[#bbe89e] border-2 mr-1 ml-1 mt-2 px-4 py-2 mb-10 bg-[#93c572]">
+                      It was another beautiful day today and we wanted to try out the café at Lanercost tearooms for lunch, 
+                        as it has had a change of management recently. We also wanted to see the bluebells in Quarrybeck wood, as they are always beautiful at this time of year. This walk is  9 miles in total, but it is quite manageable by breaking for lunch in the middle. 
+                           <br />
+                          <br />
+                        It is a very varied walk, commencing along the old disused railway track into Brampton, before picking up the Ridge and on through the Ridgewood. Near Great Easby we  crossed the road and entered Quarrybeck wood, which currently is filled with bluebells in flower. We dropped down through the wood and past the falls, finally emerging to complete the short remaining distance to Lanercost tearooms for lunch. 
+                    <br />
+                          <br />
+                          The tea shop is owned by Philip Howard, who owns Naworth Castle. He has replaced the management recently, which has received some varied reviews, but we had a very pleasant lunch today. What is especially good is that, whilst these are not on the lunch menu, they do serve all-day bacon and sausage baps. The food is slightly more reasonably priced than previously, which is helpful in the current cost-of-living crisis.
+                          <br />
+                          <br />
+                           We returned home along the road by Naworth Castle and through Miltonrigg wood, an ancient woodland. Bernard, once again, managed to attract the attention of a herd of cows..... We think they must think that he is the farmer, with his grey hair, shooting jacket and world-weary expression! We had a lovely day.
+                           <br />
+                          <br />
+
+
+
+                            <div className="p-4 font-semibold md:font-semibold text-center text-md text-gray-800 border-[#bbe89e] border-solid border-2 bg-[#F38e7e] "> 
+                              Click on the image below to open the album of our walk to Lanercost via Brampton and the Ridge.                          
+                          <br />
+                          <br />
+                                Please make sure to click on the <em>'i'</em> icon, in the top right-hand corner of each photo, as we have included a lot of additional information about the photo in that space.
+                             </div>
+                            <Link
+                              href="https://photos.app.goo.gl/9KNYbJi5EQCDoyU88"
+                              target="_blank"
+                            >
+                              <Image
+                                src={QuarrybeckWoodBluebellsTitled}
+                                className="block ml-auto mr-auto h-auto w-[100%] md:w-[70%] lg:w-[50%] border-solid border-[#bbe89e] border-4 mt-4 mb-6"
+                                alt="The bluebells in Quarrybeck wood."
+                              />
+                            </Link>
+                          </div>{" "}
+                          </div>
+                   </div>
+                        </div>
+
+
                         < div className="mb-20"> 
                 < div className="flex flex-col w-auto leading-1.5 pt-4 mt-4 md:p-4 lg:p-4  border-[#bbe89e] border-solid border-2 md:border-2 bg-[#F38e7e]">
                    < div className="text-md md:text-base max-w-full font-normal md:font-semibold text-[#white] text-center  md:text-center dark:text-[#2b8936] justify-center items-center">
-                   <div className="font-semibold md:font-semibold p-2 m-2 text-gray-800 text-center  border-[#bbe89e] border-2 bg-[#93c572]">11th May 2026, Saturday.</div> {/*End of news date block*/}
+                   <div className="font-semibold md:font-semibold p-2 m-2 text-gray-800 text-center  border-[#bbe89e] border-2 bg-[#93c572]">11th May 2026, Monday.</div> {/*End of news date block*/}
                         <div className="font-semibold md:font-semibold p-2 m-2 text-gray-800 text-center text-md border-[#bbe89e] border-2  bg-[#93c572]">Ravenglass and Eskdale Railway and Muncaster Castle.</div>
                   <div className="font-normal md:font-semibold text-center text-gray-800 border-solid border-[#bbe89e] border-2 mr-1 ml-1 mt-2 px-4 py-2 mb-10 bg-[#93c572]">
                          The rhodedendra are in flower currently at Muncaster Castle and apparently are beautiful. So being retired people, we decided to make the most of the day and not only go over to Muncaster Castle to see the rhodedendra but also to go for a ride on the Ravenglass and Eskdale Railway, which Bernard has been wanting to do for sometime.
@@ -175,6 +221,8 @@ export default function May2026() {
                           </div>
                    </div>
                         </div>
+
+
                
 
       </div>
