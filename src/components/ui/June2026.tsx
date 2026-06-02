9ea397f05c05346a0c80e9062fa1cb36cc3b390c
Jill <125391549+JillThomas799@users.noreply.github.com>
2026-06-02 20:04:49 +0100

    Addition of June 2026 blog intro

--- src/components/ui/June2026.tsx
+++ src/components/ui/June2026.tsx
@@ -4,19 +4,21 @@ import Link from 'next/link'
 export default function June2026() {
   return (
     <div className="">
-      <div className="bg-Garden3 bg-cover border-solid border-4 border-gray-600 md:p-4 mb-8 h-[100vh] text-[#D99E49] text-center font-normal text-l p-3">
+      <div className="bg-Garden3 bg-cover border-solid border-4 border-gray-600 md:p-4 mb-8 h-[80vh] text-[#D99E49] text-center font-normal text-l p-3">
         <div className="bg-[#93c572] mr-[7%] ml-[7%] md:mr-[40%] md:ml-[40%] p-1 border-gray-800 border-2 text-gray-800 font-semibold">June 2026</div>
         <br />
         <br />
+        
+        </div> 
+
         < div className="mb-20"> 
-      < div className="flex flex-col w-auto leading-1.5 p-2 md:p-4 lg:p-4  mb-4 mt-[110vh] border-gray-800 border-solid border-2 md:border-2 bg-[#bdbeba]">
+      < div className="flex flex-col w-auto leading-1.5 p-2 md:p-4 lg:p-4  mb-4 mt-4 border-gray-800 border-solid border-2 md:border-2 bg-[#bdbeba]">
          < div className="text-md md:text-lg max-w-full font-semibold text-[#f3e2d6] text-center dark:text-[#f3e2d6] justify-center items-center">
         <div className="font-semibold text-center border-solid border-black border-2 mr-1 ml-1 mt-2 mb-2 px-4 py-4 bg-[#786d68]">Welcome to our June 2026 blog.
           <br />
           <br />
           We are delighted to be able to bring you our June 2026 blog. 
             </div>{" "}
-        </div> 
          </div> 
           </div> 
 
