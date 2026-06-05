d7fa17a551c2870188fcb9ff2d11da0bc1e8ea62
Jill <125391549+JillThomas799@users.noreply.github.com>
2026-06-05 20:37:45 +0100

    Addition of 2nd June 2026 blog

--- src/components/ui/June2026.tsx
+++ src/components/ui/June2026.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image'
 import Link from 'next/link'
+import CathyRobinLowtherTitled from "/public/imagesresized/CathyRobinLowtherTitled.jpg";
 
 export default function June2026() {
   return (
@@ -27,7 +28,7 @@ export default function June2026() {
                    <div className="font-semibold md:font-semibold p-2 m-2 text-white text-center  border-gray-800 border-2 bg-[#A47DAB]">2nd June 2026, Monday.</div> {/*End of news date block*/}
                         <div className="font-semibold md:font-semibold p-2 m-2 text-white text-center text-md border-gray border-2  bg-[#A47DAB]">Lowther Castle for the day with Robin, Cathy, Nell and Wilf.</div>
                   <div className="font-normal md:font-normal text-center text-white border-solid border-gray-800 border-2 mr-1 ml-1 mt-2 px-4 py-2 mb-10 bg-[#A47DAB]">
-                    for all ages and we really enjoyed it.
+                    We had not had the opportunity to catch up with our friends Robin and Cathy since before Christmas, so it was lovely to be able to meet them at Lowther Castle for the day. With the birth of their first grandchild in the interim, they have become grandparents for the first time, which is very exciting and we wish them all the best with this new adventure.
                     <br />
                           <br />
                     <div className="p-4 font-semibold md:font-semibold text-center text-md text-white border-gray-800 border-solid border-2 bg-[#a1a09c] ">Click on the image below to open the album of our visit to Lowther Castle with Robin, Cathy, Nell and Wilf.
@@ -36,14 +37,14 @@ export default function June2026() {
                                 Please make sure to click on the <em>'i'</em> icon, in the top right-hand corner of each photo, as we have included a lot of additional information about the photo in that space.
                              </div>
                             <Link
-                              href="https://photos.app.goo.gl/fJX5beKKKuS8K7jK9"
+                              href="https://photos.app.goo.gl/qt7GXmL5ofti2uc59"
                               target="_blank"
                             >
-                              {/* <Image
-                                src={DogsLifeTitled}
+                              <Image
+                                src={CathyRobinLowtherTitled}
                                 className="block ml-auto mr-auto h-auto w-[100%] md:w-[70%] lg:w-[50%] border-solid border-[#bbe89e] border-4 mt-4 mb-6"
-                                alt="Photo of Bernard as a dog."
-                              /> */}
+                                alt="Photo of Cathy, Robin and the dogs Wilf and Nell, at Lowther Castle.."
+                              />
                             </Link>
                           </div>{" "}
                           </div>
