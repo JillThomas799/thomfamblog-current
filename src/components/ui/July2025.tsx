0c5a6c3c1671b6fcd1e80021cda52a3e25ed1e21
Jill <125391549+JillThomas799@users.noreply.github.com>
2026-06-25 14:00:14 +0100

    July2025 blog transfer

--- src/components/ui/July2025.tsx
+++ src/components/ui/July2025.tsx
@@ -102,6 +102,330 @@ export default function July2025() {
                    </div>
 
 
+                   < div className="mb-20"> 
+                < div className="flex flex-col w-auto leading-1.5 pt-4 mt-4 md:p-4 lg:p-4  border-gray-800 border-solid border-2 md:border-2 bg-[#bdbeba]">
+                   < div className="text-md md:text-base max-w-full font-normal md:font-semibold text-gray-800 text-center  md:text-center dark:text-white justify-center items-center">
+                   <div className="font-semibold md:font-semibold p-2 m-2 text-gray-800 text-center  border-gray-800 border-2 bg-[#99b19d]">18th July 2025, Friday.</div> {/*End of news date block*/}
+                        <div className="font-semibold md:font-semibold p-2 m-2 text-gray-800 text-center text-md border-gray border-2  bg-[#99b19d]">  Cardiff PhD Graduation Day: Harry officially becomes 'Dr Harry'
+            <br /> accompanied by lots of Welsh singing and speaking.
+            <br />
+           </div>
+                  <div className="font-normal md:font-normal text-center text-gray-600 border-solid border-gray-800 border-2 mr-1 ml-1 mt-2 px-4 py-2 mb-10 bg-[#99b19d]">
+                      <br />
+                    The day had finally arrived which we had all been awaiting for some
+            time: that of Harry's doctoral graduation ceremony in Cardiff. Harry
+            had been looking forward to the 'floppy hat' ceremony, more
+            officially known as the ceremony where the full doctoral regalia is
+            worn including the 'Tam' hats. Much better than the 'bucket hats',
+            more officially known as the 'mortarboards' of the graduation
+            ceremony.
+            <br />
+            <br />
+            We had fortunately managed to rent some very pleasant{" "}
+            <Link
+              href="https://www.airbnb.co.uk/rooms/609320701093859071?adults=1&search_mode=regular_search&check_in=2025-08-09&check_out=2025-08-14&children=0&infants=0&pets=0&source_impression_id=p3_1753261446_P3onrYSGnR9aShXj&previous_page_section_name=1000&federated_search_id=8292d703-7e53-49a8-9b2e-fcda546f054d"
+              target="_blank"
+              className="text-decoration-underline text-[#0000EE] font-semibold md:font-semibold underline"
+            >
+              {} self-catering accommodation in an airBNB in Bonvilston
+            </Link>
+            , just outside Cardiff, for a few days.
+            <br />
+            <br />
+            Our experience of Cardiff University over the past 5 years has been
+            that it is a great university which makes its students feel very
+            welcome. Cardiff is a lovely city which is well worth a visit for
+            anyone who has never been, or a revisit for those who have.
+            <br />
+            <br />
+            The graduation ceremony was held at the{" "}
+            <Link
+              href="https://www.utilitaarenacardiff.co.uk/"
+              target="_blank"
+              className="text-decoration-underline text-[#0000EE] font-semibold md:font-semibold underline"
+            >
+              {} Utilita Arena, Cardiff.
+            </Link>
+            {} Harry's ceremony was at 15.30 and consisted of the School of
+            Architecture, Chemistry and Physics and Astronomy graduates and
+            doctorates. We were sat towards the back of the auditorium but the
+            view was clear and there were large TV screens on either side to
+            project the close-up images. The university were running a live-feed
+            of the ceremony online so Elena (who was back in Cumbria minding
+            Lennie), and Lennie himself were watching the ceremony on the
+            live-feed on you tube.
+            <br />
+            <br />
+            The opening procession of the ceremony was accompanied by the Welsh
+            hymn{" "}
+            <Link
+              href="https://en.wikipedia.org/wiki/Calon_L%C3%A2n"
+              target="_blank"
+              className="text-decoration-underline text-[#0000EE] font-semibold md:font-semibold underline"
+            >
+              Calon Lân
+            </Link>{" "}
+            {}, which can be heard{" "}
+            <Link
+              href="https://open.spotify.com/track/4018M1GQWCpjdHOwdFmVgU"
+              target="_blank"
+              className="text-decoration-underline text-[#0000EE] font-semibold md:font-semibold underline"
+            >
+              here
+            </Link>{" "}
+            sung by the Treorchy Male Voice Choir. The words were written in the
+            1890s by Daniel James (Gwyrosydd) and sung to a tune by John Hughes.
+            For all Welsh rugby supporters, whilst originally being written as a
+            hymn, it is more widely recognised now as a rugby anthem associated
+            with the Welsh rugby union.
+            <br />
+            <br />
+            It was lovely to see Harry obtaining his well-deserved award and to
+            take part in the ceremony. After some lovely speeches in both
+            English and Welsh,{" "}
+            <Link
+              href="https://www.bass12.co.uk/"
+              target="_blank"
+              className="text-decoration-underline text-[#0000EE] font-semibold md:font-semibold underline"
+            >
+              Bass12
+            </Link>{" "}
+            a riot jazz, brass covers band, put on a very impressive show to
+            lead the exit procession.
+
+                          <br />
+                          <br />
+                    <div className="p-4 font-semibold md:font-semibold text-center text-md text-gray-800 border-gray-800 border-solid border-2 bg-[#c0b9bf] ">Click on the image below to open the album <br /> of Harry's
+                Cardiff graduation ceremony.
+                          <br />
+                          <br />
+                                Please make sure to click on the <em>'i'</em> icon, in the top right-hand corner of each photo, as we have included a lot of additional information about the photo in that space.
+                             </div>
+                            <Link
+                              href="https://photos.app.goo.gl/wM5DEM5YUrXNbqTk7"
+                              target="_blank"
+                            >
+                              <Image
+                                src={GraduationCardiff1Titled}
+                                className="block ml-auto mr-auto h-auto w-[100%] md:w-[70%] lg:w-[50%] border-solid border-[#A47DAB] border-4 mt-4 mb-6"
+                                alt="Photo of Harry in doctoral regalia."
+                              />
+                            </Link>
+                          </div>{" "}
+                          </div>
+                   </div>
+
+                   </div>
+
+                   < div className="mb-20"> 
+                < div className="flex flex-col w-auto leading-1.5 pt-4 mt-4 md:p-4 lg:p-4  border-gray-800 border-solid border-2 md:border-2 bg-[#bdbeba]">
+                   < div className="text-md md:text-base max-w-full font-normal md:font-semibold text-gray-800 text-center  md:text-center dark:text-white justify-center items-center">
+                   <div className="font-semibold md:font-semibold p-2 m-2 text-gray-800 text-center  border-gray-800 border-2 bg-[#99b19d]">20th July 2025, Friday.</div> {/*End of news date block*/}
+                        <div className="font-semibold md:font-semibold p-2 m-2 text-gray-800 text-center text-md border-gray border-2  bg-[#99b19d]"> A day of Medieval Reenactment at Greystoke Castle .
+            <br />
+           </div>
+                  <div className="font-normal md:font-normal text-center text-gray-600 border-solid border-gray-800 border-2 mr-1 ml-1 mt-2 px-4 py-2 mb-10 bg-[#99b19d]">
+                      <br />
+                  Our friends had invited us to a Medieval Reenactment Weekend taking
+            place at Greystoke Castle, Penrith on the weekend of the 19th and
+            20th July, 2025 as their daughter was taking part, and we had never
+            attended such an event before. The event was to feature living
+            history encampments, displays of medieval fashion, squire school,
+            arming of the knight demonstrations, medieval music, a battle
+            reenactment, and a medieval court of justice.
+            <br />
+            <br />
+            It was a super-enjoyable day and far more interesting than we had
+            envisaged. The preparation work and detailed research which goes in
+            behind the scenes is very impressive. The medieval reenactment at
+            Greystoke Castle involved the group{" "}
+            <br />
+           
+            <Link
+              href="https://normannis.co.uk/joining-us/"
+              target="_blank"
+              className="text-decoration-underline text-[#0000EE] font-semibold md:font-semibold underline"
+            >
+            Historia Normannis
+            </Link> {""}
+            which is a 12th century reenactment group, focusing primarily on the
+            events between the reign of Henry I and King John. The events when
+            staged tend to be hosted by a number of the regional groups. This
+            event included a Scottish group, Yorkshire group, Cumbria group and
+            West Mercia group. For anyone interested in joining or finding out
+            more about a group, contact 
+            {" "}
+            <Link
+              href="https://normannis.co.uk/joining-us/"
+              target="_blank"
+              className="text-decoration-underline text-[#0000EE] font-semibold md:font-semibold underline"
+            >Historia Normannis</Link> {" "}
+                          <br />
+                          <br />
+            The events of the day were as follows :
+            <ul>
+              <li>
+                <strong>10am Living History Encampment:</strong> an opportunity
+                to step back in time and explore a medieval encampment with
+                various displays and demonstrations.
+                <br />
+                <br />
+              </li>
+              <li>
+                <strong> 11am Medieval Fashion:</strong> Including a fashion
+                parade demonstrating examples of clothing and attire from the
+                13th century and how, in the 13th century, what you wear is who
+                you are.
+                <br />
+                <br />
+              </li>
+
+              <li>
+                <strong>12 noon Medieval Tournament: </strong> Treason at the
+                Castle? - when the Sheriff of Cumberland’s men visit Greystoke
+                Castle in 1214 they find an army of Scottish soldiers on the
+                castle grounds. Will they suspect the Castellan of Greystoke of
+                plotting treason against King John?
+                <br />
+                <br />
+              </li>
+
+              <li>
+                <strong>13.00 Squire School: </strong>Young aspiring knights
+                learnt basic combat skills and were generally more blood-thirsty
+                than their parents.
+                <br />
+                <br />
+              </li>
+              <li>
+                <strong>14.00 Medieval Music: </strong> An undercover medieval
+                talent show contest .
+                <br />
+                <br />
+              </li>
+              <li>
+                <strong>15.00 Medieval Battle: </strong> Barons Rebellion -
+                Robert de Ros, the Sheriff of Cumberland and his army arrive to
+                ensure the Castellan of Greystoke castle pledges allegiance to
+                King John of England. With civil war on the horizon, will
+                Greystoke kneel to the King, or join forces with Scotland
+                against him?
+                <br />
+                <br />
+              </li>
+              <li></li>
+            </ul>{" "}
+                    <div className="p-4 font-semibold md:font-semibold text-center text-md text-gray-800 border-gray-800 border-solid border-2 bg-[#c0b9bf] ">Click on the image below to open the album <br /> of  medieval reenactment day at Greystoke Castle..
+                          <br />
+                          <br />
+                                Please make sure to click on the <em>'i'</em> icon, in the top right-hand corner of each photo, as we have included a lot of additional information about the photo in that space.
+                             </div>
+                            <Link
+                              href="https://photos.app.goo.gl/kwBEApWmLAH8yutM7"
+                              target="_blank"
+                            >
+                              <Image
+                                src={medievalReenactmentTitled}
+                                className="block ml-auto mr-auto h-auto w-[100%] md:w-[70%] lg:w-[50%] border-solid border-[#A47DAB] border-4 mt-4 mb-6"
+                                alt="Photo of Medieval Reenactment at Greystoke."
+                              />
+                            </Link>
+                          </div>{" "}
+                          </div>
+                   </div>
+
+                   </div>
+
+
+  < div className="mb-20"> 
+                < div className="flex flex-col w-auto leading-1.5 pt-4 mt-4 md:p-4 lg:p-4  border-gray-800 border-solid border-2 md:border-2 bg-[#bdbeba]">
+                   < div className="text-md md:text-base max-w-full font-normal md:font-semibold text-gray-800 text-center  md:text-center dark:text-white justify-center items-center">
+                   <div className="font-semibold md:font-semibold p-2 m-2 text-gray-800 text-center  border-gray-800 border-2 bg-[#99b19d]">29th July 2025, Tuesday.</div> {/*End of news date block*/}
+                        <div className="font-semibold md:font-semibold p-2 m-2 text-gray-800 text-center text-md border-gray border-2  bg-[#99b19d]">   Visit to Glynebourne in East Sussex, to see Verdi's Falstaff .
+            <br />
+           </div>
+                  <div className="font-normal md:font-normal text-center text-gray-600 border-solid border-gray-800 border-2 mr-1 ml-1 mt-2 px-4 py-2 mb-10 bg-[#99b19d]">
+                      <br />
+                  Today, was very exciting as we were off to Glynebourne in East
+            Sussex to see Verdi's Falstaff. We were last at Glynebourne in 2013,
+            when we saw the Marriage of Figaro by Mozart. On that occasion we
+            all 4 were in attendance as it was to celebrate a special occasion
+            and included an overnight stay in a local hotel in Lewes, a prepared
+            meal in the interval and the more expensive seats in the circle.
+            Don't ask about the cost....we try not to think about it....
+            <br />
+            <br />
+            However, just to prove you don't need to break the bank to go to
+            Glynebourne, we adopted a very different strategy this time. Firstly
+            we combined the Glynebourne trip with a visit to Jill's brother and
+            sister-in-law in the South East, with whom we stayed for a few days
+            and Bernard's father in the South. We took our own home-made picnic
+            and, as the parking facilities are good and the roads more
+            accessible than previously, we took the car. Additionally, as the
+            auditorium is reasonably compact and most seats have excellent views
+            and all have excellent acoustics, we opted for more affordable seats
+            which really were no different in price from that which you would be
+            paying for the opera or ballet in the large cities.
+            <br />
+            <br />
+            This production of Verdi's Falstaff was originally conceived in 2009
+            and is directed by Richard Jones. It is set in 1945 in Windsor, just
+            after the end of the Second World War. The opera is adapted from
+            Shakespeare’s The Merry Wives of Windsor (with passages from Henry
+            IV Part One) and is a comedy.
+            <br />
+            <br />
+            The cast included: Sir John Falstaff – Renato Girolami; Ford –
+            Rodion Pogossov; Alice Ford – Anna Princeva; Nannetta – Mariam
+            Battistelli; Fenton – Filipe Manu; Meg Page – Stephanie Lauricella;
+            Mistress Quickly – Valentina Pernozzoli; Dr Caius – Gregory
+            Bonfatti; Bardolfo – Colin Judon; Pistola – Callum, Thorpe
+            <br />
+            <br />
+            However, the stars of the show were not just the excellent singers
+            and accompanying London Philharmonic orchestra but also a number of
+            very clever props and a set of animatronic cats. The first of these,
+            a ginger and white cat on the bar of the Garter Inn, can be seen
+            from 0.57 onwards until the end of this{" "}
+            <br />
+           
+            <Link
+              href="https://www.youtube.com/watch?v=wy3Z7akRtYo"
+              target="_blank"
+              className="text-decoration-underline text-[#0000EE] font-semibold md:font-semibold underline"
+            >
+            excerpt from the 1st Act of Falstaff at Glynebourne
+            </Link> {""}
+             . In each scene, there is a cat which reappears in different forms.
+            In the last act, it is a black cat sat high up in the tree with eyes
+            which light-up and flash.
+                          <br />
+                          <br />
+            
+                    <div className="p-4 font-semibold md:font-semibold text-center text-md text-gray-800 border-gray-800 border-solid border-2 bg-[#c0b9bf] ">Click on the image below to open the album <br /> of our trip to
+                the opera at Glynebourne to see Verdi's Falstaff.
+                          <br />
+                          <br />
+                                Please make sure to click on the <em>'i'</em> icon, in the top right-hand corner of each photo, as we have included a lot of additional information about the photo in that space.
+                             </div>
+                            <Link
+                              href="https://photos.app.goo.gl/S7Vp5KAfdfmFSsJS8"
+                              target="_blank"
+                            >
+                              <Image
+                                src={Glynebourne2025Titled}
+                                className="block ml-auto mr-auto h-auto w-[100%] md:w-[70%] lg:w-[50%] border-solid border-[#A47DAB] border-4 mt-4 mb-6"
+                                alt="Photo of the auditorium at Glynebourne."
+                              />
+                            </Link>
+                          </div>{" "}
+                          </div>
+                   </div>
+
+                   </div>
+
+
+
 
 </div>
 </div> 
