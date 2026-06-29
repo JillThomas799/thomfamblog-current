1be0d2d53d63032b2156de929ac18ea2a0aa0714
Jill <125391549+JillThomas799@users.noreply.github.com>
2026-06-29 18:59:21 +0100

    Work on HWP2024 blog intro

--- src/components/ui/hwp2024.tsx
+++ src/components/ui/hwp2024.tsx
@@ -6,7 +6,7 @@ import Link from 'next/link'
 export default function HWP2024() {
   return (
     <div className="">
-      <div className="bg-AprilGrass bg-cover border-solid border-4 border-gray-600 md:p-4 mb-8 h-[50vh] text-[#D99E49] text-center font-normal text-l p-3">
+      <div className="bg-AprilGrass bg-cover border-solid border-4 border-gray-600 md:p-4 mb-8 h-[50vh] text-black text-center font-normal text-l p-3">
         <div className="bg-gray-800 mr-[7%] ml-[7%] md:mr-[40%] md:ml-[40%] p-1 border-[#bdbeba] border-2 text-[white] font-semibold">Hadrian's Wall Path Expedition 2024</div>
         <br />
         <br />
@@ -19,21 +19,89 @@ export default function HWP2024() {
       </div>
 
        < div className="mb-20"> 
-      < div className="flex flex-col bg-[#f4f498] h-[80%] md:h-[100%]  w-auto leading-1.5 pt-4 md:p-4 lg:p-4 border-[#efca60] border-solid border-4 md:border-2 ">
-         < div className="text-sm md:text-md md:text-base max-w-full font-normal md:fount-semibold text-[#2b8936] text-center dark:text-[#2b8936] justify-center items-center">
-         <div className="font-semibold pt-2 pb-2 mr-[30%] ml-[30%]  text-[#bbe89e] text-center  border-[#bbe89e] border-2 bg-[#2b8936]"> 1st April 2026</div> {/*End of news date block*/}
-              <div className="font-bold m-4 pt-2 pb-4 py-2 md:px-4 text-[#bbe89e] text-center  border-[#bbe89e] border-2  bg-[#2b8936]"> Welcome to the April 2026 blog</div>
+      < div className="flex flex-col bg-[#850f55] h-[80%] md:h-[100%]  w-auto leading-1.5 pt-4 md:p-4 lg:p-4">
+         < div className="text-sm md:text-md md:text-base max-w-full font-normal md:fount-semibold text-[#2b8936] text-center dark:text-black justify-center items-center">
+         <div className="font-semibold pt-2 pb-2 mr-[30%] ml-[30%]  text-white text-center bg-[#66023c]">   1st June, 2024 onwards.... Hadrian's Wall Long Distance Path</div> {/*End of news date block*/}
+              <div className="font-bold m-4 pt-2 pb-4 py-2 md:px-4 text-white text-center  bg-[#66023c]"> How did we come to be here?.....</div>
 
-        <div className="font-semibold text-center border-solid border-[#efca60] border-2 mr-1 ml-1 mt-2 mb-2 px-4 py-4 bg-[#bbe89e]"> Welcome to our April blog. 
-          <br />
-          <br />
-          Last month, for mother's day, Elena kindly bought Jill a very cute toy sheep.The sheep and lambs are all in the fields currently so, on a topical theme, this has been used as the starting point for the design of this month's header.  Jill has created a graphical representation of the toy sheep as a comparison to the actual image of the toy sheep as an inset.
-          <br />
-          <br />
-          It is an early Easter this month, although with the rise in fuel prices and risk to supply, we are aiming to restrict driving of the petrol and diesel cars. There is unfortunately no home-made Simnel cake this year as Bernard is on a diet..
-          <br />
-          <br />
-          A day on the train to Edinburgh is planned and towards the middle of the month Jill and Bernard are off to Norfolk for the week. The week in Norfolk looks to involve lots of history and walking and means that at long last Jill can see the locations described in one of her favourite novelist, Ellie Griffith's, Ruth Galloway series. However, more about that later...
+        <div className="font-semibold text-center border-solid mr-1 ml-1 mt-2 mb-2 px-4 py-4 bg-[#d19b2f]"> 
+             
+              If this were a novel, it might possibly commence with the
+              following....... 
+              <br /> 
+              <br />
+         
+            <em>
+              'When they looked back afterwards, no one could quite recall how
+              they had arrived at the decision to walk this 84 mile wild and
+              windswept path from the East to the West Coast of England,
+              encompassing both current-day Northumberland and Cumbria. Or at
+              least, if anyone did recall, no one was now admitting to it, in
+              case they might have to accept responsibility....'
+              <br />
+              <br />
+            </em>
+            On a more serious note though, what is clear are the aims of this
+            undertaking, which are to:
+            <br />
+            <ul id="hwpIntroList">
+              <li>
+                Complete the Hadrian's Wall Path on separate days over a number
+                of months.
+              </li>
+              <li>
+                Ensure there is ample opportunity to visit the many museums and
+                sites of interest en route and to enjoy the fantastic views.
+              </li>
+              <li>Try and keep the trip as 'green' as possible.</li>
+              <li>
+                Make a blog record of the journey, which others considering this
+                trip might find useful.
+              </li>
+              <li>
+                Try where possible to support local and regional businesses,
+                organisations and charities.
+              </li>
+            </ul>
+            <br />
+            Over the coming weeks and months we will section by section be
+            adding news of our Hadrian's Wall Path Expedition.
+            <br />
+            <br />
+            To enter into the theme, the background colours for the Hadrian's
+            Wall Path blog have been chosen for their significance to the Romans
+            and the Roman Empire, with the key colour being Tyrian Purple, the
+            colour worn by the Roman Emperor. This{" "}
+            <Link
+              href="https://www.ncl.ac.uk/press/articles/latest/2024/05/tyrianpurplecarlisle/"
+              target="_blank"
+              title="Click here to learn more about information produced by Newcastle University about Tyrian purple"
+              className="LinkHWP"
+            >
+              {" "}
+              article{" "}
+            </Link>{" "}
+            from Newcastle University explains more about the source and
+            importance of Tyrian purple, which only the emperor was allowed to
+            wear.
+          </div>{" "}
+          {/*Closing tab for hwpDailyNarrative div */}
+        </div>{" "}
+        {/*Closing tab for newsHwp div */}
+      </div>{" "}
+      {/*Closing tab for datenewsDivHwp div */}
+      <div className="datenewsDivHwp">
+        <section className="newsheaderHwp">
+          <strong>
+            Day 1 - Wallsend to Newcastle Swing Bridge: 5.1 miles on the
+            Hadrian's Wall Path, 7.5 miles total
+          </strong>
+        </section>
+        <section className="dateHwp">1st June 2024, Saturday</section>
+        <div className="newsHwp">
+          <div className="componentWalkDetails">
+            <strong className="headTitles">Details of the Walk </strong>
+     
             </div>{" "}
         </div> 
          </div> 
