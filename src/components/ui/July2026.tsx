00686024c38abcd0a915cf15f77c60ee012ff402
Jill <125391549+JillThomas799@users.noreply.github.com>
2026-07-09 17:23:05 +0100

    Addition of 4th July 2026 blog, HWP2026 day 6

--- src/components/ui/July2026.tsx
+++ src/components/ui/July2026.tsx
@@ -28,11 +28,23 @@ export default function July2026() {
             < div className="mb-20"> 
                 < div className="flex flex-col w-auto leading-1.5 pt-4 mt-4 md:p-4 lg:p-4  border-gray-800 border-solid border-2 md:border-2 bg-[#96AD93]">
                    < div className="text-md md:text-base max-w-full font-normal md:font-semibold text-white text-center  md:text-center dark:text-white justify-center items-center">
-                   <div className="font-semibold md:font-semibold p-2 m-2 text-white text-center  border-gray-800 border-2 bg-[#336600]">4th July 2026, Tuesday.</div> {/*End of news date block*/}
+                   <div className="font-semibold md:font-semibold p-2 m-2 text-white text-center  border-gray-800 border-2 bg-[#336600]">4th July 2026, Saturday.</div> {/*End of news date block*/}
                         <div className="font-semibold md:font-semibold p-2 m-2 text-white text-center text-md border-gray border-2  bg-[#336600]">Hadrian's Wall Path West to East (abridged): Day 6, Banks East Turret to Gilsland.</div>
                   <div className="font-normal md:font-normal text-center text-white border-solid border-gray-800 border-2 mr-1 ml-1 mt-2 px-4 py-2 mb-10 bg-[#336600]">
+                    Today, it was time for more Hadrian's Wall walking as we were on to day 6, Banks East Turret to Gilsland. We were now approaching some of the main and most interesting parts of the wall as originally, the stone wall only ran from Wallsend as far as Birdoswald Fort and after that it was turf.  Subsequently, the turf wall was also converted to stone. From here on in is a segment where a significant amount of the original wall remains.
                           <br />
                           <br />
+                    The weather forecast was mixed, with quite a lot of rain but Harry was keen to give it a go, so off we went.
+                    It was not long before we ran into problems, as some of the long grass was extremely wet ( we were the first people passing that day) and so within the first mile, our lower legs are soaking wet....   Jill is worst affected as the water has run down into her walking boots and she is walking in socks which are squelching out water.....   This was not an optimistic sign for the next 8 miles. 
+                     <br />
+                          <br />
+                    However, on arriving at Birdoswald Fort, Jill disappears to the shop to purchase some replacement socks, removes the unzippable lower part of her walking trousers and puts her over trousers on and the world is back to rights and the remainder of the walk passed without incident.
+                            <br />
+                          <br />
+                          We returned from Gilsland the way we had walked out. There was much debate as to whether we should hop on the bus for the return journey as this year Cumberland council have put on a bus service linking the section of the wall between Brampton with the Roman Army Museum at Walltown. This is a hop on, hop off bus and seems to be running pretty regularly. Currently it is Reay's bus company who are providing this <Link href="https://www.cumberland.gov.uk/buses/service/HW1"  target="_blank" className="text-decoration-underline text-[#0000EE] font-semibold md:font-semibold underline">HW1 service</Link> It links with the Go NorthEast <Link href="https://www.gonortheast.co.uk/services/GNE/AD12"  target="_blank" className="text-decoration-underline text-[#0000EE] font-semibold md:font-semibold underline">122 service </Link> which runs between the Roman Army Museum at Walltown and Greenhead, and Hexham.
+                     <br />
+                          <br />
+                
                     <div className="p-4 font-semibold md:font-semibold text-center text-md text-[#336600] border-gray-800 border-solid border-2 bg-[#ea9262] ">Click on the image below to open the album of the 6th day of our Hadrian's Wall Path walk West to East (abridged): Banks East Turret to Gilsland.
                           <br />
                           <br />
