99f97face99311e6e5bd56eed593365cbc418c20
Jill <125391549+JillThomas799@users.noreply.github.com>
2026-07-22 15:59:34 +0100

    Addition of 18th July, Day 7 HWP2026 blog

--- src/components/ui/July2026.tsx
+++ src/components/ui/July2026.tsx
@@ -11,6 +11,7 @@ import TullieNLTTitled from "/public/imagesresized/TullieNLTTitled.jpg";
 import WhitbyAbbeyTitled from "/public/imagesresized/WhitbyAbbeyTitled.jpg";
 import ClevelandBTTitled from "/public/imagesresized/ClevelandBTTitled.jpg";
 import VanillaTitled from "/public/imagesresized/VanillaTitled.jpg";
+import JillHWP2026D7Titled from "/public/imagesresized/JillHWP2026D7Titled.jpg";
 
 
 
@@ -448,12 +449,50 @@ export default function July2026() {
               </div>
 
 
+              < div className="mb-20">
+          < div className="flex flex-col w-auto leading-1.5 pt-4 mt-4 md:p-4 lg:p-4  border-gray-800 border-solid border-2 md:border-2 bg-[#96AD93]">
+            < div className="text-md md:text-base max-w-full font-normal md:font-semibold text-white text-center  md:text-center dark:text-white justify-center items-center">
+              <div className="font-semibold md:font-semibold p-2 m-2 text-white text-center  border-gray-800 border-2 bg-[#336600]">18th July 2026, Saturday.</div> {/*End of news date block*/}
+              <div className="font-semibold md:font-semibold p-2 m-2 text-white text-center text-md border-gray border-2  bg-[#336600]">Hadrian's Wall Path West to East (abridged): Day 7, Gilsland to Walltown Recreation Park</div>
+              <div className="font-normal md:font-normal text-center text-white border-solid border-gray-800 border-2 mr-1 ml-1 mt-2 px-4 py-2 mb-10 bg-[#336600]">
+                Today's 7th day walk on the Hadrian's Wall Path 2026 commenced in Gilsland and took us out to Walltown Recreation Park via Thirlwall Castle and back again along the same route, making a 5.7 mile walk. It was great to see the <Link href="https://thesamsoninn.co.uk/" target="_blank" className="text-decoration-underline text-[#ffbf00] font-semibold md:font-semibold underline">Samson Inn</Link> at Gilsland reopened, thanks to its <Link href=" https://www.bbc.co.uk/news/articles/czk0502x0x7o" target="_blank" className="text-decoration-underline text-[#ffbf00] font-semibold md:font-semibold underline">purchase by the local community</Link>, as it had been closed when we last undertook the walk in 2024. 
+                <br />
+                <br />
+                The walk was reasonably busy, given we are now in the main Summer holiday period and we were grateful to one of the local Gilsland residents for redirecting us on to the correct path when we missed the turn initially on leaving the village (it seemed to be something he was quite used to...). We were hassled by some unruly geese on the path just outside Chapel House Farm, Gilsland, but we managed to pass safely and the walk otherwise passed without incident.
+                  <br />
+                <br />
+
+                <div className="p-4 font-semibold md:font-semibold text-center text-md text-[#18392b] border-gray-800 border-solid border-2 bg-[#ea9262] ">Click on the image below to open the album of the 7th day of our Hadrian's Wall Path walk West to East (abridged): Gilsland to Walltown Recreation Park.
+                  <br />
+                  <br />
+                  Please make sure to click on the <em>'i'</em> icon, in the top right-hand corner of each photo, as we have included a lot of additional information about the photo in that space.
+                </div>
+                <Link
+                  href="https://photos.app.goo.gl/aLTEhBbZJWXwjjYv9"
+                  target="_blank"
+                >
+                  <Image
+                    src={JillHWP2026D7Titled}
+                    className="block ml-auto mr-auto h-auto w-[100%] md:w-[70%] lg:w-[50%] border-solid border-[#96AD93] border-4 mt-4 mb-6"
+                    alt="Photo of Jill on the Hadrian's Wall Path between Gilsland and Walltown recreation park."
+                  />
+                </Link>
+              </div>{" "}
+            </div>
+          </div>
 
 
 
 
- </div>
- </div>
+
+
+
+
+
+
+          </div>
+          </div>
+          </div>
            </div>
            </div>
            </div>
