81717937aae160ac9b3bb2748aad3bab655328eb
Jill <125391549+JillThomas799@users.noreply.github.com>
2026-08-04 19:49:50 +0100

    Work on August2026 blog

--- src/components/ui/August2026.tsx
+++ src/components/ui/August2026.tsx
@@ -2,13 +2,14 @@ import Image from 'next/image'
 import Link from 'next/link'
 
 import BanksEastGilslandTitled from "/public/imagesresized/BanksEastGilslandTitled.jpg";
+import MoroccanFiguresTitled from "/public/imagesresized/MoroccanFiguresTitled.jpg";
 
 
 export default function August2026() {
   return (
     <div>
-      <div className="bg-MoroccanFigures bg-cover border-solid border-8 border-[#EE4B2b] md:p-4 mb-8 h-auto md:h-[100vh] text-gray-800 text-center font-normal text-l p-3">
-        <div className="bg-[#477ca5] mr-[7%] ml-[7%] md:mr-[40%] md:ml-[40%] p-1 border-gray-800 border-2 text-white font-semibold">August 2026</div>
+      <div className="bg-MoroccanFigures bg-contain border-solid border-8 border-[#477ca5] md:p-4 mb-8 h-auto text-gray-800 text-center font-normal text-lg p-3">
+        <div className="bg-[#477ca5] mr-[3%] ml-[3%] md:mr-[30%] md:ml-[30%] p-1 mb-10 border-gray-800 border-2 text-white font-semibold">August 2026</div>
         <br />
         <br />
 
@@ -27,36 +28,26 @@ export default function August2026() {
         < div className="mb-20">
           < div className="flex flex-col w-auto leading-1.5 pt-4 mt-4 md:p-4 lg:p-4  border-gray-800 border-solid border-2 md:border-2 bg-[#ff9900]">
             < div className="text-md md:text-base max-w-full font-normal md:font-semibold text-white text-center  md:text-center dark:text-white justify-center items-center">
-              <div className="font-bold md:font-semibold p-2 m-2 text-white text-center  border-gray-800 border-2 bg-[#477ca5]">4th July 2026, Saturday.</div> {/*End of news date block*/}
-              <div className="font-bold md:font-bold p-2 m-2 text-white white text-center text-md border-gray border-2  bg-[#477ca5]">Harry's holidays - Hadrian's Wall Path West to East (abridged): Day 6, Banks East Turret to Gilsland.</div>
-              <div className="font-semibold md:font-semibold text-center text-white  border-solid border-gray-800 border-2 mr-1 ml-1 mt-2 px-4 py-2 mb-10 bg-[#477ca5]">
-                Today, it was time for more Hadrian's Wall walking as we were on to day 6, Banks East Turret to Gilsland. We were now approaching some of the main and most interesting parts of the wall as originally, the stone wall only ran from Wallsend as far as Birdoswald Fort and after that it was turf.  Subsequently, the turf wall was also converted to stone. From here on in is a segment where a significant amount of the original wall remains.
-                <br />
-                <br />
-                The weather forecast was mixed, with quite a lot of rain but Harry was keen to give it a go, so off we went.
-                It was not long before we ran into problems, as some of the long grass was extremely wet ( we were the first people passing that day) and so within the first mile, our lower legs are soaking wet....   Jill is worst affected as the water has run down into her walking boots and she is walking in socks which are squelching out water.....   This was not an optimistic sign for the next 8 miles.
-                <br />
-                <br />
-                However, on arriving at Birdoswald Fort, Jill disappears to the shop to purchase some replacement socks, removes the unzippable lower part of her walking trousers and puts her over trousers on and the world is back to rights and the remainder of the walk passed without incident.
-                <br />
-                <br />
-                We returned from Gilsland the way we had walked out. There was much debate as to whether we should hop on the bus for the return journey as this year Cumberland council have put on a bus service linking the section of the wall between Brampton with the Roman Army Museum at Walltown. This is a hop on, hop off bus and seems to be running pretty regularly. Currently it is Reay's bus company who are providing this <Link href="https://www.cumberland.gov.uk/buses/service/HW1" target="_blank" className="text-decoration-underline text-[#EE4b2b] font-bold md:font-bold underline">HW1 service</Link> It links with the Go NorthEast <Link href="https://www.gonortheast.co.uk/services/GNE/AD12" target="_blank" className="text-decoration-underline text-[#EE4b2b] font-bold md:font-bold underline">122 service </Link> which runs between the Roman Army Museum at Walltown and Greenhead, and Hexham.
+              <div className="font-bold md:font-semibold p-2 m-2 text-white text-center  border-gray-800 border-2 bg-[#477ca5]">1st August 2026, Saturday.</div> {/*End of news date block*/}
+              <div className="font-bold md:font-bold p-2 m-2 text-white white text-center text-md border-gray border-2  bg-[#477ca5]"> Genghis Khan: How the Mongols Changed the World Exhibition at the Leeds Armouries and the Carlisle-Settle-Leeds Railway.</div>
+              <div className="font-semibold md:font-semibold text-center text-white  border-solid border-gray-800 border-2 mr-1 ml-1 mt-2 px-4 py-4 mb-10 bg-[#477ca5]">
+                Many, many years ago, when Jill was still a young girl at Junior school, she found a book on the school book shelves which fascinated her, all about Genghis Khan and the Mongolian Empire.
                 <br />
                 <br />
 
-                <div className="p-4 font-semibold md:font-semibold text-center text-md text-[#477ca5] border-gray-800 border-solid border-2 bg-white ">Click on the image below to open the album of the 6th day of our Hadrian's Wall Path walk West to East (abridged): Banks East Turret to Gilsland.
+                <div className="p-4 font-semibold md:font-semibold text-center text-md text-[#477ca5] border-gray-800 border-solid border-2 bg-white ">Click on the image below to open the album of our  visit to the Genghis Khan: How the Mongols Changed the World Exhibition at the Leeds Armouries and journey the Carlisle-Settle-Leeds Railway.
                   <br />
                   <br />
                   Please make sure to click on the <em>'i'</em> icon, in the top right-hand corner of each photo, as we have included a lot of additional information about the photo in that space.
                 </div>
                 <Link
-                  href="https://photos.app.goo.gl/2hWBCwaw1fcviKuz8"
+                  href="https://photos.app.goo.gl/k8TCZUd4k1XyJW9N6"
                   target="_blank"
                 >
                   <Image
-                    src={BanksEastGilslandTitled}
+                    src={MoroccanFiguresTitled}
                     className="block ml-auto mr-auto h-auto w-[100%] md:w-[70%] lg:w-[50%] border-solid border-[#ff9900] border-4 mt-4 mb-6"
-                    alt="Photo of Bernard and Harry by the car at Banks East Turret parking."
+                    alt="Photo of Moroccan Ger and horse made by e-Mongol."
                   />
                 </Link>
               </div>{" "}
