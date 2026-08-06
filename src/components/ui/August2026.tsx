cae0062e0a0e5877ae846fb0b25ae66f208cc02a
Jill <125391549+JillThomas799@users.noreply.github.com>
2026-08-06 18:05:32 +0100

    Addition of 1st August 2026 blog, Genghis Khan

--- src/components/ui/August2026.tsx
+++ src/components/ui/August2026.tsx
@@ -3,6 +3,8 @@ import Link from 'next/link'
 
 import BanksEastGilslandTitled from "/public/imagesresized/BanksEastGilslandTitled.jpg";
 import MoroccanFiguresTitled from "/public/imagesresized/MoroccanFiguresTitled.jpg";
+import MongolEmpire from "/public/imagesresized/MongolEmpire.jpg";
+
 
 
 export default function August2026() {
@@ -19,16 +21,19 @@ export default function August2026() {
         < div className="flex flex-col w-auto leading-1.5 p-2 md:p-4 lg:p-4  mb-4 mt-4 border-gray-800 border-solid border-2 md:border-2 bg-[#ff9933]">
           < div className="text-md md:text-lg max-w-full font-normal text-white text-center dark:text-white justify-center items-center">
             <div className="font-semibold text-center border-solid  border-black border-2 mr-1 ml-1 mt-2 mb-2 px-4 py-4 bg-[#477ca5] "><div className="font-bold">Welcome to our August 2026 blog.</div>
-              <br />
-            <div className="text-left">We are into August already, but the sun is shining again and there is plenty of opportunity to sit back and enjoy the garden. 
+            <div className="text-left">We are into August already, the sun is shining again and there is plenty of opportunity to sit back and enjoy the garden, but also time to fit in some important trips to look forward to this month. 
              <br />
                 <br />
-            There are 3 significant trips this month: the first is to take the Carlisle-Settle-Leeds railway for the first time and to go to see the 'Genghis Khan: How the Mongols Changed the World' Exhibition at the Leeds Armouries and visit Leeds city by train, the second is the delayed annual father's day outing for Bernard, which is this year to experience life in action on the footplate of the Flying Scotsman steam train. 
-              <br />
-                <br />            
-            Last but not least, Bernard's dad is taking the plunge and coming up to see us in the North of England, visiting relatives en-route, and we are going to take him at the age of 95 (all being well) somewhere he has never been but would very much like to go, to Lindesfarne or Holy Island. Holy Island is associated with the hugely-important monastery founded by St Aidan in 635 as a base for his Christian mission in the north of England. St Cuthbert was prior and then bishop of Lindisfarne in the late 7th century, and his body was placed in a shrine in the church after his death.   
+           Our first trip is to take the Carlisle-Settle-Leeds railway for the first time and to go to see the 'Genghis Khan: How the Mongols Changed the World' Exhibition at the Leeds Armouries. 
             <br />
                 <br />
+           Our second trip is the long-awaited annual father's day outing for Bernard, which is this year for him to experience life in action on the footplate of the Flying Scotsman steam train <Link href="https://www.railwaymuseum.org.uk/what-was-on/flying-scotsman-footplate-experience" target="_blank" className="text-decoration-underline text-[#ff0000] font-semibold md:font-semibold underline">Flying Scotsman Footplate Experience</Link>. 
+            <br />
+                <br />
+           Last but not least, Bernard's dad is taking the plunge and coming up to see us in the North of England, visiting relatives en-route, and we are going to take him at the age of 95 (all being well) somewhere he has never been but would very much like to go, which is to Lindesfarne, otherwise known as Holy Island.   
+            <br />
+                <br />
+            Our August header design this month reflects our Mongolian theme as we have created our own Mongolian mobile city, with many horses and the mobile felt Mongolian tents called gers which could be moved easily, thus supporting the Mongolian nomadic way of life.
             </div>
             </div>{" "}
           </div>
@@ -40,11 +45,35 @@ export default function August2026() {
             < div className="text-md md:text-base max-w-full font-normal md:font-semibold text-white text-center  md:text-center dark:text-white justify-center items-center">
               <div className="font-bold md:font-semibold p-2 m-2 text-white text-center  border-gray-800 border-2 bg-[#477ca5]">1st August 2026, Saturday.</div> {/*End of news date block*/}
               <div className="font-bold md:font-bold p-2 m-2 text-white white text-center text-md border-gray border-2  bg-[#477ca5]"> Genghis Khan: How the Mongols Changed the World Exhibition at the Leeds Armouries and the Carlisle-Settle-Leeds Railway.</div>
-              <div className="font-semibold md:font-semibold text-center text-white  border-solid border-gray-800 border-2 mr-1 ml-1 mt-2 px-4 py-4 mb-10 bg-[#477ca5]">
-                Many, many years ago, when Jill was still a young girl at Junior school, she found a book on the school book shelves which fascinated her, all about Genghis Khan and the Mongolian Empire.
+              <div className="font-semibold md:font-semibold text-left text-white  border-solid border-gray-800 border-2 mr-1 ml-1 mt-2 px-4 py-4 mb-10 bg-[#477ca5]">
+                Many years ago, when Jill was still a young girl at Junior school, she found a book on the school book shelves which fascinated her, all about Genghis Khan and the Mongolian Empire. Not least because she remembers it describing how violent and blood-thirsty Genghis Khan and his warriors were, the extent of their empire and how, were Genghis Khan to have continued, he would have conquered Britain. Since then Mongolia and the Mongolian Empire has always held a fascination. 
                 <br />
+                <br />                
+                At its height, the Mongol Empire of the 13th and 14th centuries was the largest contiguous land empire in history. It stretched from the sea of Japan on the east Eurasia all the way to parts of eastern Europe, almost to the gates of Vienna. It stretched from the Arctic down to Indian and the Iranian plateau. 
+                 <Image
+                    src={MongolEmpire}
+                    className="block ml-auto mr-auto h-auto w-[100%] md:w-[70%] lg:w-[50%] border-solid border-[#ff9900] border-4 mt-4 mb-6"
+                    alt="Photo of Moroccan Ger and horse made by e-Mongol."
+                  />
+                                
+                This empire was formed by Chinnghis Khan (formerly referred to as Genghis Khan). Under Chinnghis Khan the Mongolian forces were probably one of the most formidable military forces which rode across the surface of the earth. After his death in 1227, when he was preparing to take over the Jin dynasty in China, he was followed by his third son Ogödei  who became Great Khan at the kuriltai of 1229. 
+                <br />
+                <br />                
+                Ogödei played a key role in building the Mongol Empire as under his leadership, Mongol forces completed the conquest of North China and pushed westward into Central Asia and Eastern Europe. He also introduced administrative reforms that improved governance across newly conquered regions. By the time of his death in 1241, Mongol armies had reached as far as Hungary, marking the empire's transformation into a vast transcontinental power.
+                <br />
+                <br />
+                  Ogödei
+                   <br />
                 <br />
 
+                What the Mongols brought: The Mongols brought the first Postal service, passports, new maps, the development of science, a network of exchange and production on a previously unseen scale, connecting regions from China to the Middle East, Africa and Europe, paper money, a policy of religious tolerance, The first International Law and the transfer of technology across the whole of Eurasia.
+                 <br />
+                <br />
+                Additional Resouces: <Link href="https://www.bloomsbury.com/uk/empires-of-the-steppes-9781526630445/" target="_blank" className="text-decoration-underline text-[#ff0000] font-semibold md:font-semibold underline">Empires of the Steppes</Link> by Kenneth W. Harl. <Link href="https://www.penguin.co.uk/books/283044/the-secret-history-of-the-mongols-by-none/9780241197912" target="_blank" className="text-decoration-underline text-[#ff0000] font-semibold md:font-semibold underline">The Secret History of the Mongols</Link> by Kenneth W. Harl.
+
+
+                <br />
+                <br />
                 <div className="p-4 font-semibold md:font-semibold text-center text-md text-[#477ca5] border-gray-800 border-solid border-2 bg-white ">Click on the image below to open the album of our  visit to the Genghis Khan: How the Mongols Changed the World Exhibition at the Leeds Armouries and journey the Carlisle-Settle-Leeds Railway.
                   <br />
                   <br />
