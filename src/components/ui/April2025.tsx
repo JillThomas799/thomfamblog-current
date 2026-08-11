687b7c1f9391910a9c669ed2b1df7a68f31f3195
Jill <125391549+JillThomas799@users.noreply.github.com>
2026-08-11 17:20:40 +0100

    Work on April2025 blog transfer

--- src/components/ui/April2025.tsx
+++ src/components/ui/April2025.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import Image from 'next/image'
 import Link from 'next/link'
 import ArchieTitled03042025 from "/public/imagesresized/ArchieTitled03042025.jpg";
+import Derwentwater06042025Titled from "/public/imagesresized/Derwentwater06042025Titled.jpg";
+import CdSTrainTitled07042025 from "/public/imagesresized/CdSTrainTitled07042025.jpg";
+import HighCupNickTitled from "/public/imagesresized/HighCupNickTitled.jpg";
 
 
 export default function April2025() {
@@ -113,15 +116,319 @@ export default function April2025() {
                 >
                   <Image
                     src={ArchieTitled03042025}
-                    className="block ml-auto mr-auto h-auto w-[100%] md:w-[70%] lg:w-[50%] border-solid border-[#f6f0a3] border-4 mt-4 mb-6"
-                    alt="Photo of the waves crashing on to St Bees beach."
+                    className="block ml-auto mr-auto h-auto w-[100%] md:w-[70%] lg:w-[40%] border-solid border-[#f6f0a3] border-4 mt-4 mb-6"
+                    alt="Photo of Archie the dog."
                   />
                 </Link>
                   </div>   
 
 
-          
+ < div className="mb-20">
+          < div className="flex flex-col w-auto leading-1.5 pt-4 mt-4 md:p-4 lg:p-4  border-gray-800 border-solid border-2 md:border-2 bg-[#36454f]">
+            < div className="text-md md:text-base max-w-full font-normal md:font-semibold text-[#36454f] text-left md:text-center dark:text-white justify-center items-center">
+              <div className="font-semibold md:font-semibold p-2 m-2 text-[#36454f] text-center  border-gray-800 border-2 bg-[#f6f0a3]">6th April 2025</div> {/*End of news date block*/}
+                <div className="font-semibold md:font-semibold p-2 m-2 text-[#36454f] text-center text-md border-gray border-2  bg-[#f6f0a3]">Camino de Santiago Training Week 10 Day 1. Derwentwater Circuit 11
+            miles, 4 hrs 40{" "}</div>
+              <div className="font-normal md:font-normal text-left text-[#36454f] border-solid border-gray-800 border-2 mr-1 ml-1 mt-2 px-4 py-2 mb-10 bg-[#f6f0a3]">
+           We are now on week 10 of the Camino de Santiago training and the
+            schedule for this week is two walks of 60 mins . One long walk of
+            3.5 hrs and one long walk of 4.5 hrs walked on consecutive days (to
+            build up stamina). For the 4.5 hour walk we decided on the
+            Derwentwater Circuit starting from Keswick as we had not walked this
+            route for quite some time. It was the weekend and the weather
+            forecast was for a sunny, lovely day so Elena and Harry decided to
+            join us. It was an early start then to pick up Elena from her home,
+            which was probably as well as Bernard was so busy listening to the
+            Spanish CD to further improve his Spanish, that we missed the
+            Penrith turn and ended up having to go to Shap, then come back again;
+            a 20 mile detour in total!
+            <br />
+            <br />
+            In spite of the delay, we were in Keswick by 9.37am, so were
+            fortunately able to find parking for free on the Heads. Firstly, it
+            was off towards Friar's Crag with good views towards Derwent Isle on
+            our right, the now-named little island just off shore where
+            apparently some of the German community lived who came over as
+            copper miners during the 16th century when they were invited by
+            Elizabeth I to help her find the minerals urgently needed for
+            building cannons and minting coins. The German workers were known
+            for their skill and helped bring prosperity to the area but even so,
+            they decided to establish their own, relatively safe settlement a
+            little part from the discord between the immigrant workers and the
+            locals which was happening at that time. The centre of the copper
+            and lead mining industry was in Keswick and was under the control of
+            Company of Mines Royal.
+            <br />
+            <br />
+            Then it was round into Strandshag Bay and on around the lake. This
+            path has been vastly improved over the years although there was one
+            tricky point where in the storms a number of trees had come down and
+            Bernard had (so he says) to launch into his 'Uncharted' Nathan Drake
+            impression, hopping along the tree trunks! The path across the
+            bottom of the lake at Manesty was entirely passable today but after
+            heavy rain and especially in the Winter months the boardwalks can be
+            submerged under water and are impassable. In that case, an
+            alternative route bu the road and through Grange village is
+            available.
+            <br />
+            <br />
+            As the day progressed it became busier as it was a beautiful spring
+            day and a Sunday, but it was a great walk enjoyed by all and turned
+            out to be a good choice for the first of our long walks this week.
+            <br />
+            <br />                  
+              </div>{" "}
+            </div>
         
+
+           <div className="p-4 font-semibold md:font-semibold text-center text-md text-[#18392b] border-gray-800 border-solid border-2 bg-[#f6f0a3] ">Click on the image below to open the album of our Camino de Santiago Training Week 10 Day 1. Derwentwater Circuit Walk.
+                  <br />
+                  <br />
+                  Please make sure to click on the <em>'i'</em> icon, in the top right-hand corner of each photo, as we have included a lot of additional information about the photo in that space.
+                </div>
+                <Link
+                  href="https://photos.app.goo.gl/u6kt9noMYQ7Yu6Aa9"
+                  target="_blank"
+                >
+                  <Image
+                    src={Derwentwater06042025Titled}
+                    className="block ml-auto mr-auto h-auto w-[100%] md:w-[70%] lg:w-[50%] border-solid border-[#f6f0a3] border-4 mt-4 mb-6"
+                    alt="Photo of Jill on Derwentwater Circuit Walk."
+                  />
+                </Link>
+                  </div>   
+
+
+      < div className="mb-20">
+          < div className="flex flex-col w-auto leading-1.5 pt-4 mt-4 md:p-4 lg:p-4  border-gray-800 border-solid border-2 md:border-2 bg-[#36454f]">
+            < div className="text-md md:text-base max-w-full font-normal md:font-semibold text-[#36454f] text-left md:text-center dark:text-white justify-center items-center">
+              <div className="font-semibold md:font-semibold p-2 m-2 text-[#36454f] text-center  border-gray-800 border-2 bg-[#f6f0a3]">7th April 2025</div> {/*End of news date block*/}
+                <div className="font-semibold md:font-semibold p-2 m-2 text-[#36454f] text-center text-md border-gray border-2  bg-[#f6f0a3]"> Camino de Santiago Training Week 10 Day 2. Walk in the locality 8
+            miles, 3 hours 40{" "}</div>
+              <div className="font-normal md:font-normal text-left text-[#36454f] border-solid border-gray-800 border-2 mr-1 ml-1 mt-2 px-4 py-2 mb-10 bg-[#f6f0a3]">
+          It was off again today for our 2nd long walk of week 10; this time
+            the target was 3hrs 30 walking. To enable a slower start to the day,
+            we decided to walk locally but, as a means of opening up new,
+            undiscovered routes, to undertake a route we had not been aware of
+            previously. In actual fact, the longer the routes we are
+            undertaking, the more we are having to do this and in so doing
+            finding lots of interesting paths and ways we had not known existed!
+            The OS app is great for this sort of thing as you can just search on
+            your location and you will be able to see the available local
+            footpaths. The good news was that the good weather was holding out
+            really well so there were set to be some lovely North Pennine
+            views....
+            <br />
+            <br />
+            Today's walk took us out up towards the local fell (with Bernard
+            having to become Nathan Drake ... yet again ... to undertake a
+            rather tricky circumnavigation of a fallen tree) before cutting down
+            and across the river and up again into the North Pennine landscape
+            (see album below.) The plan was to walk for about 1 hr 30 and then
+            obtain a drink at the pub in the village. However, disaster! Well...
+            not really, it just felt like that. We discovered that the very nice
+            pub in the village is not open on Mondays or Tuesdays, so we had to
+            have only a brief rest before continuing on our way....
+            <br />
+            <br />
+            This lack of a pub stop was soon made up for though by Bernard being
+            befriended by two very young lambs who seemed to think he was the
+            local farmer (must be the grey hair, weary gait and frayed gardening
+            cords!- these were Bernard's words about himself...) See the album
+            below for the extremely cute photos of these lambs. Just as exciting
+            was after this when we noticed that one of the sheep was just giving
+            birth to a newborn lamb in a nearby field.
+            <br />
+            <br />
+            It was a lovely afternoon, brilliant sunshine and everywhere looking
+            pretty. The wood anenomes were in flower. After 2hrs 45 we made it
+            back as far as out local pub, which was open... So we stopped in for
+            a well-deserved drink, before commencing the final section back
+            home.
+        
+            <br />
+            <br />                  
+              </div>{" "}
+            </div>
+        
+
+           <div className="p-4 font-semibold md:font-semibold text-center text-md text-[#18392b] border-gray-800 border-solid border-2 bg-[#f6f0a3] ">Click on the image below to open the album of our  Camino de Santiago Training Week 10 Day 2. Walk in the locality.
+                  <br />
+                  <br />
+                  Please make sure to click on the <em>'i'</em> icon, in the top right-hand corner of each photo, as we have included a lot of additional information about the photo in that space.
+                </div>
+                <Link
+                  href="https://photos.app.goo.gl/Esya5J9GRB4t3paBA"
+                  target="_blank"
+                >
+                  <Image
+                    src={CdSTrainTitled07042025}
+                    className="block ml-auto mr-auto h-auto w-[100%] md:w-[70%] lg:w-[40%] border-solid border-[#f6f0a3] border-4 mt-4 mb-6"
+                    alt="Photo of Jill on Derwentwater Circuit Walk."
+                  />
+                </Link>
+                  </div>   
+
+
+      < div className="mb-20">
+          < div className="flex flex-col w-auto leading-1.5 pt-4 mt-4 md:p-4 lg:p-4  border-gray-800 border-solid border-2 md:border-2 bg-[#36454f]">
+            < div className="text-md md:text-base max-w-full font-normal md:font-semibold text-[#36454f] text-left md:text-center dark:text-white justify-center items-center">
+              <div className="font-semibold md:font-semibold p-2 m-2 text-[#36454f] text-center  border-gray-800 border-2 bg-[#f6f0a3]">10th April 2025, Thursday.</div> {/*End of news date block*/}
+                <div className="font-semibold md:font-semibold p-2 m-2 text-[#36454f] text-center text-md border-gray border-2  bg-[#f6f0a3]"> Walk up High Cup Nick from Dufton: 8 miles, 4 hours</div>
+              <div className="font-normal md:font-normal text-left text-[#36454f] border-solid border-gray-800 border-2 mr-1 ml-1 mt-2 px-4 py-2 mb-10 bg-[#f6f0a3]">
+            Today we decided to continue to make the most of the lovely weather
+            and undertake a walk we have undertaken previously and long-wanted
+            to repeat; that of High Cup Nick in the North Pennines. The walk is
+            8 miles in total by the there and back route although there are
+            other alternatives available should you wish. For example we could
+            have extended the walk up to Murton Point from the head of High Cup
+            Nick, but chose not to on this occasion. The walk involves 490
+            metres or 1608 feet of ascent. The route can be found in OS Explorer
+            Map OL19 Howgill Fells and Upper Eden Valley.
+                 <br />
+            <br /> 
+              </div>{" "}
+               <div className="bg-[#f6f0a3] text-[#36454f] font-bold">DETAILS OF THE WALK </div>
+            <div className="font-semibold text-center border-solid mr-1 ml-1 mt-2 mb-2 px-4 py-4 ">            
+        <div className="font-normal pt-2 pb-2  text-black text-center">
+             <ul className="list-none">
+              <li className="text-left bg-[#f6f0a3] p-4">
+                <div className="text-[#36454f]] font-semibold">Parking:</div>
+                  There is parking on the public car
+                  park in Dufton village. This can get full at busy times and
+                  this is a popular route, so it is worth arriving promptly.
+                  There are also bathrooms here provided by the community which
+                  is a very well-run service, so they welcome any donations
+                  towards keeping this going.
+              </li>
+             
+              <br />  
+        <br /> 
+         <li className="text-left bg-[#f6f0a3] p-4">
+                <div className="text-[#36454f]] font-semibold">Paths:</div>
+              The route consists of clear,
+                  well-signposted footpaths along the Pennine Way. It becomes a
+                  little rockier as you ascend further up, but nothing too
+                  challenging, so suitable hiking footwear is required.               
+              </li>
+              <br />  
+              <br /> 
+              <li  className="text-left  bg-[#f6f0a3] p-4">
+                <div className="text-[#36454f]]] font-semibold">Distance:</div>
+             8 miles in total. <strong> Ascent:</strong>490 metres, 1608 feet.
+              </li>
+               <br />  
+        <br /> 
+              <li  className="text-left bg-[#f6f0a3] p-4">
+                <div className="text-[#36454f]] font-semibold">Refreshments:</div>
+                The Stag Inn at Dufton
+                  currently opens on Saturdays and Sunday from 12 midday and
+                  during the week from 18.00 but it is worth checking the up to
+                  date opening times when you will be visiting as these times
+                  may change. There were no refreshments available in the
+                  village on the day and time of our visit.
+              </li>
+            </ul>
+             <br />  
+        <br /> 
+            <div className="bg-[#f6f0a3] text-[#36454f]] font-bold text-decoration-underline">GOOD</div>
+             <br />  
+        <br /> 
+        
+            <ul className="list-none">
+                <li className="text-left bg-[#f6f0a3] p-4">
+                   <div className="text-[#36454f] font-semibold">Spectacular landscape and views: </div>
+                     The geological interest of this glacially-cut valley makes it a
+                    site well-worth seeing. From a personal point of view, we
+                    prefer it to other sites of geological interest such as
+                    Malham, not least because High Cup Nick, whilst busy, is
+                    less busy than Malham. On the day we were walking we hardly
+                    saw a soul apart from a couple of farmers until high up on
+                    the ridge.{" "}                  
+                </li>
+                 <br />  
+        <br /> 
+    
+                <li className="text-left  bg-[#f6f0a3] p-4">
+                  <div className="text-[#36454f] font-semibold">Path : </div>
+                    The paths are clear, and well-marked.
+                </li>
+                 <br />  
+        <br /> 
+        <li className="text-left  bg-[#f6f0a3] p-4">
+                  <div className="text-[#36454f] font-semibold">Beautifully quiet: </div>
+                  See the comment above.
+                    It was beautifully quiet with hardly anyone about until
+                    later on in the day when it became a bit busier.
+                </li>
+                 <br />  
+        <br /> 
+         <li className="text-left  bg-[#f6f0a3] p-4">
+                  <div className="text-[#36454f] font-semibold">High level walk: </div>
+                    This is what we would
+                    call a proper high-level walk with the views to match.
+                    Choose a day where the weather is good if you can...
+                </li>
+                 <br />  
+        <br />                 
+            </ul>
+            <br />
+            <div className="bg-[#f6f0a3] text-[#36454f] font-bold text-decoration-underline">NOT SO GOOD</div>
+             <br />  
+            <ul className="list-none">
+              <li className="text-left  bg-[#f6f0a3] p-4">
+                <div className="text-[#36454f] font-semibold">  Refreshments:{" "}:</div>
+               ...or lack thereof..... Make sure you take plenty of snacks!{" "}
+                <br />
+              </li>
+               <br />  
+        <br /> 
+            </ul>
+             <div className="bg-[#f6f0a3] text-[#36454f] font-bold text-decoration-underline">HIKE RATING</div>
+              <br /> 
+          <ul className="list-none">
+              <li className="text-left bg-[#f6f0a3] p-4">
+                <div className="text-[#36454f]] text-center font-semibold">10 (out of 10):</div>  A fantastic high level walk
+                  which amazing views. Well-worth undertaking. One of our
+                  favourite Pennine walks, not least because, unlikely many high
+                  Pennine walks, it is not too bad underfoot!
+              </li>
+            </ul>
+          </div>{" "}
+            </div>
+            </div>
+        
+
+           <div className="p-4 font-semibold md:font-semibold text-center text-md text-[#18392b] border-gray-800 border-solid border-2 bg-[#f6f0a3] ">Click on the image below to open the album of our climb up High Cup Nick in the Cumbrian Pennines.
+                  <br />
+                  <br />
+                  Please make sure to click on the <em>'i'</em> icon, in the top right-hand corner of each photo, as we have included a lot of additional information about the photo in that space.
+                </div>
+                <Link
+                  href="https://photos.app.goo.gl/7gzVDPtJAwECawWt8"
+                  target="_blank"
+                >
+                  <Image
+                    src={HighCupNickTitled}
+                    className="block ml-auto mr-auto h-auto w-[100%] md:w-[70%] lg:w-[40%] border-solid border-[#f6f0a3] border-4 mt-4 mb-6"
+                    alt="Photo of High Cup Nick."
+                  />
+                </Link>
+                  </div>  
+
+
+                   
+
+ 
+
+
+
+
+
+   
+        </div>  
+        </div>     
+      </div>   
      </div>
     </div>
   );
