137ff36fcbc3dcd9616493d063fb944d4e17d13f
Jill <125391549+JillThomas799@users.noreply.github.com>
2026-08-14 20:59:13 +0100

    Work on March2025 blog transfer

--- src/components/ui/March2025.tsx
+++ src/components/ui/March2025.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import Image from 'next/image'
 import Link from 'next/link'
-import "./March2025.css";
 import Pheasant from "./pheasant";
-import pheasantGarden from "/public/imagesresized/pheasantGarden.jpg";
+import BishopsAuckland01032025Titled from "/public/imagesresized/BishopsAuckland01032025Titled.jpg";
+import SuttonHooTitled from "/public/imagesresized/SuttonHooTitled.jpg";
 
 
 
@@ -11,7 +11,7 @@ export default function March2025() {
   return (
      <div>
       <div className="bg-Garden3March2025 bg-cover border-solid border-8 border-[#36454f] md:p-4 mb-8 h-[40vh] md:h-[50vh] w-auto lg:h-[100vh] text-gray-800 text-center font-normal text-l p-3">
-        <div className="bg-[#f6f0a3] mr-[7%] ml-[7%] md:mr-[40%] md:ml-[40%] p-1 text-lg border-gray-800 border-2 text-[#36454f] font-bold">March 2025</div>
+        <div className="bg-[#816c66] mr-[7%] ml-[7%] md:mr-[40%] md:ml-[40%] p-1 text-lg border-gray-800 border-2 text-white font-bold">March 2025</div>
         <br />
         <br />
        <Pheasant />
@@ -19,10 +19,10 @@ export default function March2025() {
 
  < div className="mb-20">
           < div className="flex flex-col w-auto leading-1.5 pt-4 mt-4 md:p-4 lg:p-4  border-gray-800 border-solid border-2 md:border-2 bg-[#36454f]">
-            < div className="text-md md:text-base max-w-full font-normal md:font-semibold text-[#000080] text-left md:text-center dark:text-white justify-center items-center">
-              <div className="font-semibold md:font-semibold p-2 m-2 text-[#36454f] text-center  border-gray-800 border-2 bg-[#f6f0a3]">Welcome to our March blog 2025</div> {/*End of news date block*/}
+            < div className="text-md md:text-base max-w-full font-normal md:font-semibold text-white text-left md:text-center dark:text-white justify-center items-center">
+              <div className="font-semibold md:font-semibold p-2 m-2 text-white text-center  border-gray-800 border-2 bg-[#816c66]">Welcome to our March blog 2025</div> {/*End of news date block*/}
               
-              <div className="font-normal md:font-normal text-left text-[#36454f] border-solid border-gray-800 border-2 mr-1 ml-1 mt-2 px-4 py-2 mb-10 bg-[#f6f0a3]">
+              <div className="font-normal md:font-normal text-left text-white border-solid border-gray-800 border-2 mr-1 ml-1 mt-2 pt-6 px-4 py-2 mb-10 bg-[#816c66]">
            Apologies for the delay in posting this month's blog but we have been
           very busy. Not with anything very interesting I am afraid, just a
           number of more difficult routine matters which have taken a period of
@@ -44,29 +44,192 @@ export default function March2025() {
           complicated and there is still more work to be done but I thought I
           would get this update posted and I can continue to work on improving
           the design as the month continues....
-          <Image 
-          src={pheasantGarden}
-          className="block ml-auto mr-auto h-auto w-[100%] md:w-[70%] lg:w-[50%] border-solid border-[#bbe89e] border-4 mt-4 mb-6"
-          alt="Photo of Pheasant." />
           <br />
-          <br />
-          <div style={{ textAlign: "center" }}>
+          <br />          
             Over the coming few weeks we will be working to update the March
-            blog with our news .....
+            blog with our news .....       
+          <br />
+          <br />       
           </div>
+           </div>
+          </div>
+           </div>
+
+          < div className="mb-20">
+          < div className="flex flex-col w-auto leading-1.5 pt-4 mt-4 md:p-4 lg:p-4  border-gray-800 border-solid border-2 md:border-2 bg-[#36454f]">
+            < div className="text-md md:text-base max-w-full font-normal md:font-semibold text-white text-left md:text-center dark:text-white justify-center items-center">
+              <div className="font-semibold md:font-semibold p-2 m-2 text-white text-center  border-gray-800 border-2 bg-[#816c66]">1st March 2025</div> {/*End of news date block*/}
+                <div className="font-semibold md:font-semibold p-2 m-2 text-white text-center text-md border-gray border-2  bg-[#816c66]">   Visit to Aucklands Palace and Bishops Auckland</div>
+              <div className="font-normal md:font-normal text-left text-white border-solid border-gray-800 border-2 mr-1 ml-1 mt-2 px-4 py-2 mb-10 bg-[#816c66]">
+           Auckland Palace was the residence of the Prince Bishops of Durham from
+          approximately 1183 and was their primary residence from 1832 to 2012
+          when the Castle and its contents were sold to the Auckland Castle
+          Trust, now known as the Auckland Project. It still houses the Bishop's
+          offices. It is one of the best-preserved Bishop's palaces in Europe.
           <br />
           <br />
-       
+          The displays and information boards throughout the Palace provide
+          information about the individual Bishops in a chronological order.
+          Auckland Palace was only the home of the individual Prince Bishop of
+          Durham for as long as they held office, it did not belong to them and
+          could not be passed on to their offspring.
+          <br />
+          <br />
+          The status of the Prince Bishops was unique as they had a social rank
+          on a par with a duke and political power exceeded only by the monarch.
+          <br />
+          <br />
+          This was, although a long(ish) drive, an enjoyable day out as we had
+          never visited before. We used our historic houses membership for
+          entry. This gained us entry to the house but not to the tower and all
+          the exhibitions.
+          <br />
+          <br />
+          The Palace displays are laid out in a very interesting manner with
+          plenty of helpful information boards. The 12 paintings by Zurbarán of
+          Jacob and his sons (well, 9 were present actually as Joseph and Asher
+          have gone to the National Gallery in London for conservation work, and
+          the painting of Benjamin is a copy), are very impressive and are hung
+          in the long dining room.
+                <br />
+                <br />                    
+              </div>{" "}
+            </div>
+
+         <div className="p-4 font-semibold md:font-semibold text-center text-md text-white border-gray-800 border-solid border-2 bg-[#816c66] ">Click on the image below to open the album of our visit to Aucklands Palace and Bishops Auckland.
+                  <br />
+                  <br />
+                  Please make sure to click on the <em>'i'</em> icon, in the top right-hand corner of each photo, as we have included a lot of additional information about the photo in that space.
+                </div>
+                <Link
+                  href="https://photos.app.goo.gl/GpNxZCMBHJB2JfmE7"
+                  target="_blank"
+                >
+                  <Image
+                    src={BishopsAuckland01032025Titled}
+                    className="block ml-auto mr-auto h-auto w-[100%] md:w-[70%] lg:w-[40%] border-solid border-[#f5f5f5] border-4 mt-4 mb-6"
+                    alt="Photo of Archie the dog."
+                  />
+                </Link> 
           </div>
           </div>
+
+           < div className="mb-20">
+          < div className="flex flex-col w-auto leading-1.5 pt-4 mt-4 md:p-4 lg:p-4  border-gray-800 border-solid border-2 md:border-2 bg-[#36454f]">
+            < div className="text-md md:text-base max-w-full font-normal md:font-semibold text-white text-left md:text-center dark:text-white justify-center items-center">
+              <div className="font-semibold md:font-semibold p-2 m-2 text-white text-center  border-gray-800 border-2 bg-[#816c66]">13th - 16th March 2025</div> {/*End of news date block*/}
+                <div className="font-semibold md:font-semibold p-2 m-2 text-white text-center text-md border-gray border-2  bg-[#816c66]">Visit to relatives down South including a day out to Sutton Hoo,
+          Suffolk.</div>
+              <div className="font-normal md:font-normal text-left text-white border-solid border-gray-800 border-2 mr-1 ml-1 mt-2 pt-6 px-4 py-2 mb-10 bg-[#816c66]">
+            We have just returned from a visit down to the South and South East
+          visiting relatives, and whilst there took the opportunity to go and
+          visit{" "}
+          <Link
+            href="https://www.nationaltrust.org.uk/visit/suffolk/sutton-hoo"
+            className="LinkMarch2025"
+          >
+            {" "}
+            Sutton Hoo{" "}
+          </Link>{" "}
+          in Suffolk. It is a really interesting location and well worth a visit
+          for anyone who has not been. We lasted visited the site back in 2009
+          and had been meaning to revisit for sometime but unfortunately it is
+          only open at weekends over the winter period, so this was the first
+          opportunity that we had had.
+          <br />
+          <br />
+          I will not write too much here about the history of Sutton Hoo and the
+          findings there because there is plenty of information on the site
+          accessed by the link above and online, suffice it to say that Sutton
+          Hoo is the site of an Anglo-Saxon burial ground dating back to the 6th
+          and 7th centuries. Excavations in 1939, just as World War 2 was
+          starting, revealed an Anglo-Saxon ship burial containing many
+          treasures in what came to be called 'Mound 1'. It is thought, due to
+          the riches found buried there, that it was most likely the grave of
+          Raedwald, an east Anglian king who died in the 620s. This is one of
+          only three Anglo-Saxon ship burials in this country. The second and
+          lesser ship burial was found at Sutton Hoo in Mound Two and the other
+          was found in nearby Snape.
+          <br />
+          <br />
+          One of the conditions of the National Trust owning the Sutton Hoo site
+          is that the site has to have an educational purpose, so we had a
+          fantastic local guide Catriona who was one of the original excavating
+          team of Mound 2 back in the 1980s. She was very interesting and
+          entertaining in our hour long talk and it was fascinating to hear
+          about life on the dig back then. If anyone is a Time Team fan,{" "}
+          <Link
+            href="https://www.nationaltrust.org.uk/visit/suffolk/sutton-hoo/time-team-announcement"
+            target="_blank"
+           className="text-decoration-underline text-[#0000EE] font-semibold md:font-semibold underline"
+          >
+            {" "}
+            Time Team{" "}
+          </Link>{" "}
+          are back on the site from the 19th May 2025 - 13th June 2025.
+          <br />
+          <br />
+          Following on from our visit to Sutton Hoo we caught up with the 2021
+          film{" "}
+          <Link
+            href="https://www.nationaltrust.org.uk/visit/suffolk/sutton-hoo/digging-the-dirt-at-sutton-hoo-the-true-story-behind-the-dig"
+            target="_blank"
+            className="text-decoration-underline text-[#0000EE] font-semibold md:font-semibold underline"
+          >
+            'The Dig'
+          </Link>{" "}
+          starring Ralph Fiennes as the archeologist Basil Brown exploring the
+          story of the excavation of the Great Ship Burial at Sutton Hoo in 1939
+          and which is currently available on Netflix. It is an enjoyable film
+          in its own right which, whilst it is based on a true story and
+          accurately portrays the Sutton Hoo excavation, it is a also in parts a
+          fictionalized account with liberties taken for dramatic purposes
+          especially with the relationships between certain of the characters
+          and the addition of fictional characters such as Rory Lomax who were
+          added for dramatic effect. We felt that it was a pity it was not able
+          to be filmed on site using Tranmer House. There is an interesting
+          article
+          <Link
+            href="https://www.eadt.co.uk/news/21324495.ralph-fiennes-basil-brown-remarkable-man/"
+            target="_blank"
+           className="text-decoration-underline text-[#0000EE] font-semibold md:font-semibold underline"
+          >
+            {" "}
+            here{" "}
+          </Link>{" "}
+          in the East Anglian Times about Ralph Fiennes when he was preparing
+          for the film.
+          <br />
+          <br />
+          Whilst visiting in the South East, we were also able to catch up on
+          progress with Jill's relatives garden project, which is making good
+          progress.
+                <br />
+                <br />  
+                  
+              </div>{" "}
+            </div>
+         <div className="p-4 font-semibold md:font-semibold text-center text-md text-white border-gray-800 border-solid border-2 bg-[#816c66] ">Click on the image below to open the album of our visit to Sutton Hoo and the update of the S.E garden project.
+                  <br />
+                  <br />
+                  Please make sure to click on the <em>'i'</em> icon, in the top right-hand corner of each photo, as we have included a lot of additional information about the photo in that space.
+                </div>
+                <Link
+                  href="https://photos.app.goo.gl/512sy4tKr39WbLVP7"
+                  target="_blank"
+                >
+                  <Image
+                    src={SuttonHooTitled}
+                    className="block ml-auto mr-auto h-auto w-[100%] md:w-[70%] lg:w-[40%] border-solid border-[#f5f5f5] border-4 mt-4 mb-6"
+                    alt="Photo of Archie the dog."
+                  />
+                </Link>
+                 
+ 
+
+
           </div>
           </div>
-
-
-
-
-
-
 </div> 
  
   );
