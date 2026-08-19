393e1204bde8f85391113f2c6d71e64d300ad6c3
Jill <125391549+JillThomas799@users.noreply.github.com>
2026-08-19 17:27:24 +0100

    Addition of February 2025 blog tansfer

---
+++ src/components/ui/February2025.tsx
@@ -0,0 +1,251 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import "./February2026.css"; 
+import Deer from "./deer";
+import GawainArrivalTitled from "/public/imagesresized/GawainArrivalTitled.jpg";
+import TullieGladiatorsTitled from "/public/imagesresized/TullieGladiatorsTitled.jpg";
+import ShroveTuesday from "/public/imagesresized/ShroveTuesday.jpg";
+import Ullswater20022026Titled from "/public/imagesresized/Ullswater20022026Titled.jpg";
+import GawainExploreTitled06022026 from "/public/imagesresized/GawainExploreTitled06022026.jpg";
+import Audience from "/public/imagesresized/Audience.jpg";
+import StormySteamer from "/public/imagesresized/StormySteamer.jpg";
+import Lamb1 from "/public/imagesresized/Lamb1.jpg";
+import Lamb2 from "/public/imagesresized/Lamb2.jpg";
+import StitchIt from "/public/imagesresized/StitchIt.jpg";
+
+
+export default function February2025() {
+  return ( <div>
+ <div id="snowScene" className=" bg-black border-solid border-4 border-gray-600  p-2 md:p-4  height-[100vh]"> 
+ <div className="text-gray-600 bg-[#edeaea] border-solid border-2 mr-auto ml-auto mt-0 p-2 border-gray-600  font-bold text-xl text-center">February 2026</div>  
+ <div className="scale-[70%] translate-x-[-10%] translate-y-[-30%] md:scale-[82%] md:translate-x-[-30%] md:translate-y-0 lg:scale-[85%] lg:translate-x-[-20%]"><Deer/> </div>          
+                  <br />
+                  <br />
+             </div> 
+< div className="mb-20"> 
+      < div className="flex flex-col w-auto leading-1.5 p-2 md:p-4 lg:p-4  mb-4 mt-4 border-gray-800 border-solid border-2 md:border-2 bg-[#bdbeba]">
+         < div className="text-md md:text-lg max-w-full font-semibold text-[#f3e2d6] text-center dark:text-[#f3e2d6] justify-center items-center">
+        <div className="font-semibold text-center border-solid border-black border-2 mr-1 ml-1 mt-2 mb-2 px-4 py-4 bg-[#786d68]">Welcome to our February 2025 blog.
+          <br />
+          <br />
+          
+            </div>{" "}
+        </div> 
+         </div> 
+          </div> 
+
+           <div className="mb-20">
+          <div className="flex flex-col max-w-full leading-1.5 p-1 md:p-2 lg:p-4 m-4 border-gray-800 border-solid border-2 bg-[#bdbeba]">
+            <div className="text-md md:text-lg max-w-full font-normal mt-4 mb-4 p-2 text-white text-center md:text-left justify-center items-center bg-[#786d68]">
+
+              <div className="font-semibold pt-2 pb-2 mr-[30%] ml-[30%]  text-[#f3e2d6] text-center  border-gray-800 border-solid border-2 bg-[#5d5450]"> 2nd February 2026</div> {/*End of news date block*/}
+              <div className="font-bold m-4 pt-2 pb-4 py-2 md:px-4 text-[#f3e2d6] text-center  border-gray-800 border-solid border-2 bg-[#5d5450]"> The Arrival of Gawain, Elena and Willow's new cat</div>
+              <div className="text-[#f3e2d6] text-center md:text-left mb-4 md:*:pr-6 md:*:pl-6 ">
+                
+              </div>
+            </div>
+          </div>
+
+           <div className="mb-20">
+          <div className="flex flex-col max-w-full leading-1.5 p-1 md:p-2 lg:p-4 m-4 border-gray-800 border-solid border-2 bg-[#bdbeba]">
+            <div className="text-md md:text-lg max-w-full font-normal mt-4 mb-4 p-2 text-white text-center md:text-left justify-center items-center bg-[#786d68]">
+
+              <div className="font-semibold pt-2 pb-2 mr-[30%] ml-[30%]  text-[#f3e2d6] text-center  border-gray-800 border-solid border-2 bg-[#5d5450]"> 14th February 2026</div> {/*End of news date block*/}
+              <div className="font-bold m-4 pt-2 pb-4 py-2 md:px-4 text-[#f3e2d6] text-center  border-gray-800 border-solid border-2 bg-[#5d5450]"> Tullie Museum, Carlisle: Gladiators of Britain and Uncovering Roman Carlisle - Where Worlds Met Exhibition</div>
+              <div className="text-[#f3e2d6] text-center md:text-left mb-4 md:*:pr-6 md:*:pl-6 ">
+                
+                  <br />
+                 <br />                 
+                 <div className="m-1 pr-2 pl-2 pt-4 pb-4 font-semibold text-center border-gray-800 border-solid border-2 bg-[#5d5450] mb-6">Click on the image below to open the album <br /> of our visit to the Gladiators and Roman Carlisle Exhibition at the Tullie Museum, Carlisle.
+                <br />
+                <br />
+                      Please make sure to click on the <em>'i'</em> icon, in the top right-hand corner of each photo, as we have included a lot of additional information about the photo in that space.
+                   </div>
+                  <Link
+                    href="https://photos.app.goo.gl/4mXrnPR7wkTA8M55A"
+                    target="_blank"
+                  >
+                    <Image
+                      src={TullieGladiatorsTitled}
+                      className="block ml-auto mr-auto h-auto w-[90%] md:w-[70%] lg:w-[50%] border-solid border-black-400 border-4 mt-4 mb-6"
+                      alt="Photo of Bernard and Harry outside Tullie Museum."
+                    />
+                  </Link>
+                 
+              </div>
+              
+  </div>
+            </div>
+          </div>
+
+           <div className="mb-20">
+          <div className="flex flex-col max-w-full leading-1.5 p-1 md:p-2 lg:p-4 m-4 border-gray-800 border-solid border-2 bg-[#bdbeba]">
+            <div className="text-md md:text-lg max-w-full font-normal mt-4 mb-4 p-2 text-white text-center md:text-left justify-center items-center bg-[#786d68]">
+
+              <div className="font-semibold pt-2 pb-2 mr-[30%] ml-[30%]  text-[#f3e2d6] text-center  border-gray-800 border-solid border-2 bg-[#5d5450]"> 17th February 2026</div> {/*End of news date block*/}
+              <div className="font-bold m-4 pt-2 pb-4 py-2 md:px-4 text-[#f3e2d6] text-center  border-gray-800 border-solid border-2 bg-[#5d5450]"> Shrove Tuesday - Time for homemade pancakes....</div>
+              <div className="text-[#f3e2d6] text-center md:text-left mb-4 md:*:pr-6 md:*:pl-6 ">
+              
+               <br />
+              <br /> 
+              
+                    <Image
+                      src={ShroveTuesday}
+                      className="block ml-auto mr-auto h-auto w-[90%] md:w-[70%] lg:w-[50%]"
+                      alt="Photo of Harry eating pancakes."
+                    />
+
+                </div>
+            </div>
+          </div>
+
+</div>
+    
+            
+          <div className="mb-20">
+          <div className="flex flex-col max-w-full leading-1.5 p-1 md:p-2 lg:p-4 m-4 border-gray-800 border-solid border-2 bg-[#bdbeba]">
+            <div className="text-md md:text-lg max-w-full font-normal mt-4 mb-4 p-2 text-white text-center md:text-left justify-center items-center bg-[#786d68]">
+
+              <div className="font-semibold pt-2 pb-2 mr-[30%] ml-[30%]  text-[#f3e2d6] text-center  border-gray-800 border-solid border-2 bg-[#5d5450]"> 20th February 2026, Friday</div> {/*End of news date block*/}
+              <div className="font-bold m-4 pt-2 pb-4 py-2 md:px-4 text-[#f3e2d6] text-center  border-gray-800 border-solid border-2 bg-[#5d5450]"> Today we were meeting up with friends at Pooley Bridge, Ullswater</div>
+              <div className="text-[#f3e2d6] text-center md:text-left mb-4 md:*:pr-6 md:*:pl-6 ">
+              
+                </div>
+            </div>
+          </div>
+</div>
+
+  <div className="mb-20">
+          <div className="flex flex-col max-w-full leading-1.5 p-1 md:p-2 lg:p-4 m-4 border-gray-800 border-solid border-2 bg-[#bdbeba]">
+            <div className="text-md md:text-lg max-w-full font-normal mt-4 mb-4 p-2 text-white text-center md:text-left justify-center items-center bg-[#786d68]">
+
+              <div className="font-semibold pt-2 pb-2 mr-[30%] ml-[30%]  text-[#f3e2d6] text-center  border-gray-800 border-solid border-2 bg-[#5d5450]"> 21st February 2026, Saturday</div> {/*End of news date block*/}
+              <div className="font-bold m-4 pt-2 pb-4 py-2 md:px-4 text-[#f3e2d6] text-center  border-gray-800 border-solid border-2 bg-[#5d5450]"> Meeting Elena's partner, Willow's family for the first time.</div>
+              <div className="text-[#f3e2d6] text-center md:text-left mb-4 md:*:pr-6 md:*:pl-6 ">
+              
+               <br />
+              <br /> 
+               <Image
+                      src={ StormySteamer }
+                      className="block ml-auto mr-auto h-auto w-[90%] md:w-[70%] lg:w-[50%]"
+                      alt="Photo Ullswater steamer in stormy weather"
+                    />
+                </div>
+            </div>
+          </div>
+           </div>
+
+<div className="mb-20">
+          <div className="flex flex-col max-w-full leading-1.5 p-1 md:p-2 lg:p-4 m-4 border-gray-800 border-solid border-2 bg-[#bdbeba]">
+            <div className="text-md md:text-lg max-w-full font-normal mt-4 mb-4 p-2 text-white text-center md:text-left justify-center items-center bg-[#786d68]">
+
+              <div className="font-semibold pt-2 pb-2 mr-[30%] ml-[30%]  text-[#f3e2d6] text-center  border-gray-800 border-solid border-2 bg-[#5d5450]"> 23rd February 2026</div> {/*End of news date block*/}
+              <div className="font-bold m-4 pt-2 pb-4 py-2 md:px-4 text-[#f3e2d6] text-center  border-gray-800 border-solid border-2 bg-[#5d5450]"> Time for a Gawain Update....</div>
+              <div className="text-[#f3e2d6] text-center md:text-left mb-4 md:*:pr-6 md:*:pl-6 ">
+             ns rather wary and is reluctant to be picked up, but he is making steady progress, which is the main thing....
+              <br />
+              <br /> 
+               <div className="m-1 pr-2 pl-2 pt-4 pb-4 font-semibold text-center border-gray-800 border-solid border-2 bg-[#5d5450] mb-6">Click on the image below to open the album of Gawain's progress settling into his new home.
+                <br />
+                <br />
+                      Please make sure to click on the <em>'i'</em> icon, in the top right-hand corner of each photo, as we have included a lot of additional information about the photo in that space.
+                   </div>
+                  <Link
+                    href="https://photos.app.goo.gl/Qra2tRZ6d575jxdq8"
+                    target="_blank"
+                  >
+                    <Image
+                      src={GawainExploreTitled06022026}
+                      className="block ml-auto mr-auto h-auto w-[90%] md:w-[70%] lg:w-[50%] border-solid border-black-400 border-4 mt-4 mb-6"
+                      alt="Photo of Bernard and Harry outside Tullie Museum."
+                    />
+                  </Link>
+                  </div> 
+                    </div>
+                     </div>
+          </div>
+
+
+                 
+
+
+                   <div className="mb-20">
+          <div className="flex flex-col max-w-full leading-1.5 p-1 md:p-2 lg:p-4 m-4 border-gray-800 border-solid border-2 bg-[#bdbeba]">
+            < div className="text-md md:text-lg max-w-full font-normal mt-4 mb-4 p-2 text-white text-center md:text-left justify-center items-center bg-[#786d68]">
+
+              <div className="font-semibold pt-2 pb-2 mr-[30%] ml-[30%]  text-[#f3e2d6] text-center  border-gray-800 border-solid border-2 bg-[#5d5450]"> 26th February 2026, Thursday</div> {/*End of news date block*/}
+              <div className="font-bold m-4 pt-2 pb-4 py-2 md:px-4 text-[#f3e2d6] text-center  border-gray-800 border-solid border-2 bg-[#5d5450]"> National Theatre Live screening of 'The Audience' starring Helen Mirren at Rheged.</div>
+              <div className="text-[#f3e2d6] text-center md:text-left mb-4 md:*:pr-6 md:*:pl-6 ">
+              An evening at the Rheged Centre today to see the live screening from the National Theatre of Helen Mirren starring in <Link href="https://www.ntlive.com/plays/the-audience/#synopsis" target="_blank" className="text-decoration-underline text-white font-semibold underline">'The Audience'</Link> which was good.
+               <br />
+              <br /> 
+              
+                    <Image
+                      src={ Audience }
+                      className="block ml-auto mr-auto h-auto w-[90%] md:w-[70%] lg:w-[50%]"
+                      alt="Photo interval screensaver 'The Audience'"
+                    />
+
+  </div>
+  </div>
+  </div>
+</div>
+
+
+                 <div className="mb-20">
+          <div className="flex flex-col max-w-full leading-1.5 p-1 md:p-2 lg:p-4 m-4 border-gray-800 border-solid border-2 bg-[#bdbeba]">
+            < div className="text-md md:text-lg max-w-full font-normal mt-4 mb-4 p-2 text-white text-center md:text-left justify-center items-center bg-[#786d68]">
+
+              <div className="font-semibold pt-2 pb-2 mr-[30%] ml-[30%]  text-[#f3e2d6] text-center  border-gray-800 border-solid border-2 bg-[#5d5450]"> 27th February 2026, Friday</div> {/*End of news date block*/}
+              <div className="font-bold m-4 pt-2 pb-4 py-2 md:px-4 text-[#f3e2d6] text-center  border-gray-800 border-solid border-2 bg-[#5d5450]"> The first of the spring lambs ....</div>
+              <div className="text-[#f3e2d6] text-center md:text-left mb-4 md:*:pr-6 md:*:pl-6 ">
+             On Monday this week (23rd February 2026), we saw the first of the spring lambs near to the village. Today we walked over to see them close up. Here are a couple of them.....
+               <br />
+              <br /> 
+               <Image
+                      src={ Lamb1 }
+                      className="block ml-auto mr-auto h-auto w-[90%] md:w-[70%] lg:w-[50%]"
+                      alt="Photo first spring lamb"
+                    />
+ <br />
+              <br /> 
+                     <Image
+                      src={Lamb2 }
+                      className="block ml-auto mr-auto h-auto w-[90%] md:w-[70%] lg:w-[50%]"
+                      alt="Photo second spring lamb"
+                    />
+              
+                    
+  </div>
+  </div>
+  </div>
+</div>       
+
+<div className="mb-20">
+          <div className="flex flex-col max-w-full leading-1.5 p-1 md:p-2 lg:p-4 m-4 border-gray-800 border-solid border-2 bg-[#bdbeba]">
+            < div className="text-md md:text-lg max-w-full font-normal mt-4 mb-4 p-2 text-white text-center md:text-left justify-center items-center bg-[#786d68]">
+
+              <div className="font-semibold pt-2 pb-2 mr-[30%] ml-[30%]  text-[#f3e2d6] text-center  border-gray-800 border-solid border-2 bg-[#5d5450]"> 28th February 2026, Saturday</div> {/*End of news date block*/}
+              <div className="font-bold m-4 pt-2 pb-4 py-2 md:px-4 text-[#f3e2d6] text-center  border-gray-800 border-solid border-2 bg-[#5d5450]"> 'Stitch It, Don't Ditch it' Workshop at the Rheged Centre.</div>
+              <div className="text-[#f3e2d6] text-center md:text-left mb-4 md:*:pr-6 md:*:pl-6 ">
+              
+              <br />
+              <br /> 
+                    <Image
+                      src={ StitchIt }
+                      className="block ml-auto mr-auto h-auto w-[90%] md:w-[70%] lg:w-[50%]"
+                      alt="Photo 'Stitch It, Don't Ditch It' book"
+                    />
+
+  </div>
+  </div>
+  </div>
+</div>
+          
+
+        </div>{" "}
+
+          </div>  
+          
+            );
+            }
