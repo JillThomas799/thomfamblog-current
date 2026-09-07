fe34fda02fedaa49ef66e3a69828dea5c825038d
Jill <125391549+JillThomas799@users.noreply.github.com>
2026-09-07 19:52:59 +0100

    September 2026 blog intro

--- src/components/ui/September2026.tsx
+++ src/components/ui/September2026.tsx
@@ -7,17 +7,16 @@ export default function September2026() {
 
   return (   
  <div> 
-       <div className=" bg-HolyIslandCastle01092026 bg-cover h-[100vh] border-solid border-2 border-[#22311d] pb-28"> {/*September block background*/}
-        <div className="text-[#FBE9D0] text-center font-semibold text-2xl p-3 bg-[#714c9d] border-solid border-2 border-[#22311d] m-2">September 2026</div>
+       <div className=" bg-HolyIslandCastle01092026 bg-cover h-[80vh] w-auto border-solid border-[#836A58] border-8 pb-28"> {/*September block background*/}
+        <div className="text-gray-800 text-center font-semibold text-2xl p-3 bg-[#8d6fb0] border-solid border-4 border-[#836a58] m-2">September 2026</div>
           </div>
        
-      <div className="flex flex-col max-w-full leading-1.5 sm:p-2 md:p-2 lg:p-2 m-4 border-gray-800 border-solid border-2 bg-[#836A58]">
+      <div className="flex flex-col max-w-full leading-1.5 sm:p-2 md:p-2 lg:p-2 m-4 border-[#836A58] border-solid border-4 bg-[#d9d2cc]">
          <div className="text-md md:text-lg max-w-full font-bold text-[#244845] text-center dark:text-[#244845] justify-center items-center">
-        <div className="font-bold border-solid border-black border-2 mr-1 ml-1 mb-4 px-4 py-4 text-[#FBE9D0] bg-[#714c9d]">1st - 30th September 2026.</div> 
-          <div className="font-bold border-solid border-black border-2 mr-1 ml-1 mb-4 px-4 py-4 text-[#FBE9D0] bg-[#714c9d]">Welcome to our September 2026 blog.</div> 
+        <div className="font-semibold border-solid border-[#836A58] border-4 mr-1 ml-1 mb-4 px-2 py-2 text-gray-800 bg-[#8d6fb0]">1st - 30th September 2026.</div> 
+          <div className="font-semibold border-solid border-[#836A58] border-4 mr-1 ml-1 mb-2 px-4 py-2 text-gray-800 bg-[#8d6fb0]">Welcome to our September 2026 blog.</div> 
         
-           <div className="pr-1 pl-1 pb-2 md:pr-1 md:pl-1 font-semibold text-left text-[#FBE9D0] dark:text-[#FBE9D0]">
-            <br />
+           <div className="pr-1 pl-1 pb-2 md:pr-1 md:pl-1 font-semibold text-left text-gray-800 dark:text-gray-800">
             We start the month with Bernard's dad still being with us, so join us for our adventures with him to the Holy Island of Lindesfarne and Carlisle Cathedral.
            </div>       
            </div>          
@@ -25,12 +24,12 @@ export default function September2026() {
          
 
 
-            <div className="flex flex-col max-w-full leading-1.5 sm:p-2 md:p-2 lg:p-2 m-4 border-gray-800 border-solid border-2 bg-[#836A58]">
-         <div className="text-md md:text-lg max-w-full font-bold text-[#244845] text-center dark:text-[#244845] justify-center items-center">
-        <div className="font-bold border-solid border-black border-2 mr-1 ml-1 mb-4 px-4 py-4 text-[#FBE9D0] bg-[#714c9d]">1st September 2026.</div> 
-        <div className="font-bold border-solid border-black border-2 mr-1 ml-1 mb-4 px-4 py-4 text-[#FBE9D0] bg-[#714c9d]">Whistle-stop tour of Northumberland and visit to the Holy Island of Lindisfarne.</div> 
+            <div className="flex flex-col max-w-full leading-1.5 sm:p-2 md:p-2 lg:p-2 m-4 border-[#836A58] border-solid border-4 bg-[#d9d2cc]">
+         <div className="text-md md:text-lg max-w-full font-semibold text-[#244845] text-center dark:text-[#244845] justify-center items-center">
+        <div className="font-semibold border-solid border-[#836A58] border-4 mr-1 ml-1 mb-4 px-2 py-2 text-gray-800 bg-[#8d6fb0]">1st September 2026.</div> 
+        <div className="font-semibold border-solid border-[#836A58] border-4 mr-1 ml-1 mb-4 px-2 py-2 text-gray-800 bg-[#8d6fb0]">Whistle-stop tour of Northumberland and visit to the Holy Island of Lindisfarne.</div> 
         
-           <div className="pr-1 pl-1 pb-2 md:pr-1 md:pl-1 font-semibold text-left text-[#FBE9D0] dark:text-[#FBE9D0]">
+           <div className="pr-1 pl-1 pb-2 md:pr-1 md:pl-1 font-semibold text-left text-[#836A58] dark:text-gray-800">
             <div className="text-center"></div>
             <br />
             
