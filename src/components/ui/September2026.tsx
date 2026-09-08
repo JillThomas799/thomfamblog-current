38a95c63f5bf0a6939934ba2942a310ae8c3eb60
Jill <125391549+JillThomas799@users.noreply.github.com>
2026-09-08 15:31:47 +0100

    Work on 1st September 2026 blog

--- src/components/ui/September2026.tsx
+++ src/components/ui/September2026.tsx
@@ -30,9 +30,181 @@ export default function September2026() {
         <div className="font-bold border-solid border-[#436A6b] border-4 mr-1 ml-1 mb-4 mt-4 px-2 py-2 text-[#E7E1F0]  bg-[#8d6fb0]">1st September 2026.</div> 
         <div className="font-bold border-solid border-[#436A6b] border-4 mr-1 ml-1 mb-4 px-2 py-2 text-[#E7E1F0] bg-[#8d6fb0]">Whistle-stop tour of Northumberland and visit to the Holy Island of Lindisfarne.</div> 
         
-           <div className="pr-1 pl-1 pb-2 md:pr-1 md:pl-1 font-semibold text-left text-[#E7E1F0] dark:text-[#E7E1F0]">
-            <div className="text-center"></div>
+           <div className="pr-1 pl-1 pb-2 md:pr-1 md:pl-1 font-semibold text-left text-white dark:text-white">
+            <div className="text-center">Today was the big-day, the highlight of <em>'Operation Holy Island'</em>, it was time time to head over to the Holy Island of Lindisfarne. Causeway safe tide times for crossing for the day commenced at 9.40 am and lasted until about 15.35 so it meant a very early start!! 
+            <br/>
+            <br/>
+            The best weather was set to be in the morning so it was decided to drive out via the B6318 as far as Chollerford, before turning North East across country via Colwell, Little Bavington, Rothley, Netherwitton and Long Horsley to join the A1 south of Alnwick, before heading the further 29 miles to the causeway.
+            <br/>
+            <br/>
+            The B6318, known locally as the Military Road, is a historic 61-mile route in northern England that runs from Heddon-on-the-Wall in the east to Greenhead in the west The road closely follows the path of Hadrian's Wall, often running right alongside or directly over the top of Roman foundations. Though often mistaken for a Roman road due to its long, straight stretches, it was built between 1751 and 1757 to move military equipment. This followed transport failures by Hanoverian forces under Field Marshal George Wade during the 1745 Jacobite Rising. The road features long straight sections combined with sharp dips and blind summits that completely hide oncoming traffic, but we know it well and at the time we were going (between 7.30 and 8am), it was likely to be relatively quiet.
+            <br/>
+            <br/>
+            Before we went though, it was time to brush up on some important religious history.....
+            <br/>
+            <br/>
+            
+            <div className="bg-[#8EA6A6]">
+               <div className="bg-[rgb(67,106,107)] text-white text-center font-semibold p-1 md:p-6 border-[#f5f5f5] border-solid border-2">HISTORY OF THE HOLY ISLAND OF LINDISFARNE</div>
+           <div className="text-left md:text-left text-white md:font-semibold  p-1 md:p-6  border-2 md:border-4 border-solid border-[#786d68]"> The Holy Island of Lindisfarne is one of the most important centres of early
+            English Christianity.
             <br />
+            <br />
+            Christianity first came to Britain in Roman times in the 1st century
+            AD/CE, sitting alongside the existing religious cults of worshiping the
+            Pagan or Roman gods. It had few followers. In AD 306 the Roman Emperor Constantine 
+            ( <Link
+              href="https://www.yorkshiremuseum.org.uk/exhibition/constantius-yorks-forgotten-emperor/"
+              target="_blank"
+              className="text-decoration-underline text-[#FFC000] font-semibold underline"
+            >Constantius Chlorus</Link> father of Constantine the Great), converted on his deathbed to Christianity 
+            (his most important legacy). Christianity spread rapidly and by 314 AD, Britain had its own bishops and from 313 AD onwards, Christian worship was tolerated within the Roman Empire in Britannia. 
+            <br />
+            <br />
+            Between AD 407-443 Britain ceased to be part of the Roman Empire. Very quickly no real aspects of Roman culture were surviving, apart from some small pockets of Christianity and Latin being the language of the church. Beyond that, in relation to Roman religion, settlement types, language and legal system, that all went and by the end of the 6th century AD there was little remaining evidence.
+             <br />
+            <br />
+            From AD 410 or thereabouts, the Anglo-Saxons from Northern Germany and Southern Scandinavia (Denmark and the Netherlands) gradually settled, particularly in the West of Britannia and Wales. 
+            They were not at the time Christians and followed their own religion although Christianity
+            continued in some areas where the Anglo-Saxons had not settled.
+            <br />
+            <br /> 
+            As a result of this loss in Christianity, in AD595 Pope Gregory sent a mission to 
+            Britain from Rome led by St Augustine to convert the Anglo-Saxons to Christianity. 
+            Augustine arrived in Kent and firstly converted King Ethelbert, who ruled there. 
+            Overtime more Anglo-Saxon kings and the population also became Christians as it was
+            seen as one way in which they could unite people. 
+            <br />
+            <br />
+            In the 7th century AD  Northumbria consisted of two parts: Deira, centred on the old Roman city
+            of York, and Bernicia further north.   {" "}   
+            <Link
+              href="https://englandsnortheast.co.uk/king-oswald-aidan/"
+              target="_blank"
+              className="text-decoration-underline text-[#FFC000] font-semibold underline"
+            >
+              King Oswald of Northumbria
+            </Link>{" "}
+           was from the Bernician dynasty which was based north of the Tees. His uncle King Edwin, who died in 633AD, was from Deira (Yorkshire) and the 2 were rivals.  In AD 616 Oswald was expelled from Northumbria by his uncle Edwin. He and his brother took refuge on the Scottish island of Iona, where they met a group of Irish monks who included Aidan. Whilst in Iona Oswald remained a pagan, but his defeat of the Welsh at the Battle of Heavenfield, near Hexham in 634 AD persuaded him to convert to Christianity as before the battle, he prayed to the Christian God for victory and his prayers were seemingly answered.  
+             <br />
+            <br />            
+           On acceeding to become King Oswald of Northumbria in AD 634, he focused Northumbrian power in Bernicia, around the royal palaces at Yeavering, Mælmin (Milfield) and Bamburgh. 
+             <br />
+            <br />            
+                  
+            King Oswald was determined to reintroduce Christianity to
+            the North-East and so employed    <Link href="https://www.lindisfarne.org.uk/general/aidan.htm"
+              target="_blank"
+              className="text-decoration-underline text-[#FFC000] font-semibold underline"
+            >Aidan</Link> one of the Irish monks he had met in Iona, to
+            convert Northumbria to Celtic Christianity. King Oswald invited the monks of Iona to send a mission and eventually Aidan arrived with 12 other monks and chose to settle on the island the English had renamed Lindisfarne. In AD 633 King Oswald granted Aidan and his companions this small tidal island on which to establish a
+            monastery and in AD 635 Aidan founded the first monastery on the island and he became the first Bishop of Lindisfarne. 
+              <br />
+            <br />
+            The monastery which AIdan established was Irish in type with wooden buildings: a small church, small, circular dwelling huts, perhaps one larger building for communal purposes and in time, workshops etc as needed. Here the monks lived a life of prayer, study and austerity. From here they went out on mission. First learning the English language. In this, king Oswald, who had learnt Irish in his boyhood in exile, helped them. Then they went out, using Aidan's only method as a missionary, which was to walk the lanes, talk to all the people he met and interest them in the faith if he could. 
+             <br />
+            <br />
+            
+            After 16 years as bishop Aidan died at Bamburgh in 651 AD. His age at death is not known. What he had achieved may not have been clear to him at death but subsequent history showed the strong foundations and lasting success of his mission. The missionaries trained in his school went out and worked for the conversion of much of Anglo-Saxon England.
+            <br />
+            <br />
+            It is important to note that Celtic Christianity was slightly different
+            in terms of customs, organisation and values to the Roman style of
+            Christianity introduced by Paulinus under King Edwin. These differences
+            were largely subtle and presentational but they were resolved at the
+            Synod of Whitby in  AD 664. The Roman Catholic Church was strictly
+            hierarchical, legalistic, and elitist, whereas the Celtic Church was
+            provincial and local based, monastic, mystical, and celebrated the
+            sacredness of nature, thus reflecting the patterns of life in the
+            communities in which the church flourished.
+            <br />
+            <br />
+            <Link href="https://www.lindisfarne.org.uk/general/cuthbert1.htm"
+              target="_blank"
+              className="text-decoration-underline text-[#FFC000] font-semibold underline"
+            >Cuthbert</Link> (later to become Saint Cuthbert), was born in North Northumbria in about the year 635 - the same year in which Aidan founded the monastery on Lindisfarne. He came from a well-to-do English family and like most boys of that class, he was placed with foster-parents for part of his childhood and taught the arts of war. It seems that he was brought up as a Christian. 
+             <br />
+            <br />
+            His life changed when he was about 17 years old and was looking after some neighbour's sheep on the hills. Gazing into the night sky he saw a light descend to Earth and then return, escorting, he believed, a human soul to Heaven. The date was August 31st AD 651 which was the night that Aidan died. Perhaps Cuthbert had already been considering a possible monastic calling but that was his moment of decision.
+              <br />
+            <br />
+            He went to the monastery at Melrose, also founded by Aidan, and asked to be admitted as a Novice. 
+            For the next 13 years he was with the Melrose monks. When Melrose was given land to found a new monastery at Ripon, Cuthbert went with the founding party and was made guestmaster. In his late 20s he returned to Melrose and found that his former teacher and friend, the prior Boisil, was dying of the plague. Cuthbert became prior (second to the Abbot) at Melrose.
+            <br />
+            <br />
+            In 664AD the Synod of Whitby decided that Northumbria should cease to look to Ireland for its spiritual leadership and turn instead to the continent. The Irish monks of Lindisfarne, with others, went back to Iona. The abbot of Melrose subsequently became also abbot of Lindisfarne and Cuthbert its prior.
+              <br />
+            <br />
+            
+            According to information provided by{" "}
+            <Link
+              href="https://www.english-heritage.org.uk/visit/places/lindisfarne-priory/History/"
+              target="_blank"
+              className="text-decoration-underline text-[#FFC000] font-semibold underline"
+            >
+              English Heritage
+            </Link>{" "}
+            sometime in the AD 670s a monk named Cuthbert joined the monastery at Lindisfarne. He would have been aged about 30. He eventually became Lindisfarne’s greatest monk-bishop, and the most important saint in northern England in Medieval times. He is the patron saint of Northumbria.
+            <br />
+            <br />
+            As prior of Lindisfarne, Cuthbert reformed the monks’ way of life to conform to the religious practices of Rome rather than Ireland. This caused bitterness, and he decided to retire and live as a hermit. He lived at first on an island (now called St Cuthbert’s Isle) just offshore, but later moved across the sea to the more remote island of Inner Farne.
+            <br />
+            <br />
+            On the insistence of the king, however, Cuthbert was made a bishop in 685. His new duties brought him back into the world of kings and nobles, but he acquired a considerable reputation as a pastor, seer and healer.
+            
+            <br />
+            <br />            
+            Cuthbert died on 20 March AD 687 and was buried in a stone coffin inside the main church on Lindisfarne. Eleven years later, on opening his tomb, the monks discovered that Cuthbert’s body had not decayed, a sign
+            according to the monks of his purity and saintliness. His remains were elevated to a coffin-shrine at ground level, and this marked the beginnings of the cult of St Cuthbert, which was to alter the course of
+            Lindisfarne’s history. Miracles were soon reported at St Cuthbert’s shrine and Lindisfarne was quickly established as the major pilgrimage centre in Northumbria. As a result, the monastery grew in power and
+            wealth, attracting grants of land from kings and nobles as well as gifts of money and precious objects. The cult of St Cuthbert also consolidated the monastery’s reputation as a centre of Christian learning. One of the
+            results was the production in about 710–25 of the masterpiece of early medieval art known today as the{" "}
+            <Link
+              href="https://www.lindisfarne.org.uk/gospels/"
+              target="_blank"
+              className="text-decoration-underline text-[#FFC000] font-semibold underline"
+            >
+              Lindisfarne Gospels
+            </Link>{" "}
+            .
+            <br />
+            <br />
+            On the 8th June AD/ACE 793, the Vikings attacked. It was their first
+            significant raid in Europe and was physically and psychologically
+            devastating: one of England’s holiest shrines had been attacked by
+            pagans, and St Cuthbert had not intervened to stop them. As a result of
+            these attacks the monks moved inland to Norham and later settled in
+            Chester-le-Street. In 875 the decision was made to leave Lindisfarne for
+            good. In 995 St Cuthbert’s relics were moved again and eventually
+            enshrined at Durham, where they remain. The prosperity of the Durham
+            monastic community was based on its ability to attract pilgrims to the
+            shrine.
+            <br />
+            <br />
+            In the 11th to 13th century the Durham monks returned to Lindisfarne and
+            built {" "}
+            <Link
+              href="https://www.english-heritage.org.uk/visit/places/lindisfarne-priory/"
+              target="_blank"
+              className="text-decoration-underline text-[#FFC000] font-semibold underline"
+            >
+              Lindisfarne Priory{" "}
+            </Link>
+            with a cenotaph marking Cuthbert's original grave.
+            <br />
+            <br />
+          </div>
+          </div>          
+</div>
+            <br/>           
+            It was a long drive but we got to see lots and when we finally arrived on Holy Island, parking was straightforward, as on this occasion we could use the free, disabled parking on Green Lane at the Chare Ends car park. Once parked, it was time to decamp to Chare Ends café for tea and coffees and to meet the very tame sparrows and starlings there. 
+            <br />
+            <br />            
+            Refreshed, we made our way to Lindisfarne Museum and the ruins of Lindisfarne Priory to have a look around.
+            We then had a pleasant lunch at the Crown and Anchor pub, before wandering over as far as the entrance to the castle, although we were unable to go inside the castle as disabled access is poor within the castle building itself. We had an enjoyable day and returned via the A1 and A69 back home.
+            <br/>
+            <br/>
+           
             <div className="flex flex-col border-solid border-4 border-[#436A6b] bg-[#8d6fb0] pb-2 pt-2 mr-[12%] ml-[12%] mt-5 mb-4  justify-center text-center">
             <div className="mt-1 mb-1 sm:m-2 text-md font-normal block text-white  bg-[#8d6fb0]">
               {" "}
