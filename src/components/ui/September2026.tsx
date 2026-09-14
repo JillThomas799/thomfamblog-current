954a7695db462e792182f71159b137b50b7d2b03
Jill <125391549+JillThomas799@users.noreply.github.com>
2026-09-14 13:30:22 +0100

    Change to background color to enable visibility text

--- src/components/ui/September2026.tsx
+++ src/components/ui/September2026.tsx
@@ -12,7 +12,7 @@ export default function September2026() {
         <div className="text-[#E7E1F0] text-center font-semibold text-xl p-3 bg-[#8d6fb0] border-solid border-4 border-[#436A6b] m-2">September 2026</div>
           </div>
        
-      <div className="flex flex-col max-w-full leading-1.5 sm:p-2 md:p-2 lg:p-2 m-4 border-[#436A6b] border-solid border-4 bg-[#c3b7bb]">
+      <div className="flex flex-col max-w-full leading-1.5 sm:p-2 md:p-2 lg:p-2 m-4 border-[#436A6b] border-solid border-4 bg-[#afa488]">
          <div className="text-md md:text-lg max-w-full font-bold text-[#E7E1F0]  text-center dark:text-[#244845] justify-center items-center">
         <div className="font-bold border-solid border-[#436A6b] border-4 mr-1 ml-1 mb-4 mt-4 px-2 py-2 text-[#E7E1F0]  bg-[#8d6fb0]">1st - 30th September 2026.</div> 
           <div className="font-bold border-solid border-[#436A6b] border-4 mr-1 ml-1 mb-2 px-4 py-2 text-[#E7E1F0] bg-[#8d6fb0]">Welcome to our September 2026 blog.</div> 
@@ -25,7 +25,7 @@ export default function September2026() {
          
 
 
-            <div className="flex flex-col max-w-full leading-1.5 sm:p-2 md:p-2 lg:p-2 m-4 border-[#436A6b] border-solid border-4 bg-[#c3b7bb]">
+            <div className="flex flex-col max-w-full leading-1.5 sm:p-2 md:p-2 lg:p-2 m-4 border-[#436A6b] border-solid border-4 bg-[#afa488]">
          <div className="text-md md:text-lg max-w-full font-semibold text-[#244845] text-center dark:text-[#244845] justify-center items-center">
         <div className="font-bold border-solid border-[#436A6b] border-4 mr-1 ml-1 mb-4 mt-4 px-2 py-2 text-[#E7E1F0]  bg-[#8d6fb0]">1st September 2026.</div> 
         <div className="font-bold border-solid border-[#436A6b] border-4 mr-1 ml-1 mb-4 px-2 py-2 text-[#E7E1F0] bg-[#8d6fb0]">Whistle-stop tour of Northumberland and visit to the Holy Island of Lindisfarne.</div> 
